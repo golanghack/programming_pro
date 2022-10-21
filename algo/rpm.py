b54a6c71779b9b1b5e4f5bd331d39acde3a1c0a5
golanghack <golanghack@gmail.com>
2022-10-21 18:41:22 +0300

    rpm algo

--- algo/rpm.py
+++ algo/rpm.py
@@ -20,13 +20,7 @@ start_number, end_number = user_numbers('Enter numbers--> ')
 graf_divide = [start_number]
 graf_multiply = [end_number]
 
-while min(graf_divide) > 1:
-    graf_divide.append(fl(min(graf_divide) / 2))
-        
-while (len(graf_multiply) < len(graf_divide)):
-    graf_multiply.append(max(graf_multiply) * 2)
-    
-    
+
 graf_halfy = pd.DataFrame(zip(graf_divide, graf_multiply))
 graf_halfy = graf_halfy.loc[graf_halfy[0] % 2 == 1, :]
 full_summ = sum(graf_halfy.loc[:, 1])
