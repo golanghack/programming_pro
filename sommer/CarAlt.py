d1afead8c7b344d92c34a7747851cb9ddad8d13b
golanghack <golanghack@gmail.com>
2022-11-26 19:28:40 +0300

    added new program -> sommer -> CarAlt

--- sommer/CarAlt.py
+++ sommer/CarAlt.py
@@ -9,7 +9,7 @@
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
 # General Public License for more details.
 
-"""<--CAR CLASS-->"""
+"""<--CAR ALT CLASS-->"""
 
 """Class Car
  wins -> count wins
@@ -17,10 +17,11 @@
  mark -> name of mark
  prod -> name of produnction
  """
+import math
  
 class Car:
     
-    def __init__(self, wins=4, sits=5, mark='Subaru', prod='Japan'):
+    def __init__(self, wins=4, sits=5, mark='Subaru', prod='Japan', dist=1000):
         """A Car characteristic
         
         >>> car = Car(4, 5, 'Subaru', 'Japan')
@@ -32,8 +33,18 @@ class Car:
         self.sits = sits
         self.mark = mark
         self.prod = prod 
+        self.dist = dist
         
-
+    @property
+    def distance_price(self):
+        """The price from distance coefficient
+        
+        >>> car = Car(4, 5, 'Subaru', 'Japan', 100)
+        >>> car.distance_price
+        10.0
+        """
+        return math.sqrt(self.dist)
+    
     def __repr__(self):
         return f'Car ({self.wins!r}, {self.sits!r}, {self.mark!r}, {self.prod!r})'
             
@@ -49,6 +60,17 @@ class Wolcwagen(Car):
         >>> ww
         Car (4, 4, 'WW', 'De')
         """
+        @property
+        def count_people(self):
+            """The counting people in car
+            
+            >>> ww = Wolcwagen()
+            >>> a = ww.count_people
+            >>> int(a)
+            4
+            """
+            
+            return self.sits
         
         super().__init__(wins, sits, mark, prod)
         self.condo = condo
