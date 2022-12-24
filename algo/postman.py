4582e08058b994d072690d4499e3c1bba9c57368
golanghack <golanghack@gmail.com>
2022-12-24 17:43:59 +0300

    added new programm -> algo -> postman

---
+++ algo/postman.py
@@ -0,0 +1,44 @@
+#! /usr/bin/env python3 
+
+"""<--GEOMETRY-->"""
+
+import pylab as pl 
+from matplotlib import collections as mc 
+
+point: list = [0.8, 0.2]
+triangle_simple: list = [[0.8, 0.2], [0.5, 0.2], [0.6, 0.7]]
+
+def from_points_to_triangle(point1: list, point2: list, point3: list) -> list:
+    """Building triangle from three points"""
+    
+    triangle = [list(point1), list(point2), list(point3)]
+    return triangle
+
+def generator_of_lines(list_points: list, itinerary: list) -> list:
+    """Function generator lines from points"""
+    
+    lines: list = []
+    for i in range(len(itinerary) - 1):#TODO! -> list comp-s
+        lines.append([list_points[itinerary[i]], list_points[itinerary[i + 1]]])
+    return (lines)
+
+def plot_trinagle_generated_simple(triangle: list, thename: str) -> None:
+    """draw elementary picture of simple trinagle"""
+    
+    figure, ax_coords = pl.subplots()
+    xs_base = [triangle[0][0], triangle[1][0], triangle[2][0]]
+    ys_base = [triangle[0][1], triangle[1][1], triangle[2][1]]
+    
+    itin = [0, 1, 2, 0]
+    lines = generator_of_lines(triangle, itin)
+    
+    lc_base = mc.LineCollection(generator_of_lines(triangle, itin), linewidths=3)
+    ax_coords.add_collection(lc_base)
+    ax_coords.margins(0.2)
+    pl.scatter(xs_base, ys_base)
+    pl.show()
+    #pl.savefig(str(thename) + '.png')#TODO -> used tempfile
+    #pl.close()
+    
+    
+plot_trinagle_generated_simple(from_points_to_triangle((0.2, 0.8), (0.5, 0.2), (0.8, 0.7)), 'Triangle')
