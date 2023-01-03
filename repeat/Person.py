3b5debce03a8b16b06c6c4789ef1d97e0e6ba7d8
golanghack <golanghack@gmail.com>
2023-01-03 16:13:58 +0300

    added new programm -> repeat -> Person -> refactoring -> added methods last_name, give_raise

--- repeat/Person.py
+++ repeat/Person.py
@@ -9,7 +9,17 @@ class Person:
         self.name = name
         self.job = job
         self.pay = pay 
-
+        
+    def last_name(self):
+        """Return last name in list names with [-1] index logic"""
+        
+        return self.name.split()[-1]
+    
+    def give_raise(self, percent: float):
+        """Return new pay with raise"""
+        
+        self.pay = int(self.pay * (1 + percent))
+        
 
 if __name__ == '__main__':
     #testing
@@ -17,3 +27,6 @@ if __name__ == '__main__':
     sue = Person('Sue', job='dev', pay=100000)
     print(bob.name, bob.job, bob.pay)
     print(sue.name, sue.job, sue.pay)
+    print(bob.last_name(), sue.last_name())
+    sue.give_raise(.10)
+    print(sue.pay)
