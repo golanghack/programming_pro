9d532d752cf0615ae6d7185b99eef8d3fa230c0a
golanghack <golanghack@gmail.com>
2023-01-04 22:46:56 +0300

    added new programm -> patterns -> decorator3

--- patterns/decorator3.py
+++ patterns/decorator3.py
@@ -2,30 +2,20 @@
 
 """<--DECORATOR-->"""
 
-import numbers
-
-def ensure(name: str, validate, doc: str=None):
-    """create decorator of class"""
-    
-    def decorator(Class):
-        """create private attr of class"""
-        
-        private_name = '__', + name
+def do_ensure(Class):
+    def make_property(name, attribute):
+        private_name = '__' + name
         
         def getter(self):
-            """return mean of attribute"""
-            
             return getattr(self, private_name)
-        
         def setter(self, value):
-            """call validation function and set  write new mean"""
-            
-            validate(name, value)
+            attribute.validate(name, value)
             setattr(self, private_name, value)
-        #create attribute or write in attribute
-        setattr(Class, name, property(getter, setter, doc=doc))
-        return Class
-    return decorator
+        return property(getter, setter, doc=attribute.doc)
+    for name, attribute in Class.__dict__.items():
+        if isinstance(attribute, Ensure):
+            setattr(Class, name, make_property(name, attribute))
+    return Class
 
 def is_non_empty_str(name: str, value: str):
     """Testing empty string or str type"""
@@ -34,7 +24,19 @@ def is_non_empty_str(name: str, value: str):
         raise ValueError(f'{name} must be of type str')
     if not bool(value):
         raise ValueError(f'{name} may not be empty')
+
+def is_valid_isbn(isbn):
+    """testing lenght of isbn number"""
     
+    if len(isbn) != 10:
+        raise ValueError(f'{isbn} must by lenght 10')
+    return isbn
+
+class Ensure:
+    def __init__(self, validate, doc=None):
+        self.validate = validate
+        self.doc = doc
+
 def is_in_range(minimum: int=None, maximum: int=None):
     """Testing range for input date"""
     
@@ -47,23 +49,16 @@ def is_in_range(minimum: int=None, maximum: int=None):
         if maximum is not None and value > maximum:
             raise ValueError(f'{name} {value} is too big')
     return is_in_range
-
-def is_valid_isbn(isbn):
-    """testing lenght of isbn number"""
-    
-    if len(isbn) != 10:
-        raise ValueError(f'{isbn} must by lenght 10')
-    return isbn
-
-
-
-@ensure('title', is_non_empty_str)
-@ensure('isbn', is_valid_isbn)
-@ensure('price', is_in_range(1, 10000))
-@ensure('quantity', is_in_range(0, 10000000))
+      
+@do_ensure
 class Book:
     
-    def __init__(self, title: str, isbn: int, price: float, quantity: int) -> None:
+    title = Ensure(is_non_empty_str)
+    isbn = Ensure(is_valid_isbn)
+    price = Ensure(is_in_range(1, 10000))
+    quantity = Ensure(is_in_range(0, 10000000))
+    
+    def __init__(self, title, isbn, price, quantity):
         self.title = title
         self.isbn = isbn
         self.price = price
@@ -74,4 +69,3 @@ class Book:
         return self.price * self.quantity
     
     
-
