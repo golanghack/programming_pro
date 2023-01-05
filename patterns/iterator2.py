bc5860326a0e7111bfa962c9d56aadcf26eda730
golanghack <golanghack@gmail.com>
2023-01-05 20:59:12 +0300

    added new programm -> patterns -> iterator2

--- patterns/iterator2.py
+++ patterns/iterator2.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-# Copyright © 2012-13 Qtrac Ltd. All rights reserved.
+
 # This program or module is free software: you can redistribute it and/or
 # modify it under the terms of the GNU General Public License as published
 # by the Free Software Foundation, either version 3 of the License, or
@@ -8,7 +8,7 @@
 # WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
 # General Public License for more details.
-
+"""<--ITERATOR-->"""
 
 class Bag:
 
