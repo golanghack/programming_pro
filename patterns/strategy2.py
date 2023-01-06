dcb164286d16dabff72a56a17996d92732537a46
golanghack <golanghack@gmail.com>
2023-01-06 15:33:38 +0300

    added new program -> patterns -> strategy2

---
+++ patterns/strategy2.py
@@ -0,0 +1,97 @@
+#!/usr/bin/env python3
+
+# This program or module is free software: you can redistribute it and/or
+# modify it under the terms of the GNU General Public License as published
+# by the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version. It is provided for educational
+# purposes and is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+# General Public License for more details.
+"""<--STRATEGY-->"""
+
+
+
+import sys
+if sys.version_info[:2] < (3, 2):
+    from xml.sax.saxutils import escape
+else:
+    from html import escape
+
+
+WINNERS = ("Nikolai Andrianov", "Matt Biondi", "Bjørn Dæhlie",
+        "Birgit Fischer", "Sawao Kato", "Larisa Latynina", "Carl Lewis",
+        "Michael Phelps", "Mark Spitz", "Jenny Thompson")
+
+
+def main():
+    html_layout = Layout(HtmlTabulator)
+    for rows in range(2, 6):
+        print(html_layout.tabulate(rows, WINNERS))
+    text_layout = Layout(TextTabulator)
+    for rows in range(2, 6):
+        print(text_layout.tabulate(rows, WINNERS))
+
+
+class Layout:
+
+    def __init__(self, tabulator):
+        self.tabulator = tabulator
+
+
+    def tabulate(self, rows, items):
+        return self.tabulator.tabulate(rows, items)
+
+
+class HtmlTabulator:
+
+    @staticmethod
+    def tabulate(rows, items):
+        columns, remainder = divmod(len(items), rows)
+        if remainder:
+            columns += 1
+        column = 0
+        table = ['<table border="1">\n']
+        for item in items:
+            if column == 0:
+                table.append("<tr>")
+            table.append(f"<td>{escape(str(item))}</td>")
+            column += 1
+            if column == columns:
+                table.append("</tr>\n")
+            column %= columns
+        if table[-1][-1] != "\n":
+            table.append("</tr>\n")
+        table.append("</table>\n")
+        return "".join(table)
+
+
+class TextTabulator:
+
+    @staticmethod
+    def tabulate(rows, items):
+        columns, remainder = divmod(len(items), rows)
+        if remainder:
+            columns += 1
+            remainder = (rows * columns) - len(items)
+            if remainder == columns:
+                remainder = 0
+        column = column_width = 0
+        for item in items:
+            column_width = max(column_width, len(item))
+        column_divider = ("-" * (column_width + 2)) + "+"
+        divider = "+" + (column_divider * columns) + "\n"
+        table = [divider]
+        for item in items + (("",) * remainder):
+            if column == 0:
+                table.append("|")
+            table.append(f" {item:<{column_width}} |")
+            if column == columns:
+                table.append("\n")
+            column %= columns
+        table.append(divider)
+        return "".join(table)
+
+
+if __name__ == "__main__":
+    main()
