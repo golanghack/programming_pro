10ba1b57c0b31cf85d30b7c9741436b8f72c85d1
golanghack <golanghack@gmail.com>
2023-01-09 16:58:51 +0300

    added new program -> tests -> test_experiment

--- tests/card_proj/test_experiment.py
+++ tests/card_proj/test_experiment.py
@@ -7,5 +7,11 @@ def test_raises_with_info():
     match_regex = 'missing 1.*position argument'
     with pytest.raises(TypeError, match=match_regex):
         cards.CardsDB()
+        
+def test_raises_with_info_alt():
+    with pytest.raises(TypeError) as exc_info:
+        cards.CardsDB()
+    expected = 'missing 1 required position argument'
+    assert expected in str(exc_info.value)
 
     
