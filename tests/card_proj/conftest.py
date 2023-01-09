3b887d820d392975ff4768a9700055aab88ed6d8
golanghack <golanghack@gmail.com>
2023-01-09 21:24:00 +0300

    added new program -> tests -> conftest

--- tests/card_proj/conftest.py
+++ tests/card_proj/conftest.py
@@ -5,7 +5,7 @@ import cards
 import pytest
 
 @pytest.fixture(scope='session')
-def cards_db():
+def db():
     """CardsDB object connected to a temporary database."""
     with TemporaryDirectory() as db_dir:
         db_path = Path(db_dir)
@@ -13,3 +13,10 @@ def cards_db():
         yield db 
         db.close()
         
+@pytest.fixture(scope='function')
+def cards_db(db):
+    """CardsDB object that`s empty."""
+    db.delete_all()
+    return db 
+
+
--- tests/card_proj/test_count.py
+++ tests/card_proj/test_count.py
@@ -20,3 +20,10 @@ def test_two(cards_db):
     cards_db.add_card(cards.Card('second'))
     
     assert cards_db.count() == 2
+    
+def test_three(cards_db):
+    cards_db.add_card(cards.Card('first'))
+    cards_db.add_card(cards.Card('second'))
+    cards_db.add_card(cards.Card('third'))
+    
+    assert cards_db.count() == 3
