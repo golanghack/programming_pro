e10fa6534932e0aea41604565c26bace5f43b9c8
golanghack <golanghack@gmail.com>
2023-01-09 21:47:31 +0300

    added new program -> tests -> conftest

--- tests/card_proj/conftest.py
+++ tests/card_proj/conftest.py
@@ -4,7 +4,12 @@ from tempfile import TemporaryDirectory
 import cards
 import pytest
 
-@pytest.fixture(scope='session')
+def db_scope(fixture_name, config):
+    if config.getoption('--func-db', None):
+        return 'function'
+    return 'session'
+
+@pytest.fixture(scope='db_scope')
 def db():
     """CardsDB object connected to a temporary database."""
     with TemporaryDirectory() as db_dir:
@@ -44,21 +49,13 @@ def non_empty_db(cards_db, some_cards):
 
 def test_non_empty(non_empty_db):
     assert non_empty_db.count() > 0
-    
-@pytest.fixture(scope='db_scope')
-def _db():
-    """CardsDB object connected to a temporary database."""
-    with TemporaryDirectory() as db_dir:
-        db_path = Path(db_dir)
-        db = cards.CardsDB(db_path)
-        yield db 
-        db.close()
-        
-def db_scope(fixture_name, config):
-    if config.getoption('--func-db', None):
-        return 'function'
-    return 'session'
 
-
+def pytest_addoption(parser):
+    parser.addoption(
+        '--func-db', 
+        action='store_true', 
+        default=False, 
+        help='new db for each test',
+    )
         
         
