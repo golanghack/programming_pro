64f67484d5e65db8090e3bdb3f687c3ea8dd5dd0
golanghack <golanghack@gmail.com>
2023-01-09 19:35:07 +0300

    added new program -> tests -> conftest

---
+++ tests/card_proj/conftest.py
@@ -0,0 +1,15 @@
+#! /usr/bin/env python3 
+from pathlib import Path
+from tempfile import TemporaryDirectory
+import cards
+import pytest
+
+@pytest.fixture(scope='session')
+def cards_db():
+    """CardsDB object connected to a temporary database."""
+    with TemporaryDirectory() as db_dir:
+        db_path = Path(db_dir)
+        db = cards.CardsDB(db_path)
+        yield db 
+        db.close()
+        
