5cb29e167472a9aa519fac8f87b24ac428ab204f
golanghack <golanghack@gmail.com>
2023-01-09 16:54:42 +0300

    added new program -> tests -> test_experiment

--- tests/card_proj/test_experiment.py
+++ tests/card_proj/test_experiment.py
@@ -1,7 +1,8 @@
 #! /usr/bin/env python3 
 
 import cards
+import pytest
 
-def test_no_path_fail():
-    cards.CardsDB()
-    
+def test_no_path_raises():
+    with pytest.raises(TypeError):
+        cards.CardsDB()
