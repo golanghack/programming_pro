017770910c446384f063badd398a8cd442419800
golanghack <golanghack@gmail.com>
2023-01-09 18:48:24 +0300

    added new program -> tests -> test_count

--- tests/card_proj/test_count.py
+++ tests/card_proj/test_count.py
@@ -14,3 +14,9 @@ def cards_db():
         
 def test_empty(cards_db):
     assert cards_db.count() == 0
+    
+def test_two(cards_db):
+    cards_db.add_card(cards.Card('first'))
+    cards_db.add_card(cards.Card('second'))
+    
+    assert cards_db.count() == 2
