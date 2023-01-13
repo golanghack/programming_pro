d6cdbc3828f5c333f2b54c77377216613f757f17
golanghack <golanghack@gmail.com>
2023-01-13 17:16:00 +0300

    added new program -> tests -> test_finish_param

---
+++ tests/card_proj/test_func_param.py
@@ -0,0 +1,30 @@
+#! /usr/bin/env python3 
+
+import pytest
+from cards import Card 
+
+
+@pytest.mark.parametrize(
+    'start_summary, start_state',
+    [
+        ('write a book', 'done'),
+        ('second edition', 'in prog'),
+        ('create a course', 'todo'),
+    ],
+)
+def test_finish(cards_db, start_summary, start_state):
+    initial_card = Card(summary=start_summary, state=start_state)
+    index = cards_db.add_card(initial_card)
+    cards_db.finish(index)
+    card = cards_db.get_card(index)
+    assert card.state == 'done'
+    
+@pytest.mark.parametrize('start_state', ['done', 'inprog', 'todo'])
+def test_finish_simple(cards_db, start_state):
+    c = Card('srite a book', state=start_state)
+    index = cards_db.add_card(c)
+    cards_db.finish(index)
+    card = cards_db.get_card(index)
+    assert card.state == 'done'
+    
+    
