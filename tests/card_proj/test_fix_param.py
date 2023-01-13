96eea2874235e250f450c2399827212f5f6bfb90
golanghack <golanghack@gmail.com>
2023-01-13 21:36:09 +0300

    added new program -> tests -> test_fix_param

--- tests/card_proj/test_fix_param.py
+++ tests/card_proj/test_fix_param.py
@@ -6,3 +6,11 @@ from cards import Card
 @pytest.fixture(params=['done', 'in prog', 'todo'])
 def start_state(request):
     return request.param
+
+def test_finish(cards_db, start_state):
+    c = Card('srite a book', state=start_state)
+    index = cards_db.add_card(c)
+    cards_db.finish(index)
+    card = cards_db.get_card(index)
+    assert card.state == 'done'
+    
