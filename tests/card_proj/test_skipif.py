caf3b305dba72f6cb75c2d9b9727feeb7d408364
golanghack <golanghack@gmail.com>
2023-01-14 18:17:24 +0300

    added new program -> tests -> test_skipif

---
+++ tests/card_proj/test_skipif.py
@@ -0,0 +1,13 @@
+import pytest
+import cards
+from packaging.version import parse
+
+@pytest.mark.skipif(
+    parse(cards.__version__).major < 2,
+    reason='Card < comparison not supported in 1.x',
+)
+
+def test_less_than():
+    c1 = cards.Card('a task')
+    c2 = cards.Card('b task')
+    assert c1 < c2 
