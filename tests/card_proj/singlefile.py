c64973f3e34e7b50a2039229e7cc3e582acd893b
golanghack <golanghack@gmail.com>
2023-01-15 21:16:59 +0300

    added new program -> tests -> singlefile

---
+++ tests/card_proj/singlefile.py
@@ -0,0 +1,26 @@
+def foo():
+    return 'foo'
+def bar():
+    return 'bar'
+def baz():
+    return 'baz'
+
+def main():
+    print(foo(), baz())
+    
+if __name__ == '__main__':
+    main()
+else:
+    import pytest
+    
+def test_foo():
+    assert foo() == 'foo'
+
+def test_baz():
+    assert baz() == 'baz'
+    
+def test_main(capsys):
+    main()
+    captured = capsys.readouterr()
+    assert captured.out == 'foo baz\n'
+    
