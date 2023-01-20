da05e621827d6506a2bbc77ef6dd7ef53ad33506
golanghack <golanghack@gmail.com>
2023-01-20 16:03:59 +0300

    added new program -> dragon_area -> metafinder

--- tests/tdd/functional_tests.py
+++ tests/tdd/functional_tests.py
@@ -3,4 +3,5 @@ from selenium import webdriver
 browser = webdriver.Firefox()
 browser.get("http://localhost:8000")
 
-assert "The install worked" in browser.title
+assert 'To-Do' in browser.title
+browser.quit()
