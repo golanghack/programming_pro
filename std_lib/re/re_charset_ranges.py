88b14ad43a6f04cbdc875120d6267147f9e1f74f
golanghack <golanghack@gmail.com>
2023-01-21 13:42:42 +0300

    added new program -> std_lib -> re -> re_charset_ranges

---
+++ std_lib/re/re_charset_ranges.py
@@ -0,0 +1,13 @@
+#! /usr/bin/env python3 
+
+from re_simple_patterns import test_patterns
+
+test_patterns(
+    'This is some text -- with punctuation.',
+    [
+        ('[a-z]+', 'sequences of lowercase letters'),
+        ('[A-Z]+', 'sequences of uppercase letters'),
+        ('[a-zA-Z]+', 'sequences of lower - or uppercase letters'),
+        ('[A-Z][a-z]+', 'one uppercase followed by lowercase')
+    ],
+)
