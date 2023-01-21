5ad90646ebdd71f4bae35b81732894cd507115d6
golanghack <golanghack@gmail.com>
2023-01-21 14:03:45 +0300

    added new program -> std_lib -> re -> re_anchoring

---
+++ std_lib/re/re_anchoring.py
@@ -0,0 +1,17 @@
+#! /usr/bin/env python3
+
+from re_simple_patterns import test_patterns
+
+test_patterns(
+    'This is some text -- with punctuation.',
+    [
+        (r'^\w+', 'word at start of string'),
+        (r'\A\w+', 'word at start of string'),
+        (r'\w+\S*$', 'word near end of string'),
+        (r'\w+\S*\Z', 'word near end of string'),
+        (r'\w*t\w*', 'word containing t'),
+        (r'\bt\w+', 't at start of word'),
+        (r'\w+t\b', 't at end of word'),
+        (r'\Bt\B', 't, not start or end of word')
+    ],
+)
