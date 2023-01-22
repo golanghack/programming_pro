7be105a47798fce9b8eedbba4168e4a3ed62eff5
golanghack <golanghack@gmail.com>
2023-01-22 21:54:54 +0300

    added new program -> flask -> first -> hello.py -> aded routing /user/name

--- flask/first/hello.py
+++ flask/first/hello.py
@@ -8,5 +8,9 @@ def index():
     word = 'word!'
     return f'Hello {word}'
 
+@app.route('/user/<name>')
+def user(name: str) -> str:
+    return f'<h1>Hello, {name}</h1>'
+
 if __name__ == '__main__':
     app.run(debug=True)
