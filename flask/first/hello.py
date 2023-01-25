e5c0c249e0a53be6745932471ada1dcba38b00f8
golanghack <golanghack@gmail.com>
2023-01-25 22:07:13 +0300

    added new program -> flask -> added templates and changes hello.py downloaded bootstrap

--- flask/first/hello.py
+++ flask/first/hello.py
@@ -1,16 +1,25 @@
 #! /usr/bin/env python3 
-
-from flask import Flask 
-from flask_script import Manager 
+from flask_bootstrap import Bootstrap
+from flask import Flask, render_template
 
 app = Flask(__name__)
-
-manager = Manager(app)
-
+bootstrap = Bootstrap(app)
 @app.route('/')
 def index():
-    word = 'word!'
-    return f'Hello {word}'
+    return render_template('index.html')
+
+@app.route('/user/<name>')
+def user(name):
+    return render_template('user.html', name=name)
+
+#errors
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+@app.errorhandler(500)
+def internal_server_error(e):
+    return render_template('500.html'), 500 
 
 if __name__ == '__main__':
-    manager.run()
+    app.run()
