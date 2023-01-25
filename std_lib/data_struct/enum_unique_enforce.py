6f7dad7c55b30a9c66efc63451744dde5caf9a1c
golanghack <golanghack@gmail.com>
2023-01-25 19:01:38 +0300

    added new program -> std_lib-> data_struct -> enum_unique_enforce

---
+++ std_lib/data_struct/enum_unique_enforce.py
@@ -0,0 +1,17 @@
+#! /usr/bin/env python3 
+
+import enum
+
+@enum.unique
+class BugStatus(enum.Enum):
+    
+    new = 7
+    incomplete = 6
+    invalid = 5
+    wont_fix = 4
+    in_progress = 3
+    fix_commited = 2
+    fix_released = 1
+    by_design = 4
+    closed = 1
+    
