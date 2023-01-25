073eda31f28831ef566ea42f821add8233a4ca6d
golanghack <golanghack@gmail.com>
2023-01-25 17:57:30 +0300

    added new program -> std_lib-> difflib -> difflib_unidiff

---
+++ std_lib/difflib/difflib_unidiff.py
@@ -0,0 +1,11 @@
+#! /usr/bin/env python3
+
+import difflib
+from difflib_data import *
+
+diff = difflib.unified_diff(
+    text1_lines,
+    text2_lines,
+    lineterm='',
+)
+print('\n'.join(list(diff)))
