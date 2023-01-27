f5e0da6533433fa9d273b4a6c2dad9f4a0b58862
golanghack <golanghack@gmail.com>
2023-01-27 17:17:00 +0300

    added new program -> std_lib -> data_struct -> collections_counter_init

---
+++ std_lib/data_struct/collections_counter_init.py
@@ -0,0 +1,7 @@
+#! /usr/bin/env python3
+
+import collections 
+
+print(collections.Counter(['a', 'b', 'c', 'a', 'b', 'b']))
+print(collections.Counter({'a': 2, 'b': 3, 'c': 1}))
+print(collections.Counter(a=2, b=3, c=1))
