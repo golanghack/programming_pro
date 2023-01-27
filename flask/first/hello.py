954d86f354cb6973fe89885f9a843d7e4cc88900
golanghack <golanghack@gmail.com>
2023-01-27 20:34:53 +0300

    added new program -> flask -> first -> flask-wtf and included secret key

--- flask/first/hello.py
+++ flask/first/hello.py
@@ -5,6 +5,7 @@ from flask import Flask, render_template
 from datetime import datetime
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'hard to guess string'
 bootstrap = Bootstrap(app)
 moment = Moment(app)
 
