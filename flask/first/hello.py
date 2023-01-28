9bf6c676bdeabfefb9f2c85e8a681311ee0cbad8
golanghack <golanghack@gmail.com>
2023-01-28 19:06:29 +0300

    added new program -> flask -> first -> hello -> sqlalchemy

--- flask/first/hello.py
+++ flask/first/hello.py
@@ -52,5 +52,22 @@ def page_not_found(e):
 def internal_server_error(e):
     return render_template('500.html'), 500 
 
+#models
+class Role(db.Model):
+    __tablename__ = 'roles'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.string(54), unique=True)
+    
+    def __repr__(self) -> str:
+        return f'<Role {self.name!r}>'
+    
+class User(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(64), unique=True, index=True)
+    
+    def __repr__(self) -> str:
+        return f'<User {self.username}>'
+
 if __name__ == '__main__':
     app.run()
