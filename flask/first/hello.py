c32b4881948c0a7b4301b15ad067d436d5f57c94
golanghack <golanghack@gmail.com>
2023-01-28 21:47:18 +0300

    added new program -> flask -> first -> hello -> sqlalchemy -> models -> work with db

--- flask/first/hello.py
+++ flask/first/hello.py
@@ -10,8 +10,8 @@ from flask import Flask, render_template, session, redirect, url_for, flash
 from datetime import datetime
 
 
-basedir = os.path.abspath(os.path.dirname(__file__))
 app = Flask(__name__)
+basedir = os.path.abspath(os.path.dirname(__file__))
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'data.sqlite')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
@@ -56,20 +56,20 @@ def internal_server_error(e):
 class Role(db.Model):
     __tablename__ = 'roles'
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.string(54), unique=True)
+    name = db.Column(db.String(64), unique=True)
     
-    def __repr__(self) -> str:
-        return f'<Role {self.name!r}>'
+    def __repr__(self):
+        return '<Role %r>' % self.name
+    users = db.relationship('User', backref='role', uselist=False)
     
 class User(db.Model):
     __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), unique=True, index=True)
-    users = db.relationship('User', backref='role')
     
-    def __repr__(self) -> str:
-        return f'<User {self.username}>'
+    def __repr__(self):
+        return '<User %r>' % self.username
     role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
-
+    
 if __name__ == '__main__':
     app.run()
