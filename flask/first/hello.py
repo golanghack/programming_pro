565da386cdfb1a9b4b0710988ebc9cdd31085ca3
golanghack <golanghack@gmail.com>
2023-01-29 21:05:30 +0300

    added new program -> flask -> first -> added new functionality for index def -> integrated db session in view

--- flask/first/hello.py
+++ flask/first/hello.py
@@ -29,15 +29,17 @@ class NameForm(FlaskForm):
 def index():
     form = NameForm()
     if form.validate_on_submit():
-        old_name = session.get('name')
-        if old_name is not None and old_name != form.name.data:
-            flash('Looks like you have changed yor name!')
+        user = User.query.filter_by(username=form.name.data).first()
+        if user is None:
+            user = User(username=form.name.data)
+            db.session.add(user)
+            session['known'] = False
+        else:
+            session['known'] = True
         session['name'] = form.name.data
+        form.name.data = ''
         return redirect(url_for('index'))
-    return render_template('index.html', 
-                           current_time=datetime.utcnow(), 
-                           name=session.get('name'), 
-                           form=form)
+    return render_template('index.html', form=form, name=session.get('name'), known=session.get('known', False))
 
 @app.route('/user/<name>')
 def user(name):
