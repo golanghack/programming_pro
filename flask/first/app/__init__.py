6fda35db32c56ee4df2b36f9fda1580f87eb14df
golanghack <golanghack@gmail.com>
2023-01-30 21:30:23 +0300

    added new program -> flask -> first -> added functionality views and routing and errors

--- flask/first/app/__init__.py
+++ flask/first/app/__init__.py
@@ -11,6 +11,7 @@ moment = Moment()
 db = SQLAlchemy()
 
 def create_app(config_name: str):
+
     app = Flask(__name__)
     app.config.from_object(config[config_name])
     config[config_name].init_app(app)
@@ -19,5 +20,8 @@ def create_app(config_name: str):
     mail.init_app(app)
     moment.init_app(app)
     db.init_app(app)
+    
+    from .main import main as main_blueprint 
+    app.register_blueprint(main_blueprint)
     
     return app 
