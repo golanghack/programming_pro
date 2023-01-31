a01bf1e4e2fb1d568bece881f8ecc81c79fbfb0b
golanghack <golanghack@gmail.com>
2023-01-31 22:37:49 +0300

    added new program -> flask -> second -> manage.py -> run testing

--- flask/second/manage.py
+++ flask/second/manage.py
@@ -15,3 +15,12 @@ migrate = Migrate(app. db)
 def make_shell_context():
     return dict(db=db, User=User, Role=Role)
 
+# unit test launcher command 
+@app.cli.command()
+def test():
+    """Run the unit tests."""
+    
+    import unittest
+    
+    tests = unittest.TestLoader().discover('tests')
+    unittest.TextTestRunner(verbosity=2).run(tests)
