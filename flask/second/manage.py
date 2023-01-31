e14cd62ccbdafcd3279bfcb56cac1024bf81afe8
golanghack <golanghack@gmail.com>
2023-01-31 22:31:03 +0300

    added new program -> flask -> second -> create dir config, refactoring

---
+++ flask/second/manage.py
@@ -0,0 +1,17 @@
+#! /usr/bin/env python3 
+
+"""Script for build and start app."""
+
+import os 
+from app import create_app, db 
+from app.models import User, Role
+from flask_migrate import Migrate
+
+app = create_app(os.getenv('FLASK_CONFIG') or 'default')
+migrate = Migrate(app. db)
+
+
+@app.shell_context_processor
+def make_shell_context():
+    return dict(db=db, User=User, Role=Role)
+
