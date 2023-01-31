02d3d5262d076e1676640ea309ec5218105af0dd
golanghack <golanghack@gmail.com>
2023-01-31 22:18:58 +0300

    added new program -> flask -> second -> app -> main -> views

--- flask/second/app/main/views.py
+++ flask/second/app/main/views.py
@@ -1,1 +1,24 @@
 #! /usr/bin/env python3 
+
+"""application routes in main blueprint."""
+
+from datetime import datetime
+from flask import render_template, session, redirect, url_for
+from . import main
+from .forms import NameForm 
+from .. import db 
+from ..models import User 
+
+@main.route('/', methods=['GET', 'POST'])
+def index():
+    """Default function for default route."""
+    
+    form = NameForm()
+    if form.validate_on_submit():
+        # ...
+        return redirect(url_for('.index'))
+    return render_template('index.html', 
+                           form=form, 
+                           name=session.get('name'),
+                           known=session.get('known', False),
+                           current_time=datetime.utcnow())
