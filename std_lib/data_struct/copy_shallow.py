122dae63cc353557727c8c61549460fce0fae5e2
golanghack <golanghack@gmail.com>
2023-01-31 18:59:14 +0300

    added new program -> std_lib -> data_struct -> copy_shallow

---
+++ std_lib/data_struct/copy_shallow.py
@@ -0,0 +1,27 @@
+#! /usr/bin/env python3 
+
+import copy
+import functools
+
+@functools.total_ordering
+class MyClass:
+    
+    def __init__(self, name):
+        self.name = name
+        
+    def __eq__(self, other) -> bool:
+        return self.name == other.name 
+    
+    def __gt__(self, other) -> bool:
+        return self.name > other.name
+    
+a = MyClass('a')
+my_list = [a]
+dublicate = copy.copy(my_list)
+
+print('---------- my_list --> ', my_list)
+print('---------- dublicate > ', dublicate)
+print('---------- dublicate is my_list -> ', (dublicate is my_list))
+print('---------- dublicate == my_list -> ', (dublicate == my_list))
+print('dublicate[0] is my_list[0] -> ', (dublicate[0] is my_list[0]))
+print('dublicate[0] == my_list[0] -> ', (dublicate[0] == my_list[0]))
