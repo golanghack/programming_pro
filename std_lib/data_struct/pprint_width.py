56afe3a0acd26b78b4d08830889350b4be5f74dc
golanghack <golanghack@gmail.com>
2023-02-01 17:58:03 +0300

    added new program -> std_lib -> data_struct -> pprint_width

---
+++ std_lib/data_struct/pprint_width.py
@@ -0,0 +1,9 @@
+#! /usr/bin/env python3 
+
+from pprint import pprint
+from pprint_data import data
+
+for width in [80, 5]:
+    print('WIDTH -> ', width)
+    pprint(data, width=width)
+    print()
