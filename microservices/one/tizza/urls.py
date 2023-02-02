ab23b8db4ff35ca8db01bc96d27ec391d4fd1334
golanghack <golanghack@gmail.com>
2023-02-02 21:04:57 +0300

    added new program -> microservices -> one -> tizza -> user -> templates -> registration

--- microservices/one/tizza/urls.py
+++ microservices/one/tizza/urls.py
@@ -15,9 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from django.contrib.auth.views import LoginView, LogoutView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path('pizzas/', include('pizza.urls')),
     path('user/', include('user.urls')),
+    path('login/', LoginView.as_view()),
+    path('logout/', LogoutView.as_view()),
 ]
