87d91341e0bde4b4967dcb7a58902f2a397e7773
golanghack <golanghack@gmail.com>
2023-02-02 19:10:06 +0300

    added new program -> microservices -> one -> pizza -> views

--- microservices/one/pizza/views.py
+++ microservices/one/pizza/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+from .models import Pizza
+
+def index(request: str, pid: int) -> HttpResponse:
+    pizza = Pizza.objects.get(id=pid)
+    return HttpResponse(
+        content={
+            'id': pizza.id, 
+            'title': pizza.title,
+            'description': pizza.description,
+        }
+    )
