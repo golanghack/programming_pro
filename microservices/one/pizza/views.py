d15f457f039c3e9cde29336d752a4e8260e99989
golanghack <golanghack@gmail.com>
2023-02-03 17:04:09 +0300

    added new program -> microservices -> tizza -> pizza -> views

--- microservices/one/pizza/views.py
+++ microservices/one/pizza/views.py
@@ -1,13 +1,31 @@
+import json
+from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse
-
 from .models import Pizza
 
+@login_required
 def index(request: str, pid: int) -> HttpResponse:
-    pizza = Pizza.objects.get(id=pid)
-    return HttpResponse(
-        content={
-            'id': pizza.id, 
-            'title': pizza.title,
-            'description': pizza.description,
-        }
-    )
+    if request.method == 'POST':
+        data = json.loads(request.body)
+        new_pizza = Pizza.objects.create(
+            title=data['title'],
+            description=data['description'],
+            creator=request.user,
+        )
+        new_pizza.save()
+        return HttpResponse(
+            content={
+                'id': new_pizza.id,
+                'title': new_pizza.title,
+                'description': new_pizza.description,
+            }
+        )
+    elif request.method == 'GET':
+        pizza = Pizza.objects.get(id=pid)
+        return HttpResponse(
+            content={
+                'id': pizza.id,
+                'title': pizza.title,
+                'description': pizza.description,
+            }
+        )
