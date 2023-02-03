57c769c63bc6613575a3373941717022054a415e
golanghack <golanghack@gmail.com>
2023-02-03 17:44:19 +0300

    added new program -> microservices -> tizza -> pizza -> refactoring views

--- microservices/one/pizza/views.py
+++ microservices/one/pizza/views.py
@@ -1,16 +1,30 @@
 import json
+
 from django.contrib.auth.decorators import login_required
-from django.contrib.auth.models import Permission
 from django.http import HttpResponse
+from django.shortcuts import render
+from django.views import View
+
 from .models import Pizza
 
+
+def index(request, pid):
+    pizza = Pizza.objects.get(id=pid)
+    return HttpResponse(
+        content={
+            'id': pizza.id,
+            'title': pizza.title,
+            'description': pizza.description,
+        }
+    )
+
+
 @login_required
-def index(request: str, pid: int) -> HttpResponse:
+def protected_index(request, pid):
     if request.method == 'POST':
         data = json.loads(request.body)
         new_pizza = Pizza.objects.create(
-            title=data['title'],
-            description=data['description'],
+            title=data['title'], description=data['description'],
             creator=request.user,
         )
         new_pizza.save()
@@ -19,7 +33,7 @@ def index(request: str, pid: int) -> HttpResponse:
                 'id': new_pizza.id,
                 'title': new_pizza.title,
                 'description': new_pizza.description,
-            }
+            },
         )
     elif request.method == 'GET':
         pizza = Pizza.objects.get(id=pid)
@@ -28,9 +42,9 @@ def index(request: str, pid: int) -> HttpResponse:
                 'id': pizza.id,
                 'title': pizza.title,
                 'description': pizza.description,
-            }
+            },
         )
-    elif request.method == 'DELETE':
+    elif requet.method == 'DELETE':
         if 'can_delete' in request.user.user_permissions:
             pizza = Pizza.objects.get(id=pid)
             pizza.delete()
@@ -41,3 +55,11 @@ def index(request: str, pid: int) -> HttpResponse:
             )
         else:
             return HttpResponse(status_code=404)
+
+
+class GetTenPizzasView(View):
+    template_name = 'ten_pizzas.html'
+
+    def get(self, request):
+        pizzas = Pizza.objects.order_by('?')[:10]
+        return render(request, self.template_name, {'pizzas': pizzas})
