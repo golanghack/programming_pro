67f530f78762b468abe9f981d7f712493697c4ce
golanghack <golanghack@gmail.com>
2023-02-04 16:12:22 +0300

    added new program -> asyncio -> listing_02 -> asuncio_delay

---
+++ asyncio/listing_02/delay.py
@@ -0,0 +1,14 @@
+#! /usr/bin/env python3
+
+"""UNIVERSAL DELAY"""
+
+import asyncio
+
+async def delay(delay_seconds: int) -> int:
+    """Function sleeping delay for coroutines."""
+    
+    print(f'I`m sleeping on -> {delay_seconds} sec.')
+    await asyncio.sleep(delay_seconds)
+    print(f'sleep current -> {delay_seconds} sec finished.')
+    return delay_seconds
+
