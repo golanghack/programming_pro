24294134cc8f5fe900b646f2d3ba8080eab598af
golanghack <golanghack@gmail.com>
2023-02-04 20:20:19 +0300

    added new program -> asyncio -> listing_02 -> asyncio_create_event_loop

---
+++ asyncio/asyncio_create_event_loop.py
@@ -0,0 +1,15 @@
+#! /usr/bin/env python3 
+
+import asyncio
+
+async def main() -> None:
+    await asyncio.sleep(1)
+    
+loop = asyncio.new_event_loop()
+
+try:
+    loop.run_until_complete(main())
+finally:
+    loop.close()
+    
+    
