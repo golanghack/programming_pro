658ef60127708ed57af914b6952d7076d487c028
golanghack <golanghack@gmail.com>
2023-02-04 20:15:37 +0300

    added new program -> asyncio -> listing_02 -> uncorrect_use_coroutin_and_blocking_api

---
+++ asyncio/listing_02/uncorrect_used_coroutin_and blocking_api.py
@@ -0,0 +1,22 @@
+#! /usr/bin/env python3
+
+import asyncio
+import requests
+from util import async_timed
+
+@async_timed()
+async def get_example_status() -> int:
+    return requests.get('https://google.com').status_code
+
+@async_timed()
+async def main() -> None:
+    task_1 = asyncio.create_task(get_example_status())
+    task_2 = asyncio.create_task(get_example_status())
+    task_3 = asyncio.create_task(get_example_status())
+    
+    await task_1
+    await task_2
+    await task_3
+    
+     
+asyncio.run(main())
