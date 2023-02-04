8dbc1fa61b506ede8395112052bff7a11299ec7e
golanghack <golanghack@gmail.com>
2023-02-04 20:46:16 +0300

    added new program -> django_and_drf -> simple -> -> one -> views -> home_page_view, urls

--- django_and_drf/simple/one/pages/views.py
+++ django_and_drf/simple/one/pages/views.py
@@ -1,3 +1,4 @@
-from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+def home_page_view(request: str) -> HttpResponse:
+    return HttpResponse('Hello')
