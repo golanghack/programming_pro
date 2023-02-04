0aef8d878708b44cc1ca94da705cf0c4ce8579bd
golanghack <golanghack@gmail.com>
2023-02-04 21:47:57 +0300

    added new program -> django_and_drf -> simple  -> three -> posts -> test

--- django_and_drf/simple/three/posts/tests.py
+++ django_and_drf/simple/three/posts/tests.py
@@ -1,4 +1,5 @@
 from django.test import TestCase
+from django.urls import reverse
 from .models import Post
 
 class PostModelTest(TestCase):
@@ -10,3 +11,18 @@ class PostModelTest(TestCase):
         post = Post.objects.get(id=1)
         expected_object_name = f'{post.text}'
         self.assertEqual(expected_object_name, 'just a test')
+        
+class HomePageViewTest(TestCase):
+    
+    def setUp(self) -> None:
+        Post.objects.create(text='this ia another test')
+        
+    def test_view_url_exist_at_proper_location(self):
+        resp = self.client.get('/')
+        self.assertEqual(resp.status_code, 200)
+        
+    def test_view_uses_correct_template(self):
+        resp = self.client.get(reverse('home'))
+        self.assertEqual(resp.status_code, 200)
+        self.assertTemplateUsed(resp, 'home.html')
+        
