28c6ee89d5471029a9d935d369c15a7b08eeab6c
golanghack <golanghack@gmail.com>
2023-02-04 21:43:58 +0300

    added new program -> django_and_drf -> simple  -> three -> posts -> test

--- django_and_drf/simple/three/posts/tests.py
+++ django_and_drf/simple/three/posts/tests.py
@@ -1,3 +1,12 @@
 from django.test import TestCase
+from .models import Post
 
-# Create your tests here.
+class PostModelTest(TestCase):
+    
+    def setUp(self) -> None:
+        Post.objects.create(text='just a test')
+        
+    def test_text_content(self) -> None:
+        post = Post.objects.get(id=1)
+        expected_object_name = f'{post.text}'
+        self.assertEqual(expected_object_name, 'just a test')
