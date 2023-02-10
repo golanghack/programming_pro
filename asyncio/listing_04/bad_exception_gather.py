f7602d2c27d3fca9ee0f40d9702dfadf9a234fa0
golanghack <golanghack@gmail.com>
2023-02-10 19:54:09 +0300

    added new program -> asyncio -> listing_04 -> bad_exception_with_gather

---
+++ asyncio/listing_04/bad_exception_gather.py
@@ -0,0 +1,15 @@
+#! /usr/bin/env python3
+
+import asyncio
+import aiohttp
+from aiohttp import ClientSession
+from util import async_timed, fetch_status
+
+async def main() -> None:
+    async with aiohttp.ClientSession() as session:
+        urls = ['https://example.com', 'python://example.com']
+        tasks = [fetch_status(session, url) for url in urls]
+        status_codes = await asyncio.gather(*tasks)
+        print(status_codes)
+        
+asyncio.run(main())
