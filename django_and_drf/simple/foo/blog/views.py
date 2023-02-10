274acfb4ded72414484ad1ff4f55080d3e29ddd9
golanghack <golanghack@gmail.com>
2023-02-10 20:35:45 +0300

    added new program -> django_and_drf -> simple -> foo -> blog -> urls, added path for new, vews added createview

--- django_and_drf/simple/foo/blog/views.py
+++ django_and_drf/simple/foo/blog/views.py
@@ -1,4 +1,5 @@
 from django.views.generic import ListView, DetailView
+from django.views.generic.edit import CreateView
 from . models import Post
 
 class BlogListView(ListView):
@@ -8,3 +9,8 @@ class BlogListView(ListView):
 class BlogDetailView(DetailView):
     model = Post 
     template_name = 'post_detail.html'
+    
+class BlogCreateView(CreateView):
+    model = Post
+    template_name = 'post_new.html'
+    fields = '__all__'
