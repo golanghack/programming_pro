9fe3475ed8ffbd2dadf0459f4d265cc9a0807866
golanghack <golanghack@gmail.com>
2023-02-10 09:29:40 +0300

    added new program -> std_lib -> date_and_time -> datetime_datetime_striptime

---
+++ tasks/middle.py
@@ -0,0 +1,11 @@
+#! /usr/bin/env python3 
+
+""" 
+TASK 
+In this exercise, you will write a program to count
+the average of all user-entered numbers. indicator
+the end of the input will serve as zero. In this case, the program must issue
+print an appropriate error message if the first typed
+the user value will be zero.
+"""
+
