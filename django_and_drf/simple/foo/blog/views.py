63acaf5f2aac2d14b286ea4cd46015f8d8f32c65
golanghack <golanghack@gmail.com>
2023-02-14 18:14:37 +0300

    added new program -> django_and_drf -> simple -> foo ->  views -> BlogDeleteView

--- django_and_drf/simple/foo/blog/views.py
+++ django_and_drf/simple/foo/blog/views.py
@@ -1,5 +1,6 @@
 from django.views.generic import ListView, DetailView
-from django.views.generic.edit import CreateView, UpdateView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
 from . models import Post
 
 class BlogListView(ListView):
@@ -19,3 +20,8 @@ class BlogUpdateView(UpdateView):
     model = Post
     fields = ['title', 'body',]
     template_name = 'post_edit.html'
+    
+class BlogdeleteView(DeleteView):
+    model = Post 
+    template_name = 'post_delete.html'
+    success_url = reverse_lazy('home')
