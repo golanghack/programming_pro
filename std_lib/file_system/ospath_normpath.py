aa551ddd69f4513717ca0593b329f35437007f18
golanghack <golanghack@gmail.com>
2023-02-17 11:52:51 +0300

    added new programm -> std_lib -> file_system -> ospath_normpath

---
+++ std_lib/file_system/ospath_normpath.py
@@ -0,0 +1,12 @@
+#! /usr/bin/env python3 
+
+import os.path
+
+PATHS = [
+    'one//two//three', 
+    'one/./two/./three', 
+    'one/../alt/two/three', 
+]
+
+for path in PATHS:
+    print(f'{path!r:>22} -> {os.path.normpath(path)!r}')
