c522d10d5fc85949146a1545657116bc88ec82da
golanghack <golanghack@gmail.com>
2023-02-17 11:36:05 +0300

    added new programm -> std_lib -> file_system -> ospath_splittexr

---
+++ std_lib/file_system/ospath_splittext.py
@@ -0,0 +1,16 @@
+#! /usr/bin/env python3 
+
+import os.path
+
+PATHS = [
+    'filename.txt', 
+    'filename',
+    '/path/to/filename.txt', 
+    '/', 
+    ''
+    'my-archive.tar.gz',
+    'no-extension.',
+]
+
+for path in PATHS:
+    print(f'{path!r:>21} -> {os.path.splitext(path)!r}')
