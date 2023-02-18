109b8777bc13d253ec14837e7856f5195ef88bbe
golanghack <golanghack@gmail.com>
2023-02-18 14:40:13 +0300

    Added new program -> a_bite_of_everything -> Vector

--- a_bite_of_everything/Vector.py
+++ a_bite_of_everything/Vector.py
@@ -8,16 +8,35 @@ class Vector:
     5.0
     >>> print(Vector(5, 12).normalizer())
     13.0
+    >>> new_one = Vector(3, 4)
+    >>> new_two = Vector(3, 6)
+    >>> print(new_one + new_two)
+    (6.00, 10.00)
     """
     
     def __init__(self, x, y) -> None:
-        self.x = x 
-        self.y = y 
+        try:
+            self.x = float(x) 
+            self.y = float(y)
+        except ValueError:
+            self.x = 0.0 
+            self.y = 0.0 
         
     def normalizer(self) -> set:
         """Return norm vectors."""
         
         return (self.x ** 2 + self.y ** 2) ** 0.5
+    
+    def __add__(self, other):
+        """Return new Vector."""
+        
+        new_vector_x = self.x + other.x 
+        new_vector_y = self.y + other.y
+        
+        return Vector(new_vector_x, new_vector_y)
+    
+    def __str__(self) -> str:
+        return f'({self.x:.2f}, {self.y:.2f})'
     
 if __name__ == '__main__':
     import doctest
