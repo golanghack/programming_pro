efd93101d22b8e53580f325337c005b971be1ec8
golanghack <golanghack@gmail.com>
2023-02-19 18:53:57 +0300

    Added new program -> tests -> simple -> core_1 -> tests -> core -> test_core

---
+++ tests/simple/core_1/tests/core/test_core.py
@@ -0,0 +1,11 @@
+#! /usr/bin/env python3 
+
+def test_regex_matching() -> None:
+    pass
+
+def test_check_options() -> None:
+    assert 0 
+    
+def test_type_checking() -> None:
+    assert 0 
+    
