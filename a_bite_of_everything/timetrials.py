637746538a7c47f7d2a4d7c955ee145a3d2455e8
golanghack <golanghack@gmail.com>
2023-02-20 17:45:52 +0300

    Added new program -> a_bite_of_everything -> timetrials

---
+++ a_bite_of_everything/timetrials.py
@@ -0,0 +1,14 @@
+#! /usr/bin/env python3 
+
+import time
+
+def timetrials(func, n_range, trials=10):
+    """Counting time for count operation any functions."""
+    
+    for i in range(trials):
+        start = time.time()
+        func(list(range(n_range)))
+        end = time.time()
+        total = end - start
+    print(f'average -> {total / trials:10.7f} = {n_range}')
+        
