f9cb177cdd890700fd0d8c5ac96632e752194c2b
golanghack <golanghack@gmail.com>
2023-02-21 18:17:47 +0300

    Added new program -> a_bite_of_everything -> sum_k_numbers, test_sum_k_numbers

---
+++ a_bite_of_everything/sum_k_numbers.py
@@ -0,0 +1,14 @@
+#! /usr/bin/env python3 
+
+import time 
+
+def sum_k_numbers(limit:int) -> float:
+    """Return full summ limit(first k) numbers."""
+    
+    start = time.time()
+    full_summ = (limit * (limit + 1) / 2)
+    end = time.time()
+    full_time = f'{(end - start):.6f} sec`s'
+    return full_summ, full_time
+print(sum_k_numbers(40))    
+    
