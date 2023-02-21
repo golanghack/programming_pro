35cde3f1346283746e229220978d8a4ae07d00e0
golanghack <golanghack@gmail.com>
2023-02-21 11:45:47 +0300

    Added new program -> std_lib -> file_system -> tempfile_SpooledTemporaryFile

---
+++ std_lib/file_system/tempfile_SpooledTemporaryFile.py
@@ -0,0 +1,12 @@
+#! /usr/bin/env python3 
+
+import tempfile
+
+with tempfile.SpooledTemporaryFile(max_size=100,
+                                   mode='w+t', 
+                                   encoding='utf-8') as temp:
+    print(f'temp -> {temp!r}')
+    
+    for i in range(3):
+        temp.write('This line is repeated over and over.\n')
+        print(temp._rolled, temp._file)
