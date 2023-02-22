d660845164db1737d942a91987e3da6e1ec92ce8
golanghack <golanghack@gmail.com>
2023-02-22 17:18:22 +0300

    Added new program -> tests -> deeper_emmersion -> unittest -> listing_01 -> REFACTORING Calc

--- tests/deeper_emersion/unittest/listing_01/Calc.py
+++ tests/deeper_emersion/unittest/listing_01/Calc.py
@@ -2,8 +2,10 @@
 
 class Calc(object):
     
-    def add(self, x, y):
-        return x + y 
+    def add(self, x: int, y: int) -> int:
+        if type(x) != int and type(y) != int:
+            raise TypeError(f'Invalid type -> {type(x)} and {type(y)}')
+        return x + y
     
 if __name__ == '__main__':
     calc = Calc()
