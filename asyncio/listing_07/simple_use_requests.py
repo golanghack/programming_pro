6aaf0e3356873493d1c4c3a21352f5c9989b0491
golanghack <golanghack@gmail.com>
2023-02-23 16:54:29 +0300

    Added new program -> asyncio -> listing_07 -> simple_use_requests

---
+++ asyncio/listing_07/simple_use_requests.py
@@ -0,0 +1,11 @@
+#! /usr/bin/env python3 
+
+import requests
+
+def get_status_code(url: str) -> int:
+    response = requests.get(url)
+    return response.status_code
+
+url = 'https://www.example.com'
+print(get_status_code(url))
+print(get_status_code(url))
