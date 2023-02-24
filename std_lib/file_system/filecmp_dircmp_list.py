19ca717c56db09d3fd21149bdc3d4417ce026d0c
golanghack <golanghack@gmail.com>
2023-02-24 12:22:13 +0300

    Added new program -> std_lib -> filesystem -> filecmp_dircmp_list

---
+++ std_lib/file_system/filecmp_dircmp_list.py
@@ -0,0 +1,11 @@
+#! /usr/bin/env python3 
+
+import filecmp 
+import pprint
+
+dc = filecmp.dircmp('example/dir1', 'example/dir2')
+print('Left -> ')
+pprint.pprint(dc.left_list)
+
+print('\nRight -> ')
+pprint.pprint(dc.right_list)
