a0523c185df58dd03d0bd08ae7686e58c90d2ca3
golanghack <golanghack@gmail.com>
2023-02-24 17:34:50 +0300

    Added new program -> tests -> simple -> deeper_emersion -> unittest -> listing_03 -> Bank

--- tests/deeper_emersion/unittest/listing_03/Bank.py
+++ tests/deeper_emersion/unittest/listing_03/Bank.py
@@ -3,3 +3,9 @@
 class Bank(object):
     def __init__(self):
         self.accounts = {}
+        
+    def add_account(self, account: str):
+        self.accounts[account.account_number] = account.balance
+        
+    def get_account_balance(self, account_number: str):
+        return self.accounts.get(account_number)
