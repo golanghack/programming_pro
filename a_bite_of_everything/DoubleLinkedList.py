5d3eed00b7e8addad984a8ec5bc38356011be2fe
golanghack <golanghack@gmail.com>
2023-02-25 18:02:06 +0300

    Added new program -> a_bite_of_everything -> DoubleLinkedList

--- a_bite_of_everything/DoubleLinkedList.py
+++ a_bite_of_everything/DoubleLinkedList.py
@@ -30,23 +30,42 @@ class DoubleLinkedList:
         self._tail = None
         self._lenght = 0 
         
-    def add_first(self, item: Any) -> None:
-        if len(self) == 0:
-            self._head = self._tail = DoubleLinkedListNode(item, None, None)
-        else:
-            new_node = DoubleLinkedListNode(item, None, self._head)
-            self._head.prev = new_node
-            self._head = new_node
-            self._lenght += 1
-    
-    def add_last(self, item: Any) -> None:
-        if len(self) == 0:
-            self._head = self._tail = DoubleLinkedListNode(item, None, None)
-        else:
-            new_node = DoubleLinkedListNode(item, self._tail, None)
-            self._tail.link = new_node
-            self._tail = new_node
-        self._lenght += 1
-        
     def __len__(self):
         return self._lenght
+    
+    def _add_between(self, item: Any, before, after):
+        node = DoubleLinkedListNode(item, before, after)
+        
+        if after is self._head:
+            self._head = node
+        if before is self._tail:
+            self._tail = node
+        self._lenght += 1
+        
+    def add_first(self, item):
+        self._add_between(item, None, self._head)
+        
+    def add_last(self, item):
+        self._add_between(item, self._tail, None)
+        
+    def _remove(self, node: DoubleLinkedListNode):
+        before, after = node.prev, node.link
+        
+        if node is self._head:
+            self._head = after
+        else:
+            before.link = after
+        
+        if node is self._tail:
+            self._tail = before
+        else:
+            after.prev = before
+        self._lenght -= 1
+        return node.data
+    
+    def remove_first(self):
+        return self._remove(self._head)
+    
+    def remove_last(self):
+        return self._remove(self._tail)
+        
