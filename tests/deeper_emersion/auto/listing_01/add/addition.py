44ef5381c3d88883a4dc0ac1ea7160c233ea4ab2
golanghack <golanghack@gmail.com>
2023-02-25 16:30:40 +0300

    Added new program -> tests/deeper_emersion/unittest -> auto -> listing_01 -> add, addition

--- tests/deeper_emersion/auto/listing_01/add/addition.py
+++ tests/deeper_emersion/auto/listing_01/add/addition.py
@@ -5,3 +5,10 @@ def addition(arg1: float, arg2: float) -> float:
     
     return arg1 + arg2
 
+def sub_addition(*args: float) -> float:
+    """Summator."""
+    
+    a1, a2 = args
+    return a1 + a2 
+
+
--- tests/deeper_emersion/auto/listing_01/test_add.py
+++ tests/deeper_emersion/auto/listing_01/test_add.py
@@ -1,12 +1,16 @@
 #! /usr/bin/env python3 
 
 import unittest
-from add.addition import addition
+from add.addition import addition, sub_addition
 
 class AdditionTestCase(unittest.TestCase):
     
-    def test_main(self):
+    def test_main_addition(self):
         result = addition(3, 2)
+        assert result == 5
+        
+    def test_main_sub_addition(self):
+        result = sub_addition(3, 2)
         assert result == 5
         
 if __name__ == '__main__':
