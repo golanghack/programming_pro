3df6e9b86aaa0b153afbca01699a4d2bb2973c9b
golanghack <golanghack@gmail.com>
2023-02-27 15:30:48 +0300

    Added new program -> tests -> deeper_emersion -> auto -> listing_02 -> ChatClient added new state property and connection.setter

--- tests/deeper_emersion/auto/listing_02/ChatClient.py
+++ tests/deeper_emersion/auto/listing_02/ChatClient.py
@@ -4,11 +4,24 @@ class ChatClient:
     
     def __init__(self, nickname: str) -> None:
         self.nickname = nickname
+        self._connection = None
         
     def send_message(self, message: str) -> str:
         sent_message = f'{self.nickname}: {message}'
         self.connection.broadcast(message)
         return sent_message
     
+    @property
+    def connection(self):
+        if self._connection is None:
+            self._connection = self._get_connection()
+        return self._connection
+    
+    @connection.setter
+    def connection(self, value: str):
+        if self._connection is not None:
+            self._connection.close()
+        self._connection = value
+        
     def _get_connection(self):
         return Connection(('localhost', 9090))
