d511be33c41abfa5d6726f688fde9205048f8910
golanghack <golanghack@gmail.com>
2023-02-28 21:11:03 +0300

    Added new program -> tests -> deeper_emerison -> auto -> listing_03 -> used dummy object in test

--- tests/deeper_emersion/auto/listing_03/TestChatClient.py
+++ tests/deeper_emersion/auto/listing_03/TestChatClient.py
@@ -2,6 +2,7 @@
 
 import unittest
 from ChatClient import ChatClient
+from connection import _DummyConnection
 
 class TestChatClient(unittest.TestCase):
     
@@ -12,8 +13,9 @@ class TestChatClient(unittest.TestCase):
         
     def test_semd_message(self):
         client = ChatClient('User 1')
+        client.connection = _DummyConnection()
         sent_message = client.send_message('Hi')
         
         assert sent_message == 'User 1: Hi'
         
-        
+unittest.main()
