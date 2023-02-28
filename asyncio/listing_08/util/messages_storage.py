cc56ee7c5deecf84f17253288ae3bbe206e6aba5
golanghack <golanghack@gmail.com>
2023-02-28 20:29:05 +0300

    Added new program -> asyncio  -> listing_08 -> messages_storage

---
+++ asyncio/listing_08/util/messages_storage.py
@@ -0,0 +1,16 @@
+#! /usr/bin/env python3 
+
+from collections import deque 
+from typing import Callable, Deque, Awaitable
+
+class MessageStore:
+    
+    def __init__(self, callback: Callable[[Deque], Awaitable[None]], max_size: int): 
+        self._deque = deque(maxlen=max_size)
+        self._callback = callback
+        
+    async def apped(self, item):
+        self._deque.append(item)
+        await self._callback(self._deque)
+        
+        
