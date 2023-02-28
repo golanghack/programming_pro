a4ed038c9647b7d7ffdee867087385b5fc79a2c2
golanghack <golanghack@gmail.com>
2023-02-28 21:06:54 +0300

    Added new program -> tests -> deeper_emerison -> auto -> listing_03 -> ChatClient added new function send_message (TDD from)

--- tests/deeper_emersion/auto/listing_03/ChatClient.py
+++ tests/deeper_emersion/auto/listing_03/ChatClient.py
@@ -1,7 +1,16 @@
 #! /usr/bin/env python3 
 
+from typing import Any
+
 class ChatClient:
     
     def __init__(self, nickname: str):
         self.nickname = nickname
+        
+    def send_message(self, message: Any) -> str:
+        sent_message = f'{self.nickname}: {str(message)}'
+        self.connection.broadcast(message)
+        return sent_message
+    
+    
         
