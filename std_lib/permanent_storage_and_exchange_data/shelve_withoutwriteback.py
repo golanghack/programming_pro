f2cab65e96bfefe7a7ffef522b868d7bf99d4c1b
golanghack <golanghack@gmail.com>
2023-02-28 18:27:17 +0300

    Added new program -> std_lib -> permanent_storage_and_exchange_data -> shelve_writeback

--- std_lib/permanent_storage_and_exchange_data/shelve_withoutwriteback.py
+++ std_lib/permanent_storage_and_exchange_data/shelve_withoutwriteback.py
@@ -6,5 +6,5 @@ with shelve.open('test_shelf.db') as shelve_file:
     print(shelve_file['key1'])
     shelve_file['key1']['new_value'] = 'this was not here before'
     
-with shelve.open('tests_shelf.db', writeback=True) as shelve_file:
+with shelve.open('test_shelf.db', writeback=True) as shelve_file:
     print(shelve_file['key1'])
