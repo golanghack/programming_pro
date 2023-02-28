a34f4434c049140f127cd7ef51761c09b545bfdb
golanghack <golanghack@gmail.com>
2023-02-28 21:25:36 +0300

    Added new program -> tests -> deeper_emerison -> auto -> listing_03 -> TestConnection added mocking m]path

--- tests/deeper_emersion/auto/listing_03/TestConnection.py
+++ tests/deeper_emersion/auto/listing_03/TestConnection.py
@@ -9,6 +9,7 @@ class TestConnection(unittest.TestCase):
     def test_broadcast(self):
         with unittest.mock.patch.object(Connection, 'connect'):
             conn = Connection(('localhost', 9090))
+        with unittest.mock.patch.object(conn, 'get_messages', return_value=[]):
             conn.broadcast('message')
             
             assert conn.get_messages()[-1] == 'message'
