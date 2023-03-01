1bb2f6ae0b00e58183d471918b058fece64a4eb5
golanghack <golanghack@gmail.com>
2023-03-01 19:39:25 +0300

    Added new program -> std_lib -> permanent_storage_and_exchange_data -> sqlite3_select_variators

--- std_lib/permanent_storage_and_exchange_data/sqlite3_select_variators.py
+++ std_lib/permanent_storage_and_exchange_data/sqlite3_select_variators.py
@@ -6,20 +6,20 @@ db_filename = 'todo.db'
 
 with sqlite3.connect(db_filename) as conn:
     cursor = conn.cursor()
-    
-    cursor.execute("""
+    """
+    cursor.execute(""
                    SELECT name, description, deadline project 
                    WHERE name = 'pymotw'
-                   """)
+                   "")
     name, description, deadline = cursor.fetchone()
     
     print(f'Project details for {description} ({name})\n due {deadline}')
-    
+    """
     cursor.execute("""
                    SELECT id, priority, details, status, deadline FROM task 
                    WHERE project = 'pymotw' ORDER BY deadline
                    """)
-    
+       
     print('\nNext 5 tasks -> ')
     for row in cursor.fetchmany(5):
         task_id, priority, details, status, deadline = row 
