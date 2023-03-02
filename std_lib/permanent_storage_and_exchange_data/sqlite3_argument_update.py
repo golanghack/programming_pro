de7768943f5da659cb167293b7942a08e15e84e1
golanghack <golanghack@gmail.com>
2023-03-02 18:46:28 +0300

    Added new program -> std_lib -> permanent_storage_and_exchange_data -> sqlite3_argument_update

---
+++ std_lib/permanent_storage_and_exchange_data/sqlite3_argument_update.py
@@ -0,0 +1,13 @@
+#! /usr/bin/env python3 
+
+import sqlite3 
+import sys 
+
+db_filename = 'todo.db'
+id_ = int(sys.argv[1])
+status = sys.argv[2]
+
+with sqlite3.connect(db_filename) as conn:
+    cursor = conn.cursor()
+    query = """update task set status = :status where id = :id"""
+    cursor.execute(query, {'status': status, 'id': id_})
