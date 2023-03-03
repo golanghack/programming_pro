4d290adbdaead58f02001dd6d90bcf795e62d18a
golanghack <golanghack@gmail.com>
2023-03-03 14:39:34 +0300

    Added new program -> asyncio -> listing_09 -> simple_asgi_app

---
+++ asyncio/listing_09/simple_asgi_app.py
@@ -0,0 +1,13 @@
+#! /usr/bin/env python3 
+
+async def app(scope, recieve, send):
+    await send({
+        'type': 'http.response.start', 
+        'status': 200, 
+        'headers': [[b'content-type', b'text/html']]
+    })
+    
+    await send({
+        'type': 'http.response.body', 
+        'body': b'ASGI'
+    })
