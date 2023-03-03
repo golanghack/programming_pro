6d33aa96ddddfb9f4a924726ba604095737457a6
golanghack <golanghack@gmail.com>
2023-03-03 17:12:17 +0300

    Added new program -> tests -> deeper_emersion -> auto -> listing_03 -> TestChatClient -> dependency injection

--- tests/deeper_emersion/auto/listing_03/TestChatClient.py
+++ tests/deeper_emersion/auto/listing_03/TestChatClient.py
@@ -12,20 +12,26 @@ class TestChatClient(unittest.TestCase):
         assert client.nickname == 'User 1'
         
     def test_send_message(self):
-        client = ChatClient('User 1')
-        client.connection = unittest.mock.Mock()
+        client = ChatClient('User 1', connection_provider=unittest.mock.Mock())
         sent_message = client.send_message('Hi')
         
         assert sent_message == 'User 1: Hi'
         
     def test_client_connection(self):
-        client = ChatClient('User 1')
-        
         connection_spy = unittest.mock.MagicMock()
-        with unittest.mock.patch.object(client, '_get_connection', 
-                                        return_value=connection_spy):
-            client.send_message('Hi')
-        
+        client = ChatClient('User 1', connection_provider=lambda *args: connection_spy)
+        client.send_message('Hi')
         connection_spy.broadcast.assert_called_with(('User 1: Hi'))
+    
+    def test_client_fetch_messages(self):
+        connection = unittest.mock.Mock()
+        connection.get_messages.return_value = ['message1', 'message2']
+        client = ChatClient('User 1', connection_provider=lambda *args: connection)
+        starting_messages = client.fetch_messages()
+        client.connection.get_messages().append('message3')
+        new_messages = client.fetch_messages()
+        
+        assert starting_messages == ['message1', 'message2']
+        assert new_messages == ['message3']
         
 unittest.main()
