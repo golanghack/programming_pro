c5f84b9b1e89fc18c4d17a2d12bb1bd09f769af9
golanghack <golanghack@gmail.com>
2023-03-04 18:56:49 +0300

    Added new program -> tests -> deeper_emerison -> auto -> listing_04 -> src -> update TODOapp, added run and prompt funcions

--- tests/deeper_emersion/auto/listing_04/src/todo/app.py
+++ tests/deeper_emersion/auto/listing_04/src/todo/app.py
@@ -5,3 +5,17 @@ class TODOapp:
     
     def __init__(self, io=(input, functools.partial(print, end=''))):
         self._in, self._out = io 
+        self._quit = False
+        
+    def run(self):
+        self._quit = False
+        while not self._quit:
+            self._out(self.prompt(''))
+            command = self._in()
+        self._out('buy!\n')
+        
+    def prompt(self, output):
+        return f""" 
+    TODOs:
+    {output}
+    > """
