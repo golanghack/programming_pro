ef8d443cfb202a9f3fdf8af2dbbc585325009a01
golanghack <golanghack@gmail.com>
2023-03-05 16:02:46 +0300

    Added new program -> std_lib -> permonent_storage_and_exchange_data -> ElementTree_find_feeds_by_tag

---
+++ std_lib/permanent_storage_and_exchange_data/ElementTree_find_feeds_by_tag.py
@@ -0,0 +1,11 @@
+#! /usr/bin/env python3 
+
+from xml.etree import ElementTree
+
+with open('podcasts.opml', 'rt', encoding='utf-8') as file_:
+    tree = ElementTree.parse(file_)
+    
+for node in tree.findall('.//outline'):
+    url = node.attrib.get('xmlUrl')
+    if url:
+        print(url)
