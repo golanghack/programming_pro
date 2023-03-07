aa4d3d95a812a1977b7b9e8736b2e04456309273
golanghack <golanghack@gmail.com>
2023-03-07 20:49:29 +0300

    Added new program -> tests -> deeper_emersion -> auto -> listing_04 -> src -> todo -> db -> load function

--- tests/deeper_emersion/auto/listing_04/src/todo/db.py
+++ tests/deeper_emersion/auto/listing_04/src/todo/db.py
@@ -6,4 +6,9 @@ class BasicDB:
         self._path = path 
         self._fileopener = _fileopener
         
-        
+    def load(self):
+        with self._fileopener(self._path, 'r', encoding='utf-8') as file_:
+            txt = file_.read()
+        return eval(txt)
+    
+    
