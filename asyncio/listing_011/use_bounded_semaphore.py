c453746a7b5bcd77a183b87751c7220ba2294f23
golanghack <golanghack@gmail.com>
2023-03-08 17:12:48 +0300

    Added new program -> asyncio -> listing_011 -> use_bounded_semaphore

---
+++ asyncio/listing_011/use_bounded_semaphore.py
@@ -0,0 +1,13 @@
+#! /usr/bin/env python3 
+
+import asyncio
+from asyncio import BoundedSemaphore
+
+async def main():
+    semaphore = BoundedSemaphore(1)
+    
+    await semaphore.acquire()
+    semaphore.release()
+    semaphore.release()
+    
+asyncio.run(main())
