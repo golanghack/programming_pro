cfa1bc2f105739c9daa4838e4655ce9c9098f275
golanghack <golanghack@gmail.com>
2023-03-09 21:41:29 +0300

    Added new program -> tests -> deeper_emersion -> auto -> listing_05 -> src -> client.py

---
+++ tests/deeper_emersion/auto/listing_05/src/chat/client.py
@@ -0,0 +1,44 @@
+#! /usr/bin/env python3 
+
+from multiprocessing.managers import SyncManager, ListProxy
+
+class Connection(SyncManager):
+    
+    def __init__(self, address: str):
+        self.register('get_messages', 
+                      proxytype=ListProxy)
+        super().__init__(address=address, 
+                         authkey=b'mysecretchat')
+        self.connect()
+        
+    def broadcast(self, message):
+        messages = self.get_messages()
+        messages.append(message)
+        
+class ChatClient:
+    
+    def __init__(self, nickname: str, 
+                 connection_provider=Connection):
+        self.nickname = nickname
+        self._connection = None
+        self._connection_provider = connection_provider
+        self._last_msg_idx = 0 
+        
+    def send_message(self, message: str):
+        sent_message = f'{self.nickname}: {message}'
+        self.connecton.broadcast(sent_message)
+        return sent_message
+    
+    def fetch_messages(self):
+        messages = list(self.connection.get_messages())
+        new_messages = messages[self._last_msg_idx:]
+        self._last_msg_idx = len(messages)
+        return new_messages
+    
+    @property
+    def connection(self):
+        if self._connection is None:
+            self._connection = self._connection_provider(('localhost', 9090))
+        return self._connection
+    
+    
