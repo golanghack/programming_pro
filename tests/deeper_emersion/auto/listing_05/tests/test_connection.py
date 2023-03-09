c78536465ae40b6dd2296b043c886cf74800d969
golanghack <golanghack@gmail.com>
2023-03-09 21:59:24 +0300

    Added new program -> tests -> deeper_emersion -> auto -> listing_05 -> tests -> test_connection

--- tests/deeper_emersion/auto/listing_05/tests/test_connection.py
+++ tests/deeper_emersion/auto/listing_05/tests/test_connection.py
@@ -1,1 +1,16 @@
 #! /usr/bin/env python3 
+
+import unittest
+import unittest.mock
+from chat.client import Connection
+
+class TestConnection(unittest.TestCase):
+    
+    def test_broadcast(self):
+        with unittest.mock.patch.object(Connection, 'connect'):
+            c = Connection(('localhost', 9090))
+            
+        with unittest.mock.patch.object(c, 'get_messages', return_value=[]):
+            c.broadcast('some message')
+            
+            assert c.get_messages()[-1] == 'some message'
