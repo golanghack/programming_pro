92c539af5fd093ab5e3f986d0fae07b9bae3136a
golanghack <golanghack@gmail.com>
2023-03-12 20:59:30 +0300

    Added new program -> dash -> intro -> added html

--- dash/listing_01/intro.py
+++ dash/listing_01/intro.py
@@ -5,7 +5,26 @@ import dash_html_components as html
 
 app = dash.Dash(__name__)
 
-app.layout = html.Div([html.H1('Good Day')])
+app.layout = html.Div([
+    html.H1('Database', 
+    style={
+        'color': 'blue', 
+        'fontSize': '45px',
+    }),
+    html.H2('The World Bank'), 
+    html.P('Key Facts -> '),
+    html.Ul([
+        html.Li('Number of Economies -> 170'), 
+        html.Li('Temporal Coverage -> 174 - 2019'), 
+        html.Li('Update Frequency -> Quartely'), 
+        html.Li('Last Updated -> March 18, 2020'),
+        html.Li([
+            'Source -> ', 
+            html.A('https://datacatalog.worldbank.org/dataset/property-and-equuty-database', 
+            href='https://datacatalog.worldbank.org/dataset/proverty-and-equity-database')
+        ])
+    ])
+])
 
 
 if __name__ == '__main__':
