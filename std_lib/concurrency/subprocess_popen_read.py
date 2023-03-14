f80340276fa086116ef9ac53309f8475e114e60d
golanghack <golanghack@gmail.com>
2023-03-14 18:05:42 +0300

    Added new program -> std_lib -> concurrency -> subprocess_popen_read

---
+++ std_lib/concurrency/subprocess_popen_read.py
@@ -0,0 +1,12 @@
+#! /usr/bin/env python3 
+
+import subprocess
+
+print('READ -> ')
+proc = subprocess.Popen(
+    ['echo', '"to stdout"'], 
+    stdout=subprocess.PIPE, 
+)
+
+stdout_value = proc.communicate()[0].decode('utf-8')
+print('stdout -> ', repr(stdout_value))
