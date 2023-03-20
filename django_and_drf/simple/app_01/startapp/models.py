78dad41c2744caf37fc43222c6a24db2ca9fe96c
golanghack <golanghack@gmail.com>
2023-03-20 20:19:06 +0300

    Added new program -> django_and_drf -> simple -> app_01 -> startapp -> models

--- django_and_drf/simple/app_01/startapp/models.py
+++ django_and_drf/simple/app_01/startapp/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+
+class AppDb(models.Model):
+
+    title = models.CharField(max_length=50)
+    content = models.TextField(null=True, blank=True)
+    price = models.FloatField(null=True, blank=True)
+    published = models.DateTimeField(auto_now_add=True, db_index=True)
