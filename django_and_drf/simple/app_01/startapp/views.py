db16a050407cfa71db03265dfae2f10d7e28c183
golanghack <golanghack@gmail.com>
2023-03-20 21:25:03 +0300

    Added new program -> django_and_drf -> simple -> app_01 -> views

--- django_and_drf/simple/app_01/startapp/views.py
+++ django_and_drf/simple/app_01/startapp/views.py
@@ -1,4 +1,9 @@
 from django.http import HttpResponse
 
-def index(request) -> str:
-    return HttpResponse('WOW')
+from .models import AppDb
+
+def index(request: str) -> str:
+    announcement_list = 'List of announcement\r\n\r\n\r\n'
+    for ann in AppDb.objects.order_by('-published'):
+        announcement_list += (ann.title + '\r\n' + ann.content + '\r\n\r\n')
+    return HttpResponse(announcement_list, content_type='text/plain; charset=utf-8')
