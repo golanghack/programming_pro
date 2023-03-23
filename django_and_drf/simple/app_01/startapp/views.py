762abb8da217eb631e8a625824d6f8297efc356a
golanghack <golanghack@gmail.com>
2023-03-23 21:32:38 +0300

    Added new program -> django_and_drf -> simple -> app_01 -> shortcuts for templates

--- django_and_drf/simple/app_01/startapp/views.py
+++ django_and_drf/simple/app_01/startapp/views.py
@@ -1,10 +1,7 @@
-from django.http import HttpResponse
-from django.template import loader
+from django.shortcuts import render
 from .models import AppDb
 
 def index(request: str) -> str:
     
-    template = loader.get_template('startapp/index.html')
     boards = AppDb.objects.order_by('-published')
-    context = {'boards': boards}
-    return HttpResponse(template.render(context, request))
+    return render(request, 'startapp/index.html', {'boards': boards})
