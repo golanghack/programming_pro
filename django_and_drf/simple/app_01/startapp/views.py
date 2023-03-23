05204f748cbbf3536fa23fe196eb8f4a0eb13e45
golanghack <golanghack@gmail.com>
2023-03-23 21:27:00 +0300

    Added new program -> django_and_drf -> simple -> app_01 -> low code for templates

--- django_and_drf/simple/app_01/startapp/views.py
+++ django_and_drf/simple/app_01/startapp/views.py
@@ -1,9 +1,10 @@
 from django.http import HttpResponse
-
+from django.template import loader
 from .models import AppDb
 
 def index(request: str) -> str:
-    announcement_list = 'List of announcement\r\n\r\n\r\n'
-    for ann in AppDb.objects.order_by('-published'):
-        announcement_list += (ann.title + '\r\n' + ann.content + '\r\n\r\n')
-    return HttpResponse(announcement_list, content_type='text/plain; charset=utf-8')
+    
+    template = loader.get_template('startapp/index.html')
+    boards = AppDb.objects.order_by('-published')
+    context = {'boards': boards}
+    return HttpResponse(template.render(context, request))
