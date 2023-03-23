d107372bd3555580701d6c53c8442e84c33ea383
golanghack <golanghack@gmail.com>
2023-03-23 18:57:35 +0300

    Added new program -> std_lib -> concurrency -> multiprocessing_condition

---
+++ std_lib/concurrency/multiprocessing_condition.py
@@ -0,0 +1,47 @@
+#! /usr/bin/env python3 
+
+import multiprocessing
+import time 
+
+def stage_1(cond):
+    """Do first stage"""
+
+    name = multiprocessing.current_process().name
+    print('Starting -> ', name)
+    with cond:
+        print(f'{name} done and ready for stage2')
+        cond.notify_all()
+
+def stage_2(cond):
+    """Wait cond finish"""
+
+    name = multiprocessing.current_process().name
+    print('Starting -> ', name)
+    with cond:
+        cond.wait()
+        print(f'{name}')
+
+if __name__ == '__main__':
+    condition = multiprocessing.Condition()
+    s1 = multiprocessing.Process(
+        name='s1', 
+        target=stage_1, 
+        args=(condition,),
+    )
+
+    s2_clients = [
+        multiprocessing.Process(
+            name=f'stage_2[{i}]', 
+            target=stage_2, 
+            args=(condition,),
+        )
+        for i in range(1, 3)
+    ]
+
+    for c in s2_clients:
+        c.start()
+        time.sleep(1)
+    s1.start()
+    s1.join()
+    for c in s2_clients:
+        c.join()
