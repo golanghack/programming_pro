bb1f11a46dcbf8a1de727b138451c262137ac34a
golanghack <golanghack@gmail.com>
2023-03-23 21:44:41 +0300

    Added new program -> django_and_drf -> simple -> app_01 -> update models -> added verbose_name and Meta

--- django_and_drf/simple/app_01/startapp/admin.py
+++ django_and_drf/simple/app_01/startapp/admin.py
@@ -1,3 +1,4 @@
 from django.contrib import admin
+from .models import AppDb
 
-# Register your models here.
+admin.site.register(AppDb)
--- django_and_drf/simple/app_01/startapp/models.py
+++ django_and_drf/simple/app_01/startapp/models.py
@@ -3,7 +3,14 @@ from django.db import models
 
 class AppDb(models.Model):
 
-    title = models.CharField(max_length=50)
-    content = models.TextField(null=True, blank=True)
-    price = models.FloatField(null=True, blank=True)
-    published = models.DateTimeField(auto_now_add=True, db_index=True)
+    title = models.CharField(max_length=50, verbose_name='Товар')
+    content = models.TextField(null=True, blank=True, verbose_name='Описание товара')
+    price = models.FloatField(null=True, blank=True, verbose_name='Цена')
+    published = models.DateTimeField(auto_now_add=True, db_index=True, verbose_name='Опубликовано')
+
+
+    class Meta:
+
+        verbose_name_plural = 'Обьявления'
+        verbose_name = 'Обьявления'
+        ordering = ['-published']
