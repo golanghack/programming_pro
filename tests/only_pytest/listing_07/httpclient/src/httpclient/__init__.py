499bd528955b44e4722fdee0f09a73dfdc778c1f
golanghack <golanghack@gmail.com>
2023-03-26 18:20:48 +0300

    Added new program -> tests -> pytest_only -> listin_07 -> httpclient -> src -> httpclient -> __init__

---
+++ tests/only_pytest/listing_07/httpclient/src/httpclient/__init__.py
@@ -0,0 +1,54 @@
+#! /usr/bin/env python3 
+
+import sys 
+import urllib.parse
+import requests
+
+class HTTPClient:
+
+    def __init__(self, url: str, requests=requests):
+        self._url = url
+        self._requests = requests
+
+    
+    def follow(self, path: str):
+        base_url = self._url
+        if not base_url.endswith('/'):
+            base_url += '/'
+        return HTTPClient(urllib.parse.urljoin(base_url, path))
+
+    def GET(self):
+        return self._requests.get(self._url).text
+
+    def POST(self, **kwargs):
+        return self._requests.post(self._url, kwargs).text
+
+    def DELETE(self):
+        return self._requests.delete(self._url).text
+
+def parse_args():
+    args = sys.argv[1:]
+    try:
+        method, url, *params = args
+    except ValueError:
+        raise ValueError('Not enough arguments, ' 'at least METHOD URL must be provided')
+
+    try:
+        params = dict((p.split('=', 1) for p in params))
+    except ValueError:
+        raise ValueError('Invalid request body parameters.'
+        'They must be in name=value format, '
+        f'not {params}')
+
+    return method.upper(), url, params
+
+
+def main():
+    try:
+        method, url, params = parse_args()
+    except ValueError as err:
+        print(err)
+        return
+
+    client = HTTPClient(url)
+    print(getattr(client, method)(**params))
