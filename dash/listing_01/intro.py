a1c5e65f12473935e35497b5e35e0dfd2ae99e8a
golanghack <golanghack@gmail.com>
2023-03-27 22:36:23 +0300

    Added new program -> dash -> intro added jupyter_notebook interactive

--- dash/listing_01/intro.py
+++ dash/listing_01/intro.py
@@ -1,51 +1,16 @@
 #! /usr/bin/env python3 
 
-import dash 
+from jupyter_dash import JupyterDash
+import dash_core_components as dcc
 import dash_html_components as html 
 import dash_bootstrap_components as dbc 
 
-dbc.Row([
-    dbc.Col('Column 1', width=2), 
-    dbc.Col('Column 2', width=5), 
-    dbc.Col('Column 3', width=4),
-])
-
-app = dash.Dash(__name__, external_stylesheets=[dbc.themes.DARKLY])
+app = JupyterDash(__name__)
 
 app.layout = html.Div([
-    html.H1('Database', 
-    style={
-        'color': 'blue', 
-        'fontSize': '45px',
-    }),
-    html.H2('The World Bank'), 
-    dbc.Tabs([
-        dbc.Tab([
-            html.Ul([
-                html.Br(), 
-                html.Li('Number of Economies -> 170'), 
-                html.Li('Temporal Coverage -> 1974 - 2019'),
-                html.Li('Update Frequency -> Quarterly'), 
-                html.Li('Last Updated -> March 18, 2020'),
-                html.Li([
-                    'Source -> ', 
-                    html.A('https://datacatalog.worldbank.org/dataset/poverty-and-equity-database',
-                                    href='https://datacatalog.worldbank.org/dataset/poverty-and-equity-database')
-                ]) 
-            ])
-        ], label='Two'), 
-        dbc.Tab([
-            html.Ul([
-                html.Br(), 
-                html.Li('Data'),
-                html.Li(['Githyb -> ', html.A('https://github.com', 
-                                href='https://github.com')
-            ])
-            ])
-        ], label='One')
-    ])
+    dcc.Dropdown(options=[{'label': color, 'value': color}
+        for color in ['blue', 'green', 'yellow']]),
+        html.Div()
 ])
-
-
 if __name__ == '__main__':
-    app.run_server(debug=True)
+    app.run_server(mode='inline')
