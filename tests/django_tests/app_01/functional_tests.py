d7e7fef9c0ab9a5913826a489045c2848560eb51
golanghack <golanghack@gmail.com>
2023-04-13 18:29:50 +0300

    Added new program -> tests -> django_tests -> app_01 -> app -> functional_tests added unittest

--- tests/django_tests/app_01/functional_tests.py
+++ tests/django_tests/app_01/functional_tests.py
@@ -1,14 +1,33 @@
 #! /usr/bin/env python3 
 
 from selenium import webdriver
+import unittest
 
-browser = webdriver.Firefox()
 
-# Bob listen about cool app
-# and open browser in localhost:8000
-browser.get('http://localhost:8000')
+class NewVisitorTest(unittest.TestCase):
+    """Test of new user."""
 
-# Bob see title of site 
-assert 'To-Do' in browser.title
+    def setUp(self):
+        """Setting."""
 
-browser.quit()
+        self.browser = webdriver.Firefox()
+
+    def tearDown(self):
+        """Remove."""
+
+        self.browser.quit()
+
+    def test_can_start_a_list_and_retrieve_it_later(self):
+        """Test
+        
+        -> may begin list and get his later.
+        """
+
+        self.browser.get('http://localhost:8000')
+
+        self.assertIn('To-Do', self.browser.title)
+        self.fail('End')
+
+if __name__ == '__main__':
+    unittest.main(warnings='ignore')
+    
