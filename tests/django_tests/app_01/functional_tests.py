090583003e3c4a371a46dc1b9a88a74d4ae1bc2d
golanghack <golanghack@gmail.com>
2023-04-14 17:03:35 +0300

    tests/django_tests/app_01/functional_tests -> added new optins -> testting posible enter element of list

--- tests/django_tests/app_01/functional_tests.py
+++ tests/django_tests/app_01/functional_tests.py
@@ -1,6 +1,9 @@
 #! /usr/bin/env python3 
 
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By 
+import time
 import unittest
 
 
@@ -26,6 +29,19 @@ class NewVisitorTest(unittest.TestCase):
         self.browser.get('http://localhost:8000')
 
         self.assertIn('To-Do', self.browser.title)
+        header_text = self.browser.find_element(By.TAG_NAME, 'h1').header_text
+        self.assertIn('To-Do', header_text)
+
+        input_box = self.browser.find_element(By.ID, 'id_new_item')
+        self.assertEqual(input_box.get_attribute('placeholder'), 'Enter a to-do item')
+
+        input_box.send_keys('Bye wins')
+        input_box.send_keys(Keys.ENTER)
+        time.sleep(1)
+
+        table = self.browser.find_element(By.ID, 'id_list_table')
+        rows = table.find_elements(By.TAG_NAME, 'tr')
+        self.assertTrue(any(row.text == '1: Bye wins' for row in rows))
         self.fail('End')
 
 if __name__ == '__main__':
