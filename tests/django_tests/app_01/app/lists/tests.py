412dc7b95e605658da3a5837204ce28d200f809f
golanghack <golanghack@gmail.com>
2023-04-14 16:19:43 +0300

    tests/django_tests/app_01/added test_home_page_returns_correct_html

--- tests/django_tests/app_01/app/lists/tests.py
+++ tests/django_tests/app_01/app/lists/tests.py
@@ -1,5 +1,6 @@
 from django.test import TestCase
 from django.urls import resolve
+from django.http import HttpRequest
 from .views import home_page 
 
 class HomePageTest(TestCase):
@@ -10,6 +11,17 @@ class HomePageTest(TestCase):
 
         found = resolve('/')
         self.assertEqual(found.func, home_page)
+
+    def test_home_page_returns_correct_html(self):
+        """Home page with correct html"""
+
+        request = HttpRequest()
+        response = home_page(request)
+        html = response.content.decode('utf8')
+
+        self.assertTrue(html.startswith('<html>'))
+        self.assertIn('<title>To-Do</title>', html)
+        self.assertTrue(html.endswith('</html>'))
 
 
 
