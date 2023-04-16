69c1ba7f1e2cd0ba7f2cfad2b6a2508eeaeaae55
golanghack <golanghack@gmail.com>
2023-04-16 19:12:03 +0300

    Added new program -> django_and_drf -> middle -> app_01 -> added functional_tests for django project

---
+++ django_and_drf/middle/app_01/functional_tests.py
@@ -0,0 +1,34 @@
+#! /usr/bin/env python3 
+
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By 
+import time
+import unittest
+
+
+class NewVisitorTest(unittest.TestCase):
+    """Test of new user."""
+
+    def setUp(self):
+        """Setting."""
+
+        self.browser = webdriver.Firefox()
+
+    def tearDown(self):
+        """Remove."""
+
+        self.browser.quit()
+
+    def test_can_start_a_list_and_retrieve_it_later(self):
+        """Test
+        
+        -> may begin list and get his later.
+        """
+
+        self.browser.get('http://localhost:8000')
+        self.assertIn('The install worked', self.browser.title)
+
+if __name__ == '__main__':
+    unittest.main(warnings='ignore')
+    
