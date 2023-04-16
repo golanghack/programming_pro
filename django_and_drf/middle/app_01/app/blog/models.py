8c5c9d5049ce81e6d5170b0c173dd1718b052586
golanghack <golanghack@gmail.com>
2023-04-16 20:17:41 +0300

    Added new program -> django_and_drf -> middle -> app_01 -> updated models -> added autor field

--- django_and_drf/middle/app_01/app/blog/models.py
+++ django_and_drf/middle/app_01/app/blog/models.py
@@ -1,11 +1,17 @@
 from django.db import models
 from django.utils import timezone
+from django.contrib.auth.models import User
 
 class Post(models.Model):
 
     class Status(models.TextChoices):
-        """Class status blog writing."""
+        """Class status blog writing.
         
+            option status -> Post.Status.choices
+            repr -> Post.Status.labels
+            row -> Post.Status.values
+        """
+
         DRAFT = 'DF', 'Draft'
         PUBLISHED = 'PB', 'Published'
 
@@ -13,13 +19,18 @@ class Post(models.Model):
     title: str = models.CharField(max_length=300)
     # VARCHAR in DB
     slug: str = models.SlugField(max_length=300)
+    author: str = models.ForeignKey(User, 
+                                    on_delete=models.CASCADE, 
+                                    related_name='blog_posts')
     # TEXT in DB
     body = models.TextField()
     # DATETIME in DB
     publish = models.DateTimeField(default=timezone.now)
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
-    status = models.CharField(max_length=2, choices=Status.choices, default=Status.DRAFT)
+    status = models.CharField(max_length=2, 
+                                choices=Status.choices, 
+                                default=Status.DRAFT)
 
     class Meta:
         ordering = ['-publish']
