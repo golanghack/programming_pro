57c0e279e85438be1569dd6ddb5d46b556481a36
golanghack <golanghack@gmail.com>
2023-04-16 20:03:21 +0300

    Added new program -> django_and_drf -> middle -> app_01 -> updated models -> added new fields -> publish, created, updated, added indexes for DB

--- django_and_drf/middle/app_01/app/blog/models.py
+++ django_and_drf/middle/app_01/app/blog/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.utils import timezone
 
 class Post(models.Model):
     # VARCHAR in BD
@@ -7,6 +8,14 @@ class Post(models.Model):
     slug: str = models.SlugField(max_length=300)
     # TEXT in DB
     body = models.TextField()
+    # DATETIME in DB
+    publish = models.DateTimeField(default=timezone.now)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
 
-    def __str__(self):
+    class Meta:
+        ordering = ['-publish']
+        indexes = [models.Index(fields=['-publish']),]
+
+    def __str__(self) -> str:
         return self.title
