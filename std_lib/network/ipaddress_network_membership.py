4123e59ee615af3123c1c3e1fa647e8ea714b6fd
golanghack <golanghack@gmail.com>
2023-04-19 19:26:06 +0300

    Added new program -> std_lib -> network -> ipaddress_network_membership

---
+++ std_lib/network/ipaddress_network_membership.py
@@ -0,0 +1,24 @@
+#! /usr/bin/env python3 
+
+import ipaddress
+
+NETWORKS = [
+    ipaddress.ip_network('10.9.0.0/24'),
+    ipaddress.ip_network('fdfd:87b5:b475:5e3e::/64'),
+]
+
+ADDRESSES = [
+    ipaddress.ip_address('10.9.0.6'),
+    ipaddress.ip_address('10.7.0.31'),
+    ipaddress.ip_address('fdfd:87b5:b475:5e3e:b1bc:e121:a8eb:14aa'),
+    ipaddress.ip_address('fe80::3840:c439:b25e:63b0'),
+]
+
+for ip in ADDRESSES:
+    for net in NETWORKS:
+        if ip in net:
+            print(f'{ip}\n is on {net}')
+            break
+    else:
+        print(f'{ip}\n is not on a known network')
+    print()
