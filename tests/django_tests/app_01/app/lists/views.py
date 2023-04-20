d12a65ef7b8053c5361719b8ad722dcfd28418ad
golanghack <golanghack@gmail.com>
2023-04-20 21:50:49 +0300

    Added new program -> tests -> django_tests -> app_01 -> app -> refactoring views -> added if/else for mthod POST

--- tests/django_tests/app_01/app/lists/views.py
+++ tests/django_tests/app_01/app/lists/views.py
@@ -2,10 +2,13 @@ from django.shortcuts import render
 from .models import Item
 
 def home_page(request):
+
+    if request.method == 'POST':
+        new_item_text = request.POST['item_text']
+        Item.objects.create(text=new_item_text)
+    else:
+        new_item_text = ''
     
-    item = Item()
-    item.text = request.POST.get('item_text', '')
-    item.save()
     return render(request, 'base.html', {
-        'new_item_text': item.text,
+        'new_item_text': new_item_text,
     })
