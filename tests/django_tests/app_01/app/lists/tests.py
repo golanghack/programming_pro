02f4b32f2c8a8883495eb21d1beb1612c55dc0d5
golanghack <golanghack@gmail.com>
2023-04-21 12:00:46 +0300

    Added new program -> tests -> django_tests -> app_01 -> app -> refactoring tests -> partians tests

--- tests/django_tests/app_01/app/lists/tests.py
+++ tests/django_tests/app_01/app/lists/tests.py
@@ -17,15 +17,18 @@ class HomePageTest(TestCase):
     def test_can_save_a_POST_request(self):
         """Test -> can save a post request"""
 
-        response = self.client.post('/', data={'item_text': 'A new list item'})
-
+        self.client.post('/', data={'item_text': 'A new list item'})
         self.assertEqual(Item.objects.count(), 1)
         new_item = Item.objects.first()
-
         self.assertEqual(new_item.text, 'A new list item')
+
+    def test_redirects_after_POST(self):
+        """Redirecting test."""
+
+        response = self.client.post('/', data={'item_text': 'A new list item'})
         self.assertEqual(response.status_code, 302)
         self.assertEqual(response['location'], '/')
-
+        
     def test_only_saves_items_when_necessary(self):
         """Only saves elemnts on needs."""
 
