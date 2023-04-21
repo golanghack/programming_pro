6315cbe26f727984275e76987a072646a42c6519
golanghack <golanghack@gmail.com>
2023-04-21 13:34:00 +0300

    Added new program -> tests -> django_tests -> app_01 -> app -> tests -> added regression

--- tests/django_tests/app_01/app/lists/views.py
+++ tests/django_tests/app_01/app/lists/views.py
@@ -5,6 +5,6 @@ def home_page(request):
 
     if request.method == 'POST':
         Item.objects.create(text=request.POST['item_text'])
-        return redirect('/')
+        return redirect('/unic_list/')
     items = Item.objects.all()
     return render(request, 'base.html', {'items': items})
