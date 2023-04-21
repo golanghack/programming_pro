3523b36d996479d562edc8de65fc113248295a0c
golanghack <golanghack@gmail.com>
2023-04-21 12:05:02 +0300

    Added new program -> tests -> django_tests -> app_01 -> app -> refactoring tests -> added test_display_all_list_items

--- tests/django_tests/app_01/app/lists/tests.py
+++ tests/django_tests/app_01/app/lists/tests.py
@@ -28,12 +28,23 @@ class HomePageTest(TestCase):
         response = self.client.post('/', data={'item_text': 'A new list item'})
         self.assertEqual(response.status_code, 302)
         self.assertEqual(response['location'], '/')
-        
+
     def test_only_saves_items_when_necessary(self):
         """Only saves elemnts on needs."""
 
         self.client.get('/')
         self.assertEqual(Item.objects.count(), 0)
+
+    def test_displays_all_list_items(self):
+        """Displays all elements of lists"""
+
+        Item.objects.create(text='item 1')
+        Item.objects.create(text='item 2')
+
+        response = self.client.get('/')
+
+        self.assertIn('item 1', response.content.decode())
+        self.assertIn('item 2', response.content.decode())
 
 
 # database
