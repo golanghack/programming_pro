34afbaf9cd029b8b0d30f41967b6b24d09323fc0
golanghack <golanghack@gmail.com>
2023-04-21 13:03:38 +0300

    Added new program -> tests -> django_tests -> app_01 -> app -> mv funtionl_tests to functional_tests/tests -> refactoring wait_for_row_in_list_table

--- tests/django_tests/app_01/app/functional_tests/tests.py
+++ tests/django_tests/app_01/app/functional_tests/tests.py
@@ -4,10 +4,11 @@ from django.test import LiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By 
+from selenium.common.exceptions import WebDriverException
 import time
 import unittest
 
-
+MAX_WAIT = 10
 class NewVisitorTest(LiveServerTestCase):
     """Test of new user."""
 
@@ -21,12 +22,20 @@ class NewVisitorTest(LiveServerTestCase):
 
         self.browser.quit()
 
-    def check_for_row_in_list_table(self, row_text):
+    def wait_for_row_in_list_table(self, row_text):
         """Checking string in table of list."""
 
-        table = self.browser.find_element(By.ID, 'id_list_table')
-        rows = table.find_elements(By.TAG_NAME, 'tr')
-        self.assertIn(row_text, [row.text for row in rows])
+        start_time = time.time()
+        while True:
+            try:
+                table = self.browser.find_element(By.ID, 'id_list_table')
+                rows = table.find_elements(By.TAG_NAME, 'tr')
+                self.assertIn(row_text, [row.text for row in rows])
+                return
+            except (AssertionError, WebDriverException) as err:
+                if time.time() - start_time > MAX_WAIT:
+                    raise err 
+                time.sleep(.5)
 
     def test_can_start_a_list_and_retrieve_it_later(self):
         """Test-> may begin list and get his later.
@@ -36,15 +45,13 @@ class NewVisitorTest(LiveServerTestCase):
         input_box = self.browser.find_element(By.ID, 'id_new_item')
         input_box.send_keys('Bye wins')
         input_box.send_keys(Keys.ENTER)
-        time.sleep(1)
-        self.check_for_row_in_list_table('1: Bye wins')
+        self.wait_for_row_in_list_table('1: Bye wins')
 
         input_box = self.browser.find_element(By.ID, 'id_new_item')
         input_box.send_keys('Make auto')
         input_box.send_keys(Keys.ENTER)
-        time.sleep(1)
 
-        self.check_for_row_in_list_table('1: Bye wins')
-        self.check_for_row_in_list_table('2: Make auto')
+        self.wait_for_row_in_list_table('1: Bye wins')
+        self.wait_for_row_in_list_table('2: Make auto')
 
     
