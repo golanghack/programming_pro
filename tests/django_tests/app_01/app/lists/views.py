35ed3e1a7e788241dfc3f7da3b89e37e7063c67f
golanghack <golanghack@gmail.com>
2023-04-21 13:45:17 +0300

    Added new program -> tests -> django_tests -> app_01 -> app -> tests -> added new view -> list_view

--- tests/django_tests/app_01/app/lists/views.py
+++ tests/django_tests/app_01/app/lists/views.py
@@ -5,6 +5,13 @@ def home_page(request):
 
     if request.method == 'POST':
         Item.objects.create(text=request.POST['item_text'])
-        return redirect('/unic_list/')
+        return redirect('/lists/unic_list/')
     items = Item.objects.all()
     return render(request, 'base.html', {'items': items})
+
+def view_list(request):
+    """View of list"""
+
+    items = Item.objects.all()
+    return render(request, 'base.html', {'items': items})
+
