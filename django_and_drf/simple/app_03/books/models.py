f566bfa1e17c212f9e1fef59e0002d22bf51b52c
golanghack <golanghack@gmail.com>
2023-04-28 21:37:29 +0300

    Added new program -> django_and_drf -> simple -> app_03 -> books -> models.py, __str__

--- django_and_drf/simple/app_03/books/models.py
+++ django_and_drf/simple/app_03/books/models.py
@@ -5,8 +5,15 @@ class Author(models.Model):
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
 
+    def __str__(self):
+        return f'{self.first_name} {self.last_name}'
+
 class Book(models.Model):
     
     title = models.CharField(max_length=100)
     authors = models.ManyToManyField(to=Author)
 
+    def __str__(self):
+        return self.title
+
+        
