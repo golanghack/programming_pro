b26fd6674547999c4edbd3b8dcdc585b3a2ec808
golanghack <golanghack@gmail.com>
2023-04-28 21:47:18 +0300

    Added new program -> django_and_drf -> simple -> app_03 -> books -> models -> added model Event

--- django_and_drf/simple/app_03/books/models.py
+++ django_and_drf/simple/app_03/books/models.py
@@ -16,4 +16,22 @@ class Book(models.Model):
     def __str__(self):
         return self.title
 
-        
+
+class Event(models.Model):
+
+    title = models.CharField(max_length=60)
+    seo_title = models.CharField(max_length=59)
+    seo_description = models.CharField(max_length=160)
+    abstract = models.CharField(max_length=160)
+    body = models.TextField(default='')
+    duration = models.IntegerField(default=0)
+    slug = models.SlugField(max_length=20)
+    start_date = models.DateTimeField()
+    end_date = models.DateTimeField()
+    price = models.IntegerField()
+    location = models.TextField(max_length=100)
+    created_at = models.DateTimeField(auto_now_add=True)
+    published = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f'{self.title}'
