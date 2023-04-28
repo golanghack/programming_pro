891078ab782466e8794d9b230d568705aa0969e7
golanghack <golanghack@gmail.com>
2023-04-28 21:52:27 +0300

    Added new program -> django_and_drf -> simple -> app_03 -> books -> tests -> test_models -> added test Event model functioonality without library

--- django_and_drf/simple/app_03/books/tests/test_models.py
+++ django_and_drf/simple/app_03/books/tests/test_models.py
@@ -1,5 +1,6 @@
 from django.test import TestCase 
-from books.models import Author, Book
+from books.models import Author, Book, Event
+from datetime import datetime
 
 class TestModels(TestCase):
 
@@ -28,3 +29,21 @@ class TestModels(TestCase):
 
         self.assertEqual(str(book), 'The Lord of the Rings.')
         self.assertEqual(str(tolkin), 'John Tolkien')
+
+    def test_event_model(self):
+
+        event = Event.objects.create(
+            title='The Lord of the Rings.'
+            seo_title='Some', 
+            seo_description='Some desc',
+            abstract='The abstract',
+            body='The body', 
+            duration=2,
+            slug='the-slug',
+            start_date=datetime.now()
+            end_date=datetime.now(),
+            price=800,
+            location='London',
+            published=False,
+        )
+        
