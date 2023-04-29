fc113d3d5f56cb0b82b1bc4fd10d6afb0c38787b
golanghack <golanghack@gmail.com>
2023-04-29 18:34:18 +0300

    Added new program -> django_and_drf -> simple -> books -> tests -> test_api -> testing authenth-icate

--- django_and_drf/simple/app_03/books/tests/test_api.py
+++ django_and_drf/simple/app_03/books/tests/test_api.py
@@ -2,6 +2,7 @@ from django.test import TestCase
 from ..models import Contact
 from django.urls import reverse
 from django.forms.models import model_to_dict
+from django.contrib.auth.models import User
 
 class TestContanctForm(TestCase):
 
@@ -24,4 +25,15 @@ class TestContanctForm(TestCase):
         response = self.client.post(reverse('contact'), data=data)
         self.assertRedirects(response, reverse('correct'))
 
-       
+
+class TestDownloadView(TestCase):
+
+    def test_anonymous_cannot_see_page(self):
+        response = self.client.get(reverse('download'))
+        self.assertRedirects(response, '/accounts/login/?next=/download/')
+
+    def test_authenticated_user_cn_see_page(self):
+        user = User.objects.create_user('Ben', 'ben@ben.com', 'pass')
+        self.client.force_login(user=user)
+        response = self.client.get(reverse('download'))
+        self.assertEqual(response.status_code, 200)
