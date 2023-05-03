edb4d3beed0d1b59ef6ed2f8504d2eef0f805555
golanghack <golanghack@gmail.com>
2023-05-03 22:03:19 +0300

    added new program -> django_and_drf -> middle -> app_01 -> app -> blog -> views -> added post_list

--- django_and_drf/middle/app_01/app/blog/views.py
+++ django_and_drf/middle/app_01/app/blog/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from .models import Post
 
-# Create your views here.
+def post_list(request: str) -> tuple:
+    posts = Post.published.all()
+    return render(request, 
+                    'blog/post/list.html',
+                    {'posts': posts,})
+
