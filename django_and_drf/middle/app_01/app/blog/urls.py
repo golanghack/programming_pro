e9e926e4e8066abcfe414e0db499349f4e32b61a
golanghack <golanghack@gmail.com>
2023-05-07 21:05:57 +0300

    added new program -> django_and_drf -> middle -> app_01 -> app -> added inluded urls from blog to app

--- django_and_drf/middle/app_01/app/blog/urls.py
+++ django_and_drf/middle/app_01/app/blog/urls.py
@@ -6,5 +6,5 @@ app_name = 'blog'
 
 urlpatterns = [
     path('', views.post_list, name='post_list'),
-    path('<int:id>/', views.post_detail, name='post_datil'),
+    path('<int:id>/', views.post_detail, name='post_datail'),
 ]
