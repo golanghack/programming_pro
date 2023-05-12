7f23f323a2bf94f8c5a2dba2d4fb5ddf11b4b6a6
golanghack <golanghack@gmail.com>
2023-05-12 20:35:18 +0300

    added new program -> django_and_drf -> app_01 -> app -> blog -> admin -> added Comment model in admin panel

--- django_and_drf/middle/app_01/app/blog/admin.py
+++ django_and_drf/middle/app_01/app/blog/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Post
+from .models import Post, Comment
 
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
@@ -29,3 +29,11 @@ class PostAdmin(admin.ModelAdmin):
         'status',
         'publish',
     ]
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+
+    list_display = ['name', 'email', 'post', 'created', 'active',]
+    list_filter = ['active', 'created', 'updated',]
+    search_fields = ['name', 'email', 'body',]
