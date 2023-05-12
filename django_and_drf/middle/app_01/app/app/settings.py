9b2da02c476ff78dc506bacf08463fc155cbf267
golanghack <golanghack@gmail.com>
2023-05-12 19:00:32 +0300

    added new program -> django_and_drf -> app_01 -> app -> settings -> added email

--- django_and_drf/middle/app_01/app/app/settings.py
+++ django_and_drf/middle/app_01/app/app/settings.py
@@ -123,3 +123,13 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+# smtp server 
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'testuser@gmail.com'
+EMAIL_HOST_PASSWORD = '123pass123'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+
+# local 
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
