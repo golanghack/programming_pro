571127febad48e99e6819ca80fe4775445fa31de
golanghack <golanghack@gmail.com>
2023-05-13 18:07:43 +0300

    added new program -> django_and_drf -> middle -> app_01 -> app -> settings added new app -> django_taggit

--- django_and_drf/middle/app_01/app/blog/models.py
+++ django_and_drf/middle/app_01/app/blog/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
 from django.urls import reverse
+from taggit.managers import TaggableManager 
 
 class PublishedManager(models.Manager):
     """Custom manager."""
@@ -56,6 +57,8 @@ class Post(models.Model):
             self.publish.day, 
             self.slug,
         ])
+
+        tags = TaggableManager()
 
 
 class Comment(models.Model):
