a75f46df2f4ea12a5f01a791ca1b0ba80058ac1c
golanghack <golanghack@gmail.com>
2023-05-15 21:36:14 +0300

    added new program -> django_and_drf -> middle -> app_01 - app -> blog -> templatetags -> blog_tags  added markdown_format

--- django_and_drf/middle/app_01/app/blog/templatetags/blog_tags.py
+++ django_and_drf/middle/app_01/app/blog/templatetags/blog_tags.py
@@ -1,5 +1,7 @@
 from django import template 
+from django.utils.safestring import mark_safe
 from django.db.models import Count
+import markdown
 from ..models import Post
 
 register = template.Library()
@@ -19,3 +21,9 @@ def get_most_commented_posts(count=5):
     return Post.published.annotate(
         total_comments = Count('comments')
     ).order_by('-total_comments')[:count]
+
+
+# filters 
+@register.filter(name='markdown')
+def markdown_format(text: str):
+    return mark_safe(markdown.markdown(text))
