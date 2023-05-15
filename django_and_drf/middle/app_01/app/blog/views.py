4f5d00bfade30dd168d61ee02f412c7b4da2b390
golanghack <golanghack@gmail.com>
2023-05-15 18:52:31 +0300

    added new program -> django_and_drf -> middle -> app_01 -> app -> blog -> views -> added tags manipulation

--- django_and_drf/middle/app_01/app/blog/views.py
+++ django_and_drf/middle/app_01/app/blog/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from django.views.decorators.http import require_POST
 from django.http import Http404
+from django.db.models import Count 
 from django.core.mail import send_mail
 from .models import Post, Comment
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
@@ -43,11 +44,18 @@ def post_detail(request: str, year: int, month: int, day: int, post: str) -> tup
     # form for comments
     form = CommentForm()
 
+    # tegs
+    post_tags_ids = post.tags.values_list('id', flat=True)
+    similar_posts = Post.published.filter(tags__in=post_tags_ids).exclude(id=post.id)
+    similar_posts = similar_posts.annotate(same_tags=Count('tags')).order_by('-same_tags', '-publish')[:4]
+
+
     return render(request, 
                     'blog/post/detail.html', 
                     {'post': post,
                     'comments': comments, 
-                    'form': form,})
+                    'form': form,
+                    'similar_posts': similar_posts,})
 
 
 def post_share(request: str, post_id: int):
