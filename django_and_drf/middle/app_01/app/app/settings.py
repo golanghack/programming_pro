2ef6900c87bbe5bdf42cd8ef82af59d93aa2f125
golanghack <golanghack@gmail.com>
2023-05-15 21:47:24 +0300

    added new program -> django_and_drf -> middle -> app_01 - app -> settings -> added site_id, sitemaps functionality

--- django_and_drf/middle/app_01/app/app/settings.py
+++ django_and_drf/middle/app_01/app/app/settings.py
@@ -15,6 +15,8 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+# Site id and sitemap
+SITE_ID = 1
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
@@ -37,6 +39,9 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    # sitemaps
+    'django.contrib.sites', 
+    'django.contrib.sitemaps',
     # local
     'blog.apps.BlogConfig',
     # downloaded '
