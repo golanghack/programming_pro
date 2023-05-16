45e9e7604c2bd7dab3846a845f0939ec7b79b7e0
golanghack <golanghack@gmail.com>
2023-05-16 22:19:42 +0300

    added new program -> django_and_drf -> middle -> app_01 -> app -> blog -> views, search -> refactoring added cong=fig for stop words in postgres

--- django_and_drf/middle/app_01/app/blog/views.py
+++ django_and_drf/middle/app_01/app/blog/views.py
@@ -3,7 +3,7 @@ from django.views.decorators.http import require_POST
 from django.http import Http404
 from django.db.models import Count 
 from django.core.mail import send_mail
-from django.contrib.postgres.search import SearchVector
+from django.contrib.postgres.search import SearchVector, SearchQuery, SearchRank
 from .models import Post, Comment
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from .forms import EmailPostForm, CommentForm, SearchForm
@@ -116,10 +116,13 @@ def post_search(request):
         form = SearchForm(request.GET)
         if form.is_valid():
             query = form.cleaned_data['query']
+            search_vector = SearchVector('title', 'body', config='russian')
+            search_query = SearchQuery(query, config='russian')
             results = Post.published.annotate(
-                search=SearchVector('title', 'body'),
-            ).filter(serch=query)
-    return render(request, 'blog/post/serach.html', 
+                search=search_vector,
+                rank=SearchRank(search_vector, search_query)
+            ).filter(search=search_query).order_by('-rank')
+    return render(request, 'blog/post/search.html', 
                             {'form': form, 
                             'query': query,
                             'results': results})
