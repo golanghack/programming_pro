e52ae2992963f2939dde94ac9c835f98dd44b136
golanghack <golanghack@gmail.com>
2023-05-20 14:25:44 +0300

    added new program -> django_and_drf -> app_01 -> app -> blog -> tests -> added new test slug lenght

--- django_and_drf/middle/app_01/app/blog/tests/test_models.py
+++ django_and_drf/middle/app_01/app/blog/tests/test_models.py
@@ -19,7 +19,7 @@ class PostTest(TestCase):
 
         super().setUpClass()
         cls.post = Post.objects.create(
-            title='one post', 
+            title='one post 1', 
             body='one post', 
             slug='one_blog',
             author=my_test_user, 
@@ -58,6 +58,20 @@ class PostTest(TestCase):
                     expected_value
                 )
         
+    def test_text_to_slug_convertion(self):
+        """From title to slug"""
 
+        post = PostTest.post
+        slug = post.slug
+        self.assertEqual(slug, 'one_blog')
+
+    
+    def test_text_slug_max_lenght_not_exceed(self):
+        """Long slug breaking and dont more max_lenght"""
+
+        post = PostTest.post
+        max_length_slug = post._meta.get_field('slug').max_length
+        lenght_slug = len(post.slug) * 37.5
+        self.assertEqual(max_length_slug, lenght_slug)
 
 
