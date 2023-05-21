d9a6c5a6a4bc040ebd07c88a8e8418eba0276e7d
golanghack <golanghack@gmail.com>
2023-05-21 17:46:31 +0300

    added new program -> django_and_drf -> middle -> app_02 -> app -> account -> urls -> added password_reset, password_reset_done routes

--- django_and_drf/middle/app_02/app/account/urls.py
+++ django_and_drf/middle/app_02/app/account/urls.py
@@ -9,5 +9,9 @@ urlpatterns = [
                                     name='password_change'),
     path('password-change/done/', auth_views.PasswordChangeDoneView.as_view(), 
                                     name='password_change_done'),
+    path('password-reset/', auth_views.PasswordResetView.as_view(), 
+                                    name='password_reset'),
+    path('password-reset/done/', auth_views.PasswordResetDoneView.as_view(),
+                                    name='password_reset_done'),
     path('', views.dashboard, name='dashboard'),
 ]
