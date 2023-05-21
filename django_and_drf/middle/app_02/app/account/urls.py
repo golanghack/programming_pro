7e9b575c93f0d784a5083842e0526fb07f008eb9
golanghack <golanghack@gmail.com>
2023-05-21 17:49:20 +0300

    added new program -> django_and_drf -> middle -> app_02 -> app -> account -> urls -> added password_reset_complete, password_reset_confirm

--- django_and_drf/middle/app_02/app/account/urls.py
+++ django_and_drf/middle/app_02/app/account/urls.py
@@ -13,5 +13,9 @@ urlpatterns = [
                                     name='password_reset'),
     path('password-reset/done/', auth_views.PasswordResetDoneView.as_view(),
                                     name='password_reset_done'),
+    path('password-reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(),
+                                    name='password_reset_confirm'),
+    path('password-reset/complete/', auth_views.PasswordResetCompleteView.as_view(), 
+                                    name='password_reset_complete'),
     path('', views.dashboard, name='dashboard'),
 ]
