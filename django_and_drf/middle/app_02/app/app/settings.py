6620a03746a521ea8c68b83c57d1a95e2745fb3a
golanghack <golanghack@gmail.com>
2023-05-21 16:21:23 +0300

    added new program -> django_and_drf -> middle -> app_02 -> app -> settings -> refactoring

--- django_and_drf/middle/app_02/app/app/settings.py
+++ django_and_drf/middle/app_02/app/app/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 from pathlib import Path
 import os 
+SETTINGS_PATH = os.path.dirname(os.path.dirname(__file__))
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -38,6 +39,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    #local
+    'account'
 ]
 
 MIDDLEWARE = [
@@ -55,7 +58,7 @@ ROOT_URLCONF = "app.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [os.path.join(BASE_DIR, 'templates')],
+        "DIRS": [os.path.join(SETTINGS_PATH, 'account', 'templates', 'account')],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
