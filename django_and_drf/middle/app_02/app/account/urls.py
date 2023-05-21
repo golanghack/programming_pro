e8a60271c5b1537c60a518592c395c5a63605dae
golanghack <golanghack@gmail.com>
2023-05-21 17:35:59 +0300

    added new program -> django_and_drf -> middle -> app_02 -> app -> account -> urls -> added password_change, password_chage_done

--- django_and_drf/middle/app_02/app/account/urls.py
+++ django_and_drf/middle/app_02/app/account/urls.py
@@ -5,5 +5,9 @@ from . import views
 urlpatterns = [
     path('login/', auth_views.LoginView.as_view(), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('password-change/', auth_views.PasswordChangeView.as_view(), 
+                                    name='password_change'),
+    path('password-change/done/', auth_views.PasswordChangeDoneView.as_view(), 
+                                    name='password_change_done'),
     path('', views.dashboard, name='dashboard'),
 ]
