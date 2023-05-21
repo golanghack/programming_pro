bf193ebbe860461beaba868c8a91db949b8efdaa
golanghack <golanghack@gmail.com>
2023-05-21 18:55:29 +0300

    added new program -> django_and_drf -> middle -> app_02 -> app -> account -> urls -> added register route

--- django_and_drf/middle/app_02/app/account/urls.py
+++ django_and_drf/middle/app_02/app/account/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', include('django.contrib.auth.urls')),
+    path('register/', views.register, name='register'),
     path('', views.dashboard, name='dashboard'),
 ]
