ba37ce97f4f411803f696da492cf4c998ac51061
golanghack <golanghack@gmail.com>
2023-05-21 18:20:15 +0300

    added new program -> django_and_drf -> middle -> app_02 -> app -> account -> forms -> added user_registration_form

--- django_and_drf/middle/app_02/app/account/forms.py
+++ django_and_drf/middle/app_02/app/account/forms.py
@@ -1,6 +1,14 @@
 from django import forms
+from django.contrib.auth.models import User
 
 class LoginForm(forms.Form):
     username = forms.CharField(label='Enter user name')
     password = forms.CharField(label='Enter password', widget=forms.PasswordInput)
 
+class UserRegistrationForm(forms.ModelForm):
+    password = forms.CharField(label='Password', widget=forms.PasswordInput)
+    password2 = forms.CharField(label='Repeat password', widget=forms.PasswordInput)
+
+    class Meta:
+        model = User
+        fields = ['username', 'first_name', 'email',]
