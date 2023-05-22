74c11f79d00490a88762dc2e9b9ed7f11e1d973c
golanghack <golanghack@gmail.com>
2023-05-22 21:51:44 +0300

    added new program -> django_and_drf -> middle -> app_02 -> app -> settings -> Authentication backends

--- django_and_drf/middle/app_02/app/app/settings.py
+++ django_and_drf/middle/app_02/app/app/settings.py
@@ -136,3 +136,9 @@ EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 # media
 MEDIA_URL = 'media/'
 MEDIA_ROOT = BASE_DIR / 'media'
+
+# custom auth
+AUTHENTICATION_BACKENDS = [
+    'django.contrib.auth.backends.ModelBackend',
+    'account.authentication.EmailAuthBackend',
+]
