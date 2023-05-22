d02fefa0edc0230675932896c0c420d47c1d47df
golanghack <golanghack@gmail.com>
2023-05-22 18:48:52 +0300

    added new program -> django_and_drf -> middle -> app_02 -> account -> forms -> added UserEditForm , ProfileEditForm

--- django_and_drf/middle/app_02/app/account/forms.py
+++ django_and_drf/middle/app_02/app/account/forms.py
@@ -1,5 +1,6 @@
 from django import forms
 from django.contrib.auth.models import User
+from .models import Profile
 
 class LoginForm(forms.Form):
     username = forms.CharField(label='Enter user name')
@@ -19,3 +20,18 @@ class UserRegistrationForm(forms.ModelForm):
         if cd['password'] != cd['password2']:
             raise forms.ValidationError('Passwords don\`t mattc')
         return cd['password2']
+
+
+class UserEditionForm(forms.ModelForm):
+
+    class Meta:
+
+        model = User
+        fields = ['first_name', 'last_name', 'email',]
+
+class ProfileEditForm(forms.ModelForm):
+
+    class Meta:
+
+        model = Profile
+        fields = ['date_of_birth', 'photo',]
