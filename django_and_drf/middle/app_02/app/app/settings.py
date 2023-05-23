297fec3b9c68d2a4d53d9aa5a4865657c340899f
golanghack <golanghack@gmail.com>
2023-05-23 18:40:23 +0300

    added new program -> django_and_drf -> middle -> app_02 -> app -> settings -> added new authenticated beckends -> facebook OAuth2

--- django_and_drf/middle/app_02/app/app/settings.py
+++ django_and_drf/middle/app_02/app/app/settings.py
@@ -144,4 +144,5 @@ MEDIA_ROOT = BASE_DIR / 'media'
 AUTHENTICATION_BACKENDS = [
     'django.contrib.auth.backends.ModelBackend',
     'account.authentication.EmailAuthBackend',
+    'social_core.backends.facebook.FacebookOAuth2',
 ]
