c0d821f4af4d86cec91f429eacc210bcbed1e08c
golanghack <golanghack@gmail.com>
2023-05-23 20:01:20 +0300

    added new program -> django_and_drf -> middle -> app_02 -> account -> authentication -> create_profile function for backends for auth social

--- django_and_drf/middle/app_02/app/account/authentication.py
+++ django_and_drf/middle/app_02/app/account/authentication.py
@@ -1,4 +1,5 @@
 from django.contrib.auth.models import User
+from .models import Profile
 
 
 class EmailAuthBackend:
@@ -24,3 +25,8 @@ class EmailAuthBackend:
                 return User.objects.get(pk=user_id)
             except User.DoesNotExist:
                 return None
+
+def create_profile(backend, user, *args, **kwargs):
+    """Create profile user for social auth"""
+
+    Profile.objects.get_or_create(user=user)
