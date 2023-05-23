c4ccc3842128a0959e0194daa6526d3c3247b4ab
golanghack <golanghack@gmail.com>
2023-05-23 19:57:31 +0300

    added new program -> django_and_drf -> middle -> app_02 -> app -> settings -> added auth pipeline

--- django_and_drf/middle/app_02/app/app/settings.py
+++ django_and_drf/middle/app_02/app/app/settings.py
@@ -149,3 +149,17 @@ AUTHENTICATION_BACKENDS = [
 
 SOCIAL_AUTH_GOOGLE_OAUTH2_KEY = '286579809115-eeoc6dpmn108e9r43mg8gn9rbps9n27k.apps.googleusercontent.com'
 SOCIAL_AUTH_GOOGLE_OAUTH2_SECRET = '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'
+
+# auth pipeline
+# docs -> https://python-social-auth.readthedocs.io/en/latest/pipeline.html
+SOCIAL_AUTH_PIPELINE = [
+'social_core.pipeline.social_auth.social_details',
+'social_core.pipeline.social_auth.social_uid',
+'social_core.pipeline.social_auth.auth_allowed',
+'social_core.pipeline.social_auth.social_user',
+'social_core.pipeline.user.get_username',
+'social_core.pipeline.user.create_user',
+'social_core.pipeline.social_auth.associate_user',
+'social_core.pipeline.social_auth.load_extra_data',
+'social_core.pipeline.user.user_details',
+]
