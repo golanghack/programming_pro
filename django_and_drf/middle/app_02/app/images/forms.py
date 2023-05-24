27452cd0e3859f465fe03a3e9d1a1fc54a76f54c
golanghack <golanghack@gmail.com>
2023-05-24 18:17:10 +0300

    added new program -> django_and_drf -> middle -> app_02 -> images -> added requests and added save functiona(standart save function updated)

--- django_and_drf/middle/app_02/app/images/forms.py
+++ django_and_drf/middle/app_02/app/images/forms.py
@@ -1,4 +1,7 @@
 from django import forms
+from django.core.files.base import ContentFile
+from django.utils.text import slugify
+import requests
 from .models import Image
 
 class ImageCreateForm(forms.ModelForm):
@@ -18,5 +21,20 @@ class ImageCreateForm(forms.ModelForm):
             raise forms.ValidationError('The given URL does not mathch valid image format.')
         
         return url 
+
+    def save(self, forse_insert: bool=False, force_update: bool=False, commit: bool=True):
+        image = super().save(commit=False)
+        image_url = self.cleaned_data['url']
+        name = slugify(image.title)
+        extension = image_url.rsplit('.', 1)[1].lower()
+        image_name = f'{name}.{extension}'
+
+        # dowmload
+        response = requests.get(image_url)
+        image.image.save(image_name, ContentFile(response.content), save=False)
+
+        if commit:
+            image.save()
+        return image
 
     
