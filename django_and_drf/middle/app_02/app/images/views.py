3c4c4cb6c871d1c1c256234885b1ce6cc2305503
golanghack <golanghack@gmail.com>
2023-05-24 18:27:55 +0300

    added new program -> django_and_drf -> middle -> app_02 -> images -> views -> added function for image create

--- django_and_drf/middle/app_02/app/images/views.py
+++ django_and_drf/middle/app_02/app/images/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
+from .forms import ImageCreateForm
 
-# Create your views here.
+@login_required()
+def image_create(request):
+    if request.method == 'POST':
+        # send
+        form = ImageCreateForm(data=request.POST)
+        if form.is_valid():
+            # OK
+            cd = form.cleaned_data
+            new_image = form.save(commit=False)
+            # current user for element
+            new_image.user = request.user
+            new_image.save()
+            messages.success(request, 'You successfully added image')
+            # redirect for detail
+            return redirect(new_image.get_absolute_url())
+    else:
+        # get method building form with data 
+        form = ImageCreateForm(data=request.GET)
+    return render(request, 'images/image/create.html', 
+                            {'section': 'images',
+                            'form': form})
