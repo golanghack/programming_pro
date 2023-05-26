953decac9d44e47329d9cc0f0d1a2d164f619df3
golanghack <golanghack@gmail.com>
2023-05-26 15:20:42 +0300

    added new program -> django_and_drf -> middle -> app_02 -> app -> account -> views -> added function user_follow

--- django_and_drf/middle/app_02/app/account/views.py
+++ django_and_drf/middle/app_02/app/account/views.py
@@ -1,12 +1,13 @@
 from django.contrib.auth.decorators import login_required
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
+from django.views.decorators.http import require_POST
 from django.contrib import messages 
 from django.shortcuts import render, get_object_or_404
 from django.contrib.auth import authenticate, login 
 from django.contrib.auth.models import User
 from .forms import LoginForm, UserRegistrationForm
 from .forms import UserEditForm, ProfileEditForm
-from .models import Profile
+from .models import Profile, Contact
 
 def register(request):
     if request.method == 'POST':
@@ -90,3 +91,26 @@ def user_detail(request, username):
     return render(request, 'account/user/detail.html',
                             {'section': 'people',
                             'user': user})               
+
+
+@require_POST
+@login_required
+def user_follow(request):
+    user_id = request.POST.get('id')
+    action = request.POST.get('action')
+
+    if user_id and action:
+        try:
+            user = User.objects.get(id=user_id)
+            if action == 'follow':
+                Contact.objects.get_or_create(
+                    user_from=request.user,
+                    user_to=user
+                )
+            else:
+                Contact.objects.filter(user_from=request.user, 
+                                            user_to=user).delete()
+            return JsonResponse({'status': 'ok',})
+        except User.DoesNotExist:
+            return JsonResponse({'status': 'error',})
+    return JsonResponse({'status': 'error',})
