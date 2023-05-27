71b5ec249af9c62f3ed4d6b9a2f7142a8594dc4c
golanghack <golanghack@gmail.com>
2023-05-27 16:52:14 +0300

    added new program -> django_and_drf -> middle -> app_02 -> actions -> models -> added model Actions

--- django_and_drf/middle/app_02/app/actions/models.py
+++ django_and_drf/middle/app_02/app/actions/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class Actions(models.Model):
+    user = models.ForeignKey('auth.User', related_name='actions', on_delete=models.CASCADE)
+    verb = models.CharField(max_length=255, verbose_name='Enter verbs')
+    created = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        indexes = [
+            models.Index(fields=['-created',]),
+        ]
+        ordering = ['-created',]
