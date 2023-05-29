60baf0d68506b46ada77219cebb0ae525feb636c
golanghack <golanghack@gmail.com>
2023-05-29 20:42:17 +0300

    added new program -> django_and_drf -> middle -> app_02 -> app -> account -> tests -> tests_urls

--- django_and_drf/middle/app_02/app/account/tests/tests_url.py
+++ django_and_drf/middle/app_02/app/account/tests/tests_url.py
@@ -1,14 +1,61 @@
 from django.test import TestCase, Client
+from django.contrib.auth import get_user_model
+from http import HTTPStatus
+from account.models import Profile, Contact
+
+User = get_user_model()
 
 class TestStaticURL(TestCase):
     def setUp(self):
         self.client = Client()
 
-    def test_home(self):
-        
-        response = self.client.get('')
-        self.assertEqual(response.status_code, 302)
+    def test_urls_correct_template_used(self) -> None:
+        templates_url: dict = {
+            '': '/account/login.html',
+        }
 
-    def test_register(self):
-        response = self.client.get('')
-        self.assertEqual(response.status_code, 302)
+        for address, template in templates_url.items():
+            with self.subTest(address=address):
+                response = self.client.get(address)
+                error_name: str = f'Error -> {address} waiting template {template}'
+
+                self.assertTemplateNotUsed(response, template, error_name)
+    
+class TestAccountURL(TestCase):
+
+    def setUp(self):
+        self.guest_client = Client()
+        self.user = User.objects.create_user(username='auth')
+        self.authorized_client = Client()
+        self.authorized_client.force_login(self.user)
+        self.profile = Profile.objects.create(user=self.user)
+        self.contact = Contact.objects.create(
+            user_from=self.user, 
+            user_to=self.user,
+        )
+
+    def test_urls_quest_client(self):
+        """NON AUTHORIZED USER PERMISSION"""
+
+        pages = ('', '/users/', '/users/follow/')
+
+        for page in pages:
+            response = self.guest_client.get(page)
+            error_name = f'Error -> permission diened for {page}'
+            self.assertEqual(response.status_code, 302, error_name)
+
+    def test_register_page_status_200(self):
+        """Register page displays for guest user""" 
+
+        register_page = '/register/'
+        response = self.guest_client.get(register_page)
+        error_name = f'Error -> dont show  {register_page}'
+        self.assertEqual(response.status_code, 200, error_name)
+
+    def test_edit_page_status_301(self):
+        """Register page displays for guest user""" 
+
+        edit_page = '/edit'
+        response = self.guest_client.get(edit_page)
+        error_name = f'Error -> dont use  {edit_page}'
+        self.assertEqual(response.status_code, 301, error_name)
