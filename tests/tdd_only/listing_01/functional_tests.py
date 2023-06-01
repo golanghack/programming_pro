f90a6b4b4e092de841b846f50282f59948e3bb75
golanghack <golanghack@gmail.com>
2023-06-01 17:17:27 +0300

    added new program -> tests -> only_tdd -> listing_01 -> refactoring functional_tests -> added Unittest

--- tests/tdd_only/listing_01/functional_tests.py
+++ tests/tdd_only/listing_01/functional_tests.py
@@ -1,8 +1,28 @@
-#! /usr/bin/env python3 
+import unittest
+from selenium.webdriver import Firefox
 
-from selenium import webdriver
+class NewTest(unittest.TestCase):
+    """Test for New Visiter""" 
 
-browser = webdriver.Firefox()
-browser.get('http://127.0.0.1:8000')
+    def setUp(self):
+        """set"""
 
-assert 'The install worked' in browser.title
+        self.browser = Firefox()
+
+    def tearDown(self):
+        """Unset""" 
+
+        self.browser.quit() 
+
+    def test_can_start_and_retrieve_it_later(self):
+        """May by will start a list app and get his later""" 
+
+        self.browser.get('http://127.0.0.1:8000')
+        
+        member = 'The install worked'
+        container = self.browser.title
+        self.assertIn(member, container)
+        self.fail('ENDED -> ')
+
+if __name__ == '__main__':
+    unittest.main(warnings='ignore')
