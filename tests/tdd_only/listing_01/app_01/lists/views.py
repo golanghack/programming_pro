15eabdcc6b01f39d3d2883c0f0fdbc706cc7d8ca
golanghack <golanghack@gmail.com>
2023-06-01 18:04:24 +0300

    added new program -> tests -> only_tdd -> listing_01 -> app_01 -> lists -> added templates/lists, home.html, added rendering template home in views

--- tests/tdd_only/listing_01/app_01/lists/views.py
+++ tests/tdd_only/listing_01/app_01/lists/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
 
 def home(request):
-    return HttpResponse('<html><title>To-do</title></html>')
+    return render(request, 'home.html')
