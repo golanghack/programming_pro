14a930ff64efed43dd5687a85b8a6109a91ec4eb
golanghack <golanghack@gmail.com>
2023-06-01 21:23:24 +0300

    added new program -> django_and_drf -> middle -> app_03 -> app -> shop -> models -> added Category, Product models

--- django_and_drf/middle/app_03/app/shop/models.py
+++ django_and_drf/middle/app_03/app/shop/models.py
@@ -1,3 +1,44 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    """Category shops""" 
+
+    name = models.CharField(max_length=200, help_text='Enter name of shop product')
+    slug = models.SlugField(max_length=200, unique=True)
+
+    class Meta:
+        ordering = ['name']
+        indexes = [
+            models.Index(fields=['name']),
+        ]
+        verbose_name = 'category'
+        verbose_name_plural = 'categories'
+
+    def __str__(self):
+        return self.name
+
+    
+class Product(models.Model):
+    category = models.ForeignKey(Category, 
+                                on_delete=models.CASCADE, 
+                                related_name='products')
+    name = models.CharField(max_length=200, help_text='Enter product name ')
+    slug = models.SlugField(max_length=200)
+    image = models.ImageField(upload_to='products/%Y/%m/%d', blank=True)
+    description = models.TextField(blank=True, help_text='Enter description of product')
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    available = models.BooleanField(default=True)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ['name']
+        indexes = [
+            models.Index(fields=['id', 'slug']),
+            models.Index(fields=['name']),
+            models.Index(fields=['-created']),
+        ]
+
+    def __str__(self):
+        return self.name 
