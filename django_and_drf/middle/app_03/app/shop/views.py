bc75378dddbbfe090713da6a0177febc6fdb25ec
golanghack <golanghack@gmail.com>
2023-06-02 21:07:10 +0300

    added new program -> django_and_drf -> middle -> app_03 -> app -> shop -> views -> added product_detail view

--- django_and_drf/middle/app_03/app/shop/views.py
+++ django_and_drf/middle/app_03/app/shop/views.py
@@ -15,3 +15,11 @@ def product_list(request, category_slug=None):
         'categories': categories,
         'products': products,
     })
+
+
+def product_detail(request, id, slug):
+    product = get_object_or_404(Product, id=id, slug=slug, available=True)
+
+    return render(request, 'shop/product/detail.html', {
+        'product': product,
+    })
