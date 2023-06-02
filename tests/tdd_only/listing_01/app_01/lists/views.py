a1343b6fdff5f2b8d1eb1e6c40c97ced3b28c8af
golanghack <golanghack@gmail.com>
2023-06-02 17:36:38 +0300

    added new program -> tests -> tdd_only -> listing_01 -> app_01 -> lists -> views -> added stupid patch for test finished

--- tests/tdd_only/listing_01/app_01/lists/views.py
+++ tests/tdd_only/listing_01/app_01/lists/views.py
@@ -2,4 +2,6 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home(request):
+    if request.method == 'POST':
+        return HttpResponse(request.POST['item_text'])
     return render(request, 'home.html')
