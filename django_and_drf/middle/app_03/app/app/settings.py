1b1e4817a92fce8361982a08eb7842d12a176e2d
golanghack <golanghack@gmail.com>
2023-06-02 21:32:11 +0300

    added new program -> django_and_drf -> middle -> app_03 -> app -> settings -> added media url and root

--- django_and_drf/middle/app_03/app/app/settings.py
+++ django_and_drf/middle/app_03/app/app/settings.py
@@ -123,3 +123,8 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+
+# seerver for media
+MEDIA_URL = 'media/'
+MEDIA_ROOT = BASE_DIR / 'media'
