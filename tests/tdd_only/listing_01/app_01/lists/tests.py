5726d82115451f98f63760e40a2ba88ee0f5373d
golanghack <golanghack@gmail.com>
2023-06-02 19:02:09 +0300

    added new program -> tests -> tdd_only -> listing_01 -> app_01 -> lists -> tests -> refactoring -> decomposition

--- tests/tdd_only/listing_01/app_01/lists/tests.py
+++ tests/tdd_only/listing_01/app_01/lists/tests.py
@@ -5,29 +5,21 @@ from lists.models import Item
 class HomeTest(TestCase):
     """Home page test"""
 
-    def test_uses_home_template(self):
-        """-> used home template""" 
-
-        response = self.client.get('/')
-        self.assertTemplateUsed(response, 'home.html')
-
     def test_can_save_a_POST_request(self):
-        """-> may be save post-request?""" 
-        
-        response = self.client.post('/', data={'item_text': 'A new list item'})
-        new_item = Item.objects.first()
+        """-> may save post"""
+
+        self.client.post('/', data={'item_text': 'A new list item'})
         self.assertEqual(Item.objects.count(), 1)
-        self.assertEqual(new_item.text, 'A new list item')
         
-        # redirecting 
+        new_item = Item.objects.first()
+        self.assertEqual(new_item, 'A new list item')
+
+    def test_redirects_after_POST(self):
+        """-> redirect after POST""" 
+
+        response = self.client.post('/', data={'item_text': 'A new list item'})
         self.assertEqual(response.status_code, 302)
         self.assertEqual(response['location'], '/')
-
-    def test_only_saves_items_when_necessary(self):
-        """-> save of items with needes""" 
-
-        self.client.get('/')
-        self.assertEqual(Item.objects.count(), 0)
 
 
 class TestModelItem(TestCase):
