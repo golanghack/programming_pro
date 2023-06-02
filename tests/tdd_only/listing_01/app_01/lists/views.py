44a9bb4c4a96fa491a8c3bec943bb1bac1d9ebeb
golanghack <golanghack@gmail.com>
2023-06-02 18:31:34 +0300

    added new program -> tests -> tdd_only -> listing_01 -> app_01 -> lists -> views -> added item

--- tests/tdd_only/listing_01/app_01/lists/views.py
+++ tests/tdd_only/listing_01/app_01/lists/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from lists.models import Item
 
 def home(request):
+    
+    item = Item()
+    item.text = request.POST.get('item_text', '')
+    item.save()
     
     return render(request, 'home.html', {
         'new_item_text': request.POST.get('item_text', ''),
