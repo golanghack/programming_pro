d1fa54d4d13ea92599cfdb9b43ee0883339ec82e
golanghack <golanghack@gmail.com>
2023-06-03 19:00:10 +0300

    added new program -> django_and_drf -> middle -> app_03 -> app -> shop -> views -> product_detail -> added cart_product_form

--- django_and_drf/middle/app_03/app/shop/views.py
+++ django_and_drf/middle/app_03/app/shop/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, get_object_or_404
+from cart.forms import CartAddProductForm
 from .models import Category, Product
 
 def product_list(request, category_slug=None):
@@ -19,7 +20,9 @@ def product_list(request, category_slug=None):
 
 def product_detail(request, id, slug):
     product = get_object_or_404(Product, id=id, slug=slug, available=True)
+    cart_product_form = CartAddProductForm()
 
     return render(request, 'shop/product/detail.html', {
         'product': product,
+        'cart_product_form': cart_product_form,
     })
