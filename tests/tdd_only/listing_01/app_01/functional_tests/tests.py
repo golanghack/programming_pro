a13ffc19e812ea187372a292f444d2a7ca44231d
golanghack <golanghack@gmail.com>
2023-06-03 17:35:50 +0300

    added new program -> tests -> tdd_only -> listing_01 -> app_01 -> functional_tests -> tests -> refactoring -> added wait_for function

--- tests/tdd_only/listing_01/app_01/functional_tests/tests.py
+++ tests/tdd_only/listing_01/app_01/functional_tests/tests.py
@@ -3,8 +3,10 @@ from django.test import LiveServerTestCase
 from selenium.webdriver import Firefox
 from selenium.webdriver.common.keys import Keys 
 from selenium.webdriver.common.by import By
+from selenium.common.exceptions import WebDriverException
 import time 
 
+MAX_WAIT = 10 
 class TestNewVisiter(LiveServerTestCase):
     """Test for New Visiter""" 
 
@@ -18,14 +20,20 @@ class TestNewVisiter(LiveServerTestCase):
 
         self.browser.quit() 
     
-    def check_for_row_in_list_table(self, member: str):
-        """String in table of list""" 
+    def wait_for_row_in_list_table(self, member: str):
+        """Waiting string in table of list""" 
 
-        table = self.browser.find_element(By.ID, 'id_list_table')
-        rows = table.find_elements(By.TAG_NAME, 'tr')
-        container = [row.text for row in rows]
-
-        self.assertIn(member, container)
+        start_time = time.time()
+        while True:
+            try:
+                table = self.browser.find_element(By.ID, 'id_list_table')
+                rows = table.find_elements(By.TAG_NAME, 'tr')
+                self.assertIn(member, [row.text for row in rows])
+                return 
+            except (AssertionError, WebDriverException) as err:
+                if time.time() - start_time > MAX_WAIT:
+                    raise err 
+                time.sleep(0.5)
 
     def test_can_start_and_retrieve_it_later(self):
         """May by will start a list app and get his later""" 
@@ -43,13 +51,13 @@ class TestNewVisiter(LiveServerTestCase):
 
         input_box.send_keys('one')
         input_box.send_keys(Keys.ENTER)
-        time.sleep(1)
-        self.check_for_row_in_list_table('1 -> one')
+        
+        self.wait_for_row_in_list_table('1 -> one')
 
         input_box = self.browser.find_element(By.ID, 'id_new_item')
         input_box.send_keys('two')
         input_box.send_keys(Keys.ENTER)
-        time.sleep(1)
-        self.check_for_row_in_list_table('1 -> one')
-        self.check_for_row_in_list_table('2 -> two')
+        
+        self.wait_for_row_in_list_table('1 -> one')
+        self.wait_for_row_in_list_table('2 -> two')
         
