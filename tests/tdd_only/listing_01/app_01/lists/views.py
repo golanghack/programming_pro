453327526c1c29609931d464563b8a77b4f3259e
golanghack <golanghack@gmail.com>
2023-06-04 17:37:36 +0300

    added new program -> tests -> only_tdd -> listing_01 -> app_01 -> lists -> views -> removed all items in home function

--- tests/tdd_only/listing_01/app_01/lists/views.py
+++ tests/tdd_only/listing_01/app_01/lists/views.py
@@ -6,7 +6,6 @@ def home(request):
     if request.method == 'POST':
         Item.objects.create(text=request.POST['item_text'])
         return redirect('/lists/one/')
-    items = Item.objects.all()
     return render(request, 'home.html', {
         'items': items,
     })
