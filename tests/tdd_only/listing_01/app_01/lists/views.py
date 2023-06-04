36c4dc744f8aa31a5208a9faafce3a027e7b3c33
golanghack <golanghack@gmail.com>
2023-06-04 16:55:15 +0300

    added new program -> tests -> tdd_only -> listing_01 -> app_01 -> lists -> views -> added new sintetic redirects

--- tests/tdd_only/listing_01/app_01/lists/views.py
+++ tests/tdd_only/listing_01/app_01/lists/views.py
@@ -5,7 +5,7 @@ def home(request):
     
     if request.method == 'POST':
         Item.objects.create(text=request.POST['item_text'])
-        return redirect('/')
+        return redirect('/lists/one/')
     items = Item.objects.all()
     return render(request, 'home.html', {
         'items': items,
