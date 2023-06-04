f491a17982685c9dcf304e59765057d2b9e313df
golanghack <golanghack@gmail.com>
2023-06-04 21:15:28 +0300

    added new program -> django_and_drf -> middle -> app_03 -> app -> orders -> forms -> OrderCreateForm

--- django_and_drf/middle/app_03/app/orders/admin.py
+++ django_and_drf/middle/app_03/app/orders/admin.py
@@ -4,7 +4,7 @@ from .models import Order, OrderItem
 class OrderItemInline(admin.TabularInline):
 
     model = OrderItem
-    raw_id_fields = ['products',]
+    raw_id_fields = ['product']
 
 @admin.register(Order)
 class OrderAdmin(admin.ModelAdmin):
