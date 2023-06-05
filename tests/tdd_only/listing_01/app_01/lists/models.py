5b82cf2b1ee530cce0ef6e012dbe930afbddf3e2
golanghack <golanghack@gmail.com>
2023-06-05 17:36:44 +0300

    added new program -> tests -> tdd_only -> listing_01 -> app_01 -> app -> lists -> models -> refactoring added my_list table in model Item

--- tests/tdd_only/listing_01/app_01/lists/models.py
+++ tests/tdd_only/listing_01/app_01/lists/models.py
@@ -5,6 +5,7 @@ class Item(models.Model):
     """ELement of list""" 
 
     text = models.TextField(default='', null=True)
+    my_list = models.TextField(default='')
 
 class List(models.Model):
     """model for list""" 
