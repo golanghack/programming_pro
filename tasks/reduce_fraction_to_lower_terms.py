44fa1efee0621b5c9ccfa292ca5e7020cfa996e2
golanghack <golanghack@gmail.com>
2023-06-05 23:04:22 +0300

    added new program -> tasks -> fractions -> dont solved

---
+++ tasks/reduce_fraction_to_lower_terms.py
@@ -0,0 +1,33 @@
+#! /usr/bin/env python3 
+
+"""  
+TASK 
+
+Write a function that takes two positive integers that represent the numerator and
+denominator of a fraction as its only parameters. The body of the function should
+reduce the fraction to lowest terms and then return both the numerator and denom-
+inator of the reduced fraction as its result. For example, if the parameters passed
+to the function are 6 and 63 then the function should return 2 and 21. Include a
+main program that allows the user to enter a numerator and denominator. Then your
+program should display the reduced fraction.
+""" 
+
+from util_for_tasks.get_number import get_number
+
+def fractions(nominator: int, denominator: int) -> tuple:
+    """Return maximum possible fraction reduce"""
+
+    switcher = True
+    while switcher:
+        if (nominator / denominator) == 1:
+            switcher = False
+            return '1/1'
+
+def main():
+    nominator = get_number('Enter nominator -> ')
+    denominator = get_number('Enter denominator -> ')
+
+    print(fractions(nominator, denominator))
+
+if __name__ == '__main__':
+    main()
