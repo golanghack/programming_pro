12c407c7cc4e3e34b3343719003663118a0c3353
golanghack <golanghack@gmail.com>
2023-06-05 16:57:34 +0300

    added new program -> tests -> tdd_only -> listing_01 -> app_01 -> app -> lists -> urls -> added new url -> new

--- tests/tdd_only/listing_01/app_01/lists/urls.py
+++ tests/tdd_only/listing_01/app_01/lists/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('lists/new', views.new_list, name='new_list'),
     path('lists/one/', views.view_list, name='view_list'),
 ]
