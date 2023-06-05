4478b55f2aebbc3c6615f53d08b236ccc4a1dc8e
golanghack <golanghack@gmail.com>
2023-06-05 18:07:22 +0300

    added new program -> tests -> tdd_only -> listing_01 -> app_01 -> app -> lists -> urls -> added new regular expressions for new urls for id

--- tests/tdd_only/listing_01/app_01/lists/urls.py
+++ tests/tdd_only/listing_01/app_01/lists/urls.py
@@ -1,8 +1,8 @@
-from django.urls import path
+from django.urls import path, re_path
 from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('lists/new', views.new_list, name='new_list'),
-    path('lists/one/', views.view_list, name='view_list'),
+    re_path('lists/new$', views.new_list, name='new_list'),
+    re_path('lists/(.+)/$', views.view_list, name='view_list'),
 ]
