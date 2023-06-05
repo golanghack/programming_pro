82da22f8df29b676391b5795ad4575b1117c526a
golanghack <golanghack@gmail.com>
2023-06-05 17:20:59 +0300

    added new program -> tests -> tdd_only -> listing_01 -> app_01 -> app -> lists -> tests -> HomeTest -> removing all_methods

--- tests/tdd_only/listing_01/app_01/lists/tests/tests.py
+++ tests/tdd_only/listing_01/app_01/lists/tests/tests.py
@@ -8,18 +8,7 @@ class HomeTest(TestCase):
     def test_can_save_a_POST_request(self):
         """-> may save post"""
 
-        self.client.post('/', data={'item_text': 'A new list item'})
-        self.assertEqual(Item.objects.count(), 1)
-        
-        new_item = Item.objects.first()
-        self.assertEqual(new_item.text, 'A new list item')
-
-    def test_redirects_after_POST(self):
-        """-> redirect after POST""" 
-
-        response = self.client.post('/', data={'item_text': 'A new list item'})
-        self.assertEqual(response.status_code, 302)
-        self.assertEqual(response['location'], '/lists/one/')
+        pass
 
 class TestModelItem(TestCase):
     """-> model item of list""" 
