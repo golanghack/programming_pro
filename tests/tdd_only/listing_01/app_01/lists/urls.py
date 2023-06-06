94c47fc73af35fb3c0eb98094ec20bdd5ecb52d2
golanghack <golanghack@gmail.com>
2023-06-06 18:46:28 +0300

    added new program -> tests -> tdd_only -> listing_01 -> app_01 -> lists -> bug fixes and moved routers in lists without includes in module

--- tests/tdd_only/listing_01/app_01/lists/urls.py
+++ tests/tdd_only/listing_01/app_01/lists/urls.py
@@ -1,8 +1,9 @@
+
 from django.urls import path, re_path, include
 from . import views as list_views 
-from . import routers as list_urls 
+from . import routers
 
 urlpatterns = [
     re_path('$', list_views.home, name='home'),
-    path('lists/', include(list_urls)),
+    path('lists/', include(routers)),
 ]
