ff106cf20565b042964b6d04f83b4019dba908ad
golanghack <golanghack@gmail.com>
2023-06-06 17:56:01 +0300

    added new program -> tests -> tdd_only -> listing_01 -> app_01 -> app -> lists -> urls -> added new url for add_item

--- tests/tdd_only/listing_01/app_01/lists/urls.py
+++ tests/tdd_only/listing_01/app_01/lists/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('', views.home, name='home'),
     re_path('lists/new$', views.new_list, name='new_list'),
     re_path('lists/(\d+)/$', views.view_list, name='view_list'),
+    re_path('lists/(\d+)/add_item$', views.add_item, name='add_item'),
 ]
