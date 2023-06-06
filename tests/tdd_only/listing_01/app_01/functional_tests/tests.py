88e3707cfa2d9758103862ae14cf753ab562614d
golanghack <golanghack@gmail.com>
2023-06-06 19:05:54 +0300

    added new program -> tests -> tdd_only -> listing_01 -> app_01 -> lists -> functional_tests -> refactoring -> added new inputs text

--- tests/tdd_only/listing_01/app_01/functional_tests/tests.py
+++ tests/tdd_only/listing_01/app_01/functional_tests/tests.py
@@ -109,6 +109,11 @@ class TestNewVisiter(LiveServerTestCase):
 
         self.browser.get(self.live_server_url)
         self.browser.set_window_size(1024, 768)
+        input_box = self.browser.find_element(By.ID, 'id_new_item')
+        input_box.send_keys('test')
+        input_box.send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table('1 -> test')
 
         input_box = self.browser.find_element(By.ID, 'id_new_item')
+
         self.assertAlmostEqual(math.floor(input_box.location['x'] + input_box.size['width']) / 2, 512, delta=10)
