57ad791a6e7a691e509d5b22ef4e4951b77af748
golanghack <golanghack@gmail.com>
2023-06-06 17:54:26 +0300

    added new program -> tests -> tdd_only -> listing_01 -> app_01 -> app -> lists -> urls -> refactoring for re_path

--- tests/tdd_only/listing_01/app_01/lists/urls.py
+++ tests/tdd_only/listing_01/app_01/lists/urls.py
@@ -4,5 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     re_path('lists/new$', views.new_list, name='new_list'),
-    re_path('lists/(.+)/$', views.view_list, name='view_list'),
+    re_path('lists/(\d+)/$', views.view_list, name='view_list'),
 ]
