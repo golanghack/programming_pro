07b8e635c263ee413cdc80ee85af6b05dc1f72a6
golanghack <golanghack@gmail.com>
2023-06-10 16:59:42 +0300

    added new program -> tests -> tdd_only -> listing_02 -> app_02 -> lists -> functional_tests -> refactoring

--- tests/tdd_only/listing_02/app_02/functional_tests/base.py
+++ tests/tdd_only/listing_02/app_02/functional_tests/base.py
@@ -1,9 +1,11 @@
 import os 
+
 from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from selenium.webdriver import Firefox
 from selenium.webdriver.common.by import By
 from selenium.common.exceptions import WebDriverException
 import time 
+
 
 MAX_WAIT = 10 
 class FunctionalTest(StaticLiveServerTestCase):
@@ -29,6 +31,18 @@ class FunctionalTest(StaticLiveServerTestCase):
                 rows = table.find_elements(By.TAG_NAME, 'tr')
                 self.assertIn(member, [row.text for row in rows])
                 return 
+            except (AssertionError, WebDriverException) as err:
+                if time.time() - start_time > MAX_WAIT:
+                    raise err 
+                time.sleep(0.5)
+
+    def wait_for(self, member: str):
+        """Waiting string in table of list""" 
+
+        start_time = time.time()
+        while True:
+            try:
+                return member()
             except (AssertionError, WebDriverException) as err:
                 if time.time() - start_time > MAX_WAIT:
                     raise err 
