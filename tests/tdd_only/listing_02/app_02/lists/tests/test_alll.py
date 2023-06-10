d5890836ccc7c4fd53503e793a7baa63fe430874
golanghack <golanghack@gmail.com>
2023-06-10 17:06:29 +0300

    added new program -> tests -> tdd_only -> listing_02 -> app_02 -> lists -> tests -> different modules tests

--- tests/tdd_only/listing_02/app_02/lists/tests/test_alll.py
+++ tests/tdd_only/listing_02/app_02/lists/tests/test_alll.py
@@ -10,38 +10,6 @@ class HomeTest(TestCase):
 
         pass
 
-class TestListAndItemModel(TestCase):
-    """-> model item of list""" 
-
-    def test_saving_and_retrieving_items(self):
-        """-> save and get element of list""" 
-
-        my_list = List()
-        my_list.save()
-
-        first_item = Item() 
-        first_item.my_list = my_list
-        first_item.text = 'One'
-        first_item.save()
-
-        second_item = Item()
-        second_item.text = 'Two'
-        second_item.my_list = my_list
-        second_item.save()
-
-        saved_items = Item.objects.all()
-        self.assertEqual(saved_items.count(), 2)
-        
-        saved_list = List.objects.first()
-        self.assertEqual(saved_list, my_list)
-
-        first_saved_item = saved_items[0]
-        second_saved_item = saved_items[1]
-        self.assertEqual(first_saved_item.text, 'One')
-        self.assertEqual(second_saved_item.text, 'Two')
-        self.assertEqual(first_saved_item.my_list, my_list)
-        self.assertEqual(second_saved_item.my_list, my_list)
-
 
 
 class ListViewTest(TestCase):
