543addff021e759e67f5c3fbda3bed9ca9c095d7
golanghack <golanghack@gmail.com>
2023-06-11 14:48:24 +0300

    added new program -> tests -> tdd_only -> listing_02 -> app_02 -> lists -> tests -> test_viewsrefasctoring

--- tests/tdd_only/listing_02/app_02/lists/tests/test_views.py
+++ tests/tdd_only/listing_02/app_02/lists/tests/test_views.py
@@ -15,6 +15,39 @@ class HomeTest(TestCase):
 class ListViewTest(TestCase):
     """Testing views of list"""
 
+    def test_can_save_a_POST_request_to_an_exiting_list(self):
+        """-> can post request in existing list""" 
+
+        other_list = List.objects.create()
+        correct_list = List.objects.create()
+        response = self.client.post(
+            f'/lists/{correct_list.id}/',
+            data={'item_text': 'A new'}
+        )
+        self.assertRedirects(response, f'lists/{correct_list.id}/')
+
+        self.client.post(
+            f'/lists/{correct_list.id}/add_item', 
+            data={'item_text': 'A new item for an existing list'}
+        )
+        self.assertEqual(Item.objects.count(), 1)
+
+        new_item = Item.objects.first()
+        self.assertEqual(new_item.text, 'A new item for an existing list')
+        self.assertEqual(new_item.my_list, correct_list)
+
+    def test_redirects_to_list_view(self):
+        """-> redirect on views for list"""
+
+        other_list = List.objects.create()
+        correct_list = List.objects.create()
+
+        response = self.client.post(
+            f'/lists/{correct_list.id}/add_item', 
+            data={'item_text': 'A new item_for an existing list'}
+        )
+        self.assertRedirects(response, f'/lists/{correct_list.id}/')
+
     def test_uses_list_template(self):
         """Use template for list""" 
 
@@ -95,34 +128,3 @@ class NewListTest(TestCase):
         self.client.post('/lists/new', data={'item_text': ''})
         self.assertEqual(List.objects.count(), 0)
         self.assertEqual(Item.objects.count(), 0)
-
-class TestNewItem(TestCase):
-    """-> new element of list""" 
-
-    def test_can_save_a_POST_request_to_an_exiting_list(self):
-        """-> can post request in existing list""" 
-
-        other_list = List.objects.create()
-        correct_list = List.objects.create()
-
-        self.client.post(
-            f'/lists/{correct_list.id}/add_item', 
-            data={'item_text': 'A new item for an existing list'}
-        )
-        self.assertEqual(Item.objects.count(), 1)
-
-        new_item = Item.objects.first()
-        self.assertEqual(new_item.text, 'A new item for an existing list')
-        self.assertEqual(new_item.my_list, correct_list)
-
-    def test_redirects_to_list_view(self):
-        """-> redirect on views for list"""
-
-        other_list = List.objects.create()
-        correct_list = List.objects.create()
-
-        response = self.client.post(
-            f'/lists/{correct_list.id}/add_item', 
-            data={'item_text': 'A new item_for an existing list'}
-        )
-        self.assertRedirects(response, f'/lists/{correct_list.id}/')
