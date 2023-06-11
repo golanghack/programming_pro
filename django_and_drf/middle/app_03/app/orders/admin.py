d0e667edbb432e68e5d7632aa8b3b8241ab61389
golanghack <golanghack@gmail.com>
2023-06-11 17:10:34 +0300

    added new program -> django_and_drf -> middle -> app_03 -> app -> orders -> admin -> added export to csv

--- django_and_drf/middle/app_03/app/orders/admin.py
+++ django_and_drf/middle/app_03/app/orders/admin.py
@@ -1,5 +1,31 @@
 from django.contrib import admin
+from django.http import HttpResponse
 from .models import Order, OrderItem
+import csv
+import datetime
+
+
+def export_to_csv(modeladmin, request, queryset):
+    opts = modeladmin.model._meta
+    content_disposition = f'attachment; filename={opts.verbose_name}.csv'
+    response = HttpResponse(content_type='text/csv')
+    response['Content-Disposition'] = content_disposition
+    writer = csv.writer(response)
+    fields = [field for field in opts.get_fields() if not field.many_to_many and not field.one_to_many]
+
+    # one line
+    writer.writerow([field.verbose_name for field in fields])
+    # write lines
+    for obj in queryset:
+        data_row = []
+        for field in fields:
+            value = getattr(obj, field.name)
+            if isinstance(value, datetime.datetime):
+                value = value.strftime('%d/%m/%Y')
+            data_row.append(value)
+        writer.writerow(data_row)
+    return response
+export_to_csv.short_description = 'Экспортировать как CSV'
 
 class OrderItemInline(admin.TabularInline):
 
@@ -12,3 +38,5 @@ class OrderAdmin(admin.ModelAdmin):
     list_display = ['id', 'first_name', 'last_name', 'email', 'paid', 'created', 'updated',]
     list_filter = ['paid', 'created', 'updated',]
     inlines = [OrderItemInline]
+    actions = [export_to_csv]
+
