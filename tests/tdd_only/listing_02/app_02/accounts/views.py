1a3c64f7a48f9d1c5d6872abcc6690957543184f
golanghack <golanghack@gmail.com>
2023-06-12 14:59:22 +0300

    added new program -> tests -> tdd_only -> listing_02 -> app_02 -> accounts -> views -> login

--- tests/tdd_only/listing_02/app_02/accounts/views.py
+++ tests/tdd_only/listing_02/app_02/accounts/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.core.mail import send_mail
+from django.contrib.auth import authenticate
+from django.contrib.auth import login as auth_login
 import uuid
 import sys 
 from accounts.models import Token 
@@ -16,4 +18,13 @@ def send_login_email(request):
                     'noreply@superuser', [email],)
     return render(request, 'login_email_sent.html')
 
-    
+
+def login(request):
+    """-> register in system""" 
+
+    print('login view', file=sys.stderr)
+    uid = request.GET.get('uid')
+    user = authenticate(uid=uid)
+    if user is not None:
+        auth_login(request, user)
+    return redirect('/')
