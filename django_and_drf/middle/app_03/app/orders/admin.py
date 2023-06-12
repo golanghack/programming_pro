047ee95d7850fcd2f65b1f3e1300ee033fe40394
golanghack <golanghack@gmail.com>
2023-06-12 17:21:49 +0300

    added new program -> django_end_drf -> middle -> app_03 -> app -> orders -> admin -> added order_pdf

--- django_and_drf/middle/app_03/app/orders/admin.py
+++ django_and_drf/middle/app_03/app/orders/admin.py
@@ -38,10 +38,17 @@ def order_detail(obj):
     url = reverse('orders:admin_order_detail', args=[obj.id])
     return mark_safe(f'<a href="{url}">Представление</a>')
 
+def order_pdf(obj):
+    url = reverse('orders:admin_order_pdf', args=[obj.id])
+    return mark_safe(f'<a href="{url}">PDF</a>')
+order_pdf.short_description = 'Invoice'
+
 @admin.register(Order)
 class OrderAdmin(admin.ModelAdmin):
 
-    list_display = ['id', 'first_name', 'last_name', 'email', 'paid', 'created', 'updated',order_detail]
+    list_display = ['id', 'first_name', 
+                    'last_name', 'email', 'paid', 'created', 
+                    'updated',order_detail, order_pdf]
     list_filter = ['paid', 'created', 'updated',]
     inlines = [OrderItemInline]
     actions = [export_to_csv]
