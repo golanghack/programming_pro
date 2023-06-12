f448c82ce25a154a7a04bb90cbbdb4381450c317
golanghack <golanghack@gmail.com>
2023-06-12 13:07:09 +0300

    added new program -> tests -> tdd_only -> listing_02 -> app_02 -> lists -> tests -> test_forms added ItemFormTest

---
+++ tests/tdd_only/listing_02/app_02/lists/tests/test_forms.py
@@ -0,0 +1,11 @@
+from django.test import TestCase
+from lists.models import ItemForm
+
+class ItemFormTest(TestCase):
+    """-> form test for element of list"""
+
+    def test_form_renders_item_text_input(self):
+        """-> form show text input"""
+
+        form = ItemForm()
+        self.fail(form.as_p())
