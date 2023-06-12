8cb8356f4809b23d45b401b79fd706b3d4f25375
golanghack <golanghack@gmail.com>
2023-06-12 15:09:26 +0300

    added new program -> tests -> tdd_only -> listing_02 -> app_02 -> accounts -> urls -> added logout, login

--- tests/tdd_only/listing_02/app_02/accounts/urls.py
+++ tests/tdd_only/listing_02/app_02/accounts/urls.py
@@ -3,4 +3,6 @@ from accounts import views
 
 urlpatterns = [
     re_path('^send_email$', views.send_login_email, name='send_login_email'),
+    re_path('^login$', views.login, name='login'),
+    re_path('^logout$', views.logout, name='logout'),
 ]
