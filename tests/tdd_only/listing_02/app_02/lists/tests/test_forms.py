e5f7bd81499ed1679833ae775f918d009694f621
golanghack <golanghack@gmail.com>
2023-06-12 13:15:01 +0300

    added new program -> tests -> tdd_only -> listing_02 -> app_02 -> lists -> tests -> test_forms -> added function test_form_item_input_has_placeholder_and_css_classes

--- tests/tdd_only/listing_02/app_02/lists/tests/test_forms.py
+++ tests/tdd_only/listing_02/app_02/lists/tests/test_forms.py
@@ -9,3 +9,10 @@ class ItemFormTest(TestCase):
 
         form = ItemForm()
         self.fail(form.as_p())
+
+    def test_form_item_input_has_placeholder_and_css_classes(self):
+        """-> field input have attr placeholder and css clsses"""
+
+        form = ItemForm()
+        self.assertIn('placeholder="Enter a to-do"', form.as_p())
+        self.assertIn('class="form-control input-lg"', form.as_p())
