4213be6e071d1443a2519ce7d96f00846f026bb9
golanghack <golanghack@gmail.com>
2023-06-13 18:15:48 +0300

    added new program -> django_and_drf -> middle -> app_03 -> app -> coupons -> models -> added model Coupon and migrations

--- django_and_drf/middle/app_03/app/coupons/models.py
+++ django_and_drf/middle/app_03/app/coupons/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
 
-# Create your models here.
+class Coupon(models.Model):
+    """Coupon model"""
+
+    code = models.CharField(max_length=50, unique=True)
+    valid_from = models.DateTimeField()
+    valid_to = models.DateTimeField()
+    discount = models.IntegerField(validators=[MinValueValidator(0), MaxValueValidator(100)],
+                                    help_text='Процент скидки (от 0 до 100)')
+    active = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.code
+        
