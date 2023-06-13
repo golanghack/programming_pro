af5538e5d04e838b1f3c76da3203a8425875e469
golanghack <golanghack@gmail.com>
2023-06-13 15:02:33 +0300

    added new program -> tests -> tdd_only -> listing_02 -> app_02 -> accounts -> tests -> test_views -> added test SendLoginEmailViewTest

---
+++ tests/tdd_only/listing_02/app_02/accounts/tests/test_views.py
@@ -0,0 +1,10 @@
+from django.test import TestCase
+
+class SendLoginEmailViewTest(TestCase):
+    """-> view for send message for into""" 
+
+    def test_redirects_to_home_page(self):
+        """-> redirects on home page""" 
+
+        response = self.client.post('/accounts/send_login_email', data={'email': 'test@test.com'})
+        self.assertRedirects(response, '/')
