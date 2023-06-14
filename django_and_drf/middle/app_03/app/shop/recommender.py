59e71597a8958e1aafa5f3e64b0b6e1ad7db2e0b
golanghack <golanghack@gmail.com>
2023-06-14 19:44:00 +0300

    added new program -> django_nad_drf -> middle -> app_03 -> app -> shop -> recommender update

--- django_and_drf/middle/app_03/app/shop/recommender.py
+++ django_and_drf/middle/app_03/app/shop/recommender.py
@@ -48,4 +48,7 @@ class Recommender:
         suggested_products = list(Product.objects.filter(id__in=suggested_products_ids))
         suggested_products.sort(key=lambda x: suggested_products_ids.index(x.id))
         return suggested_products
-        
+    
+    def clear_purchases(self):
+        for id in Product.objects.values_list('id', flat=True):
+            r.delete(self.get_product_key(id))
