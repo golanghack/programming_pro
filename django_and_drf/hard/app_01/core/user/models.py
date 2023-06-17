2663bfb07271b2101bda2ad0210942cb8fa78c90
golanghack <golanghack@gmail.com>
2023-06-17 17:00:06 +0300

    added new program -> django_and_drf -> hard -> app_01 -> app -> core -> user -> models -> added UserManager model

--- django_and_drf/hard/app_01/core/user/models.py
+++ django_and_drf/hard/app_01/core/user/models.py
@@ -5,6 +5,49 @@ from django.db import models
 from django.http import Http404
 
 
+class UserManager(BaseUserManager):
+    """-> custom managing users""" 
+
+    def get_object_by_public_id(self, public_id: int):
+        try:
+            instance = self.get(public_id=public_id)
+            return instance
+        except (ObjectDoesNotExist, ValueError, TypeError):
+            return Http404
+    
+    def create_user(self, username: str, email: str, password: str=None, **kwargs):
+        """Create and return a 'User' with email, phone number, username and password""" 
+
+        if username is None:
+            raise TypeError('Users must be a username')
+        if email is None:
+            raise TypeError('Users must be a emaul')
+        if password is None:
+            raise TypeError('Users must be pass')
+
+        user = self.model(username=username, email=self.normalize_email(email), **kwargs)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user 
+
+    def create_superuser(self, username: str, email: str, password: str, **kwargs):
+        """Creating and return a 'User' with superuser premis-s"""
+
+        if password is None:
+            raise TypeError('Superuser must have a pass-d')
+        if email is None:
+            raise TypeError('Superuser must have an email')
+        if username is None:
+            raise TypeError('superuser must have an username')
+        user = self.create_user(username, email, password, **kwargs)
+        user.is_superuser = True
+        user.is_staff = True
+        user.save(using=self._db)
+        return user 
+
+
+
+
 class User(AbstractBaseUser, PermissionsMixin):
     """Model for user"""
 
@@ -19,7 +62,7 @@ class User(AbstractBaseUser, PermissionsMixin):
     updated = models.DateTimeField(auto_now_add=True)
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = ['username']
-    objects = BaseUserManager()
+    objects = UserManager()
     
     def __str__(self):
         return f'{self.email}'
