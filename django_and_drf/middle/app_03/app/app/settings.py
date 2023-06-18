a84d742255db28a2ec9384ddae410d5aaed33563
golanghack <golanghack@gmail.com>
2023-06-18 17:18:01 +0300

    added new program -> django_and_drf -> middle -> app_03 ->app -> install and added rosetta app

--- django_and_drf/middle/app_03/app/app/settings.py
+++ django_and_drf/middle/app_03/app/app/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    # downloaded 
+    "rosetta",
     # local
     "shop.apps.ShopConfig",
     "cart.apps.CartConfig",
