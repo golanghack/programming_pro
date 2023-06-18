be7a1cde84abed164a3bab4b3e30bdf3e432ebf5
golanghack <golanghack@gmail.com>
2023-06-18 18:15:21 +0300

    added new program -> django_and_drf -> middle -> app_03 ->app -> shop, orders -> translated models

--- django_and_drf/middle/app_03/app/shop/models.py
+++ django_and_drf/middle/app_03/app/shop/models.py
@@ -1,18 +1,18 @@
 from django.db import models
 from django.urls import reverse
+from parler.models import TranslatableModel, TranslatedFields
 
 
-class Category(models.Model):
+class Category(TranslatableModel):
     """Category shops""" 
 
-    name = models.CharField(max_length=200, help_text='Enter name of shop product')
+    transplations = TranslatedFields(
+    name = models.CharField(max_length=200, help_text='Enter name of shop product'),
     slug = models.SlugField(max_length=200, unique=True)
+    )
 
     class Meta:
-        ordering = ['name']
-        indexes = [
-            models.Index(fields=['name']),
-        ]
+
         verbose_name = 'category'
         verbose_name_plural = 'categories'
 
@@ -23,24 +23,23 @@ class Category(models.Model):
         return reverse('shop:product_list_by_category', args=[self.slug])
 
     
-class Product(models.Model):
+class Product(TranslatableModel):
+    translations = TranslatedFields(
     category = models.ForeignKey(Category, 
                                 on_delete=models.CASCADE, 
-                                related_name='products')
-    name = models.CharField(max_length=200, help_text='Enter product name ')
-    slug = models.SlugField(max_length=200)
-    image = models.ImageField(upload_to='products/%Y/%m/%d', blank=True)
-    description = models.TextField(blank=True, help_text='Enter description of product')
-    price = models.DecimalField(max_digits=10, decimal_places=2)
-    available = models.BooleanField(default=True)
-    created = models.DateTimeField(auto_now_add=True)
-    updated = models.DateTimeField(auto_now=True)
+                                related_name='products'),
+    name = models.CharField(max_length=200, help_text='Enter product name '),
+    slug = models.SlugField(max_length=200),
+    image = models.ImageField(upload_to='products/%Y/%m/%d', blank=True),
+    description = models.TextField(blank=True, help_text='Enter description of product'),
+    price = models.DecimalField(max_digits=10, decimal_places=2),
+    available = models.BooleanField(default=True),
+    created = models.DateTimeField(auto_now_add=True),
+    updated = models.DateTimeField(auto_now=True),
+    )
 
     class Meta:
-        ordering = ['name']
         indexes = [
-            models.Index(fields=['id', 'slug']),
-            models.Index(fields=['name']),
             models.Index(fields=['-created']),
         ]
 
