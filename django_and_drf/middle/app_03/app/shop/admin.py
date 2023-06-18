5a8a4b3d426c5ef76ce6ca37bcd5df9d3d8e4886
golanghack <golanghack@gmail.com>
2023-06-18 18:23:06 +0300

    added new program -> django_and_drf -> middle -> app_03 ->app -> integrated django-parler in admin site

--- django_and_drf/middle/app_03/app/shop/admin.py
+++ django_and_drf/middle/app_03/app/shop/admin.py
@@ -1,14 +1,17 @@
 from django.contrib import admin
+from parler.admin import TraslatableAdmin
 from .models import Category, Product
 
 @admin.register(Category)
-class CategoryAdmin(admin.ModelAdmin):
+class CategoryAdmin(TraslatableAdmin):
     list_display = ['name', 'slug']
-    prepopulated_fields = {'slug': ('name',)}
+    def get_prepopulated_fields(self, request, obj=None):
+        return {'slug': ('name',)}
 
 @admin.register(Product)
-class ProductAdmin(admin.ModelAdmin):
+class ProductAdmin(TraslatableAdmin):
     list_display = ['name', 'slug', 'price', 'available', 'created', 'updated']
     list_filter = ['available', 'created', 'updated']
     list_editable = ['price', 'available']
-    prepopulated_fields = {'slug': ('name',)}
+    def get_prepopulated_fields(self, request, obj=None):
+        return {'slug': ('name',)}
