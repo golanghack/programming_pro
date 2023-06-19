6a19717fdef3cafb474725f25bfc2050fc51575b
golanghack <golanghack@gmail.com>
2023-06-19 18:30:09 +0300

    added new program -> django_and_drf -> hard -> app_01 -> app -> core -> user -> vieesets -> added permissions IsAuthenticated

--- django_and_drf/hard/app_01/core/user/viewsets.py
+++ django_and_drf/hard/app_01/core/user/viewsets.py
@@ -1,4 +1,4 @@
-from rest_framework.permissions import AllowAny
+from rest_framework.permissions import IsAuthenticated
 from rest_framework import viewsets
 from core.user.serializers import UserSerializer
 from core.user.models import User
@@ -8,7 +8,7 @@ class UserViewSet(viewsets.ModelViewSet):
     """-> user view set from model User""" 
 
     http_method_names = ('patch', 'get')
-    permission_classes = (AllowAny,)
+    permission_classes = (IsAuthenticated,)
     serializer_class = UserSerializer
 
     def get_queryset(self):
