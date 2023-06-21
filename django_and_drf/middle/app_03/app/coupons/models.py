316f8288ed5025632d155a453730fb63790f717e
golanghack <golanghack@gmail.com>
2023-06-21 20:32:53 +0300

    added new program -> django_and_drf -> middle -> app_03 -> added tests in coupons

--- django_and_drf/middle/app_03/app/coupons/models.py
+++ django_and_drf/middle/app_03/app/coupons/models.py
@@ -5,10 +5,10 @@ class Coupon(models.Model):
     """Coupon model"""
 
     code = models.CharField(max_length=50, unique=True)
-    valid_from = models.DateTimeField()
-    valid_to = models.DateTimeField()
+    valid_from = models.DateTimeField(auto_now_add=True)
+    valid_to = models.DateTimeField(auto_now=True)
     discount = models.IntegerField(validators=[MinValueValidator(0), MaxValueValidator(100)],
-                                    help_text='Процент скидки (от 0 до 100)')
+                                    help_text='Процент скидки (от 0 до 100)', default=0, null=True)
     active = models.BooleanField(default=False)
 
     def __str__(self):
---
+++ django_and_drf/middle/app_03/app/orders/tests/functional_tests.py
@@ -0,0 +1,29 @@
+from selenium import webdriver
+import unittest
+
+
+class NewVisitorTest(unittest.TestCase):
+    """Test of new user."""
+
+    def setUp(self):
+        """Setting."""
+
+        self.browser = webdriver.Firefox()
+
+    def tearDown(self):
+        """Remove."""
+
+        self.browser.quit()
+
+    def test_home_page(self):
+        """Test
+        
+        -> may begin list and get his later.
+        """
+
+        self.browser.get('http://localhost:8000/en/cart')
+        self.assertIn('Ваша корзина', self.browser.title)
+
+
+if __name__ == '__main__':
+    unittest.main(warnings='ignore')
