aec6b29b357235ecc571dabc60403f3bf52f158b
golanghack <golanghack@gmail.com>
2023-06-21 18:54:04 +0300

    added new program -> django_and_drf -> middle -> app_03 -> added tests in cart app

--- django_and_drf/hard/app_01/core/routers.py
+++ django_and_drf/hard/app_01/core/routers.py
@@ -1,10 +1,12 @@
 
 from rest_framework import routers
 from core.user.viewsets import UserViewSet
+from core.auth.viewsets.register import RegisterViewSet
 
 router = routers.SimpleRouter()
 # USer
-router.register(r'user', UserViewSet, basename='user')
+router.register(r'api/user', UserViewSet, basename='user')
+router.register(r'api/auth/register', RegisterViewSet, basename='auth-register')
 
 urlpatterns = [
     *router.urls,
---
+++ django_and_drf/middle/app_03/app/cart/tests/__init__.py
@@ -0,0 +1,4 @@
+from .test_models import *
+from .functional_tests import * 
+from .test_views import * 
+from .test_urls import * 
