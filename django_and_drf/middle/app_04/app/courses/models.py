05e1d63786c4cf22170970b71a2bafad4b057649
golanghack <golanghack@gmail.com>
2023-06-22 21:22:22 +0300

    added new program -> django_and_drf -> middle -> app_04 -> app -> courses -> models -> added Subject model

--- django_and_drf/middle/app_04/app/courses/models.py
+++ django_and_drf/middle/app_04/app/courses/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Subject(models.Model):
+    """-> model Subject for education cms platform""" 
+
+    title = models.CharField(max_length=200)
+    slug = models.SlugField(max_length=200, unique=True)
+
+    class Meta:
+        ordering = ['title',]
+
+    def __str__(self):
+        return self.title
+
+    
