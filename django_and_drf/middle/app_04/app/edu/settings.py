d97470f9fcb9c5921ff8f4a38eb5243db8db02e1
golanghack <golanghack@gmail.com>
2023-06-22 21:15:47 +0300

    added new program -> django_and_drf -> middle -> app_04 -> app -> edu -> settings -> added media routing

--- django_and_drf/middle/app_04/app/edu/settings.py
+++ django_and_drf/middle/app_04/app/edu/settings.py
@@ -124,3 +124,8 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+
+# media
+MEDIA_URL = 'media/'
+MEDIA_ROOT = BASE_DIR / 'media'
