7b73c066067149f850060ff73418ff49642594c1
golanghack <golanghack@gmail.com>
2023-06-22 21:26:11 +0300

    added new program -> django_and_drf -> middle -> app_04 -> app -> courses -> models -> added Course model

--- django_and_drf/middle/app_04/app/courses/models.py
+++ django_and_drf/middle/app_04/app/courses/models.py
@@ -15,3 +15,18 @@ class Subject(models.Model):
         return self.title
 
     
+class Course(models.Model):
+    """-> model course for education cms platform"""
+
+    owner = models.ForeignKey(User, related_name='courses_created', on_delete=models.CASCADE)
+    subject = models.ForeignKey(Subject, related_name='courses', on_delete=models.CASCADE)
+    title = models.CharField(max_length=200)
+    slug = models.SlugField(max_length=200, unique=True)
+    overview = models.TextField(default='')
+    created = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['created',]
+
+    def __str__(self):
+        return self.title 
