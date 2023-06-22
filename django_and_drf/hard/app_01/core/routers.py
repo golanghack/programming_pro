bc1a952d4929ffee65729098ab07ded24d52f0fa
golanghack <golanghack@gmail.com>
2023-06-22 17:10:17 +0300

    added new program -> django_and_drf -> hard -> app_01 -> core -> routers -> added router for RefreshViewSet

--- django_and_drf/hard/app_01/core/routers.py
+++ django_and_drf/hard/app_01/core/routers.py
@@ -2,14 +2,15 @@
 from rest_framework import routers
 from core.user.viewsets import UserViewSet
 from core.auth.viewsets.register import RegisterViewSet
-from core.auth.viewsets. login import LoginViewSet
+from core.auth.viewsets.login import LoginViewSet
+from core.auth.viewsets.refresh import RefreshViewSet
 
 router = routers.SimpleRouter()
 
 # auth
 router.register(r'auth/register', RegisterViewSet, basename='auth-register')
 router.register(r'auth/login', LoginViewSet, basename='auth-login')
-
+router.register(r'auth/refresh', RefreshViewSet, basename='auth-refresh')
 # USer
 router.register(r'api/user', UserViewSet, basename='user')
 
