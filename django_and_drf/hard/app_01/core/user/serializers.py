16a011c918870d3dec13bf605f0de93e44118fce
golanghack <golanghack@gmail.com>
2023-06-22 18:23:08 +0300

    added new program -> django_and_drf -> hard -> app_01 -> core -> user -> serializers -> bug fixed and makemigrations

--- django_and_drf/hard/app_01/core/user/serializers.py
+++ django_and_drf/hard/app_01/core/user/serializers.py
@@ -1,12 +1,9 @@
+
 from core.abstract.serializers import AbstractSerializer
 from core.user.models import User 
 
 class UserSerializer(AbstractSerializer):
     """-> user serializer model"""
-
-    id = serializers.UUIDField(source='public_id', read_only=True, format='hex')
-    created = serializers.DateTimeField(read_only=True)
-    updated = serializers.DateTimeField(read_only=True)
 
     class Meta:
         model = User 
