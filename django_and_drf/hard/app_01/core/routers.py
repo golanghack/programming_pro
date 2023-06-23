cac5e5161f15afec3678f8c835bb46c159f5abb7
golanghack <golanghack@gmail.com>
2023-06-23 15:37:38 +0300

    added new program -> django_and_drf -> hard -> app_01 -> core -> routers -> added nesten router

--- django_and_drf/hard/app_01/core/routers.py
+++ django_and_drf/hard/app_01/core/routers.py
@@ -1,10 +1,11 @@
-
-from rest_framework import routers
+from rest_framework_nested import nested_routers
+from rest_framework.routers import routers
 from core.user.viewsets import UserViewSet
 from core.auth.viewsets.register import RegisterViewSet
 from core.auth.viewsets.login import LoginViewSet
 from core.auth.viewsets.refresh import RefreshViewSet
 from core.post.viewsets import PostViewSet
+from core.comment.viewsets import CommentViewSet
 
 router = routers.SimpleRouter()
 
@@ -18,7 +19,10 @@ router.register(r'api/user', UserViewSet, basename='user')
 
 # Post
 router.register(r'api/post', PostViewSet, basename='post')
+post_router = nested_routers.NestedSimpleRouter(router, r'post', lookup='post')
+post_router.register(r'comment', CommentViewSet, basename='post-comment')
 
 urlpatterns = [
     *router.urls,
+    *post_router.urls,
 ]
