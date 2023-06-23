f563ba7fe23483fddfdff273571382c8af7cfe71
golanghack <golanghack@gmail.com>
2023-06-23 15:40:37 +0300

    added new program -> django_and_drf -> hard -> app_01 -> core -> comments -> viewsets -> CommentViewSet -> added get_queryset

--- django_and_drf/hard/app_01/core/comment/viewsets.py
+++ django_and_drf/hard/app_01/core/comment/viewsets.py
@@ -13,3 +13,14 @@ class CommentViewSet(AbstractViewSet):
     http_method_names = ['post', 'get', 'delete', 'put']
     permission_classes = (UserPermission, )
     serializer_class = CommentSerializer
+
+    def get_queryset(self):
+        if self.request.user.is_superuser:
+            return Comment.objects.all()
+        post_pk = self.kwargs['post_pk']
+        if post_pk is None:
+            return Http404
+        queryset = Comment.objects.filter(post__public_id=post_pk)
+        return queryset
+
+    
