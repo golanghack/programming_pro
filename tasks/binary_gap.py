e5d40839ce7838c41f7dfae790378167dd4f977c
golanghack <golanghack@gmail.com>
2023-06-24 18:50:53 +0300

    added new program -> tasks -> binary_gap

--- tasks/binary_gap.py
+++ tasks/binary_gap.py
@@ -8,6 +8,43 @@ VIEW START
 Find longest sequence of zeros in binary representation of an integer.
 """
 
+def to_binary(number: int):
+    """return list binary integers""" 
+    return bin(number)
+
+
+def find_longest_indent_with_zeros(N: int) -> int:
+    """return longest indent with zeros"""
+
+    bin_num = bin(N)
+    bad_pattern = '0b'
+    if bad_pattern in bin_num:
+        bin_num = bin_num.replace(bad_pattern, '')
+    string_num = str(bin_num)
+    splitting_num = string_num.split('1')
+    binary_indent = str(splitting_num)
+    have_a_binary_undent = True
+    bin_split = binary_indent.split('0')
+    
+    if '' in bin_split:
+        have_a_binary_undent = False
+    lens_num = []
+    for line in splitting_num:
+        lens_num.append(len(line))
+    if have_a_binary_undent == True or string_num[-1] != '0':
+        return max(lens_num)
+    else:
+        return 0
+
+print(find_longest_indent_with_zeros(32))
+
+
+        
+
+
+
+
+
 
 def find_zeros_in_numbers(numbers: list) -> int:
     """return list number with zero"""
@@ -31,10 +68,11 @@ def longest_zeros_number(numbers: list) -> int:
     return max_zero_number
 
 def main():
-    start_numbers = [12, 20, 40, 400, 5000, 50000]
+    """start = [12, 20, 40, 400, 5000, 50000]
+    start_numbers = to_binary(start)
     numbers_list = find_zeros_in_numbers(start_numbers)
     longest = longest_zeros_number(numbers_list)
-    print(start_numbers[longest])
+    print(start_numbers[longest])"""
 
 if __name__ == '__main__':
     main()
