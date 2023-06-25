56e222aae1dd2e4a27b808c54a58f2bc2aafff01
golanghack <golanghack@gmail.com>
2023-06-25 21:48:35 +0300

    added new program -> fastapi -> app_01 -> refaxctoring, fastapi -> app_02 -> install fastapi in environment, create requirements.txt

--- fastapi/app_01/app_01/main.py
+++ fastapi/app_01/app_01/main.py
@@ -1,7 +1,13 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Path, Query
+from typing import Optional
 
 app = FastAPI()
 
-@app.get('/')
-async def index():
-    return {'message': 'Test'}
+@app.get('/admin/{name}/branch/{branch_id}')
+async def get_admin(branch_id: int=Path(1, gt=0, le=100), 
+                    age: int=Query(None, ge=20, lt=61), 
+                    name: str=Path(None, min_lenght=10),
+                    branch_name: str=Query(None, min_length=5, max_length=10)):
+            admin = {'name': name, 'branch': branch_name, 
+                        'branch id': branch_id, 'age': age}
+            return admin
