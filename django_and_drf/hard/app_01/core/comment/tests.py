6d2526993c884287f8ec27c8b96c17123e2cc51e
golanghack <golanghack@gmail.com>
2023-06-25 19:00:13 +0300

    added new program -> django_and_drf -> hard -> app_01 -> core -> comment -> tests -> added test create comment

--- django_and_drf/hard/app_01/core/comment/tests.py
+++ django_and_drf/hard/app_01/core/comment/tests.py
@@ -1,1 +1,14 @@
 import pytest
+from core.fixtures.user import user
+from core.fixtures.post import post
+from core.comment.models import Comment
+
+@pytest.mark.django_db
+def test_create_comment(user, post):
+    """-> create comment from test data of user and post""" 
+
+    comment = Comment.objects.create(author=user, post=post, body='Test')
+
+    assert comment.author == user
+    assert comment.post == post
+    assert comment.body == 'Test'
