1699a0a2303977e7bd8938c3720857a0e45a0ee3
golanghack <golanghack@gmail.com>
2023-06-30 15:10:01 +0300

    added new programm -> std_lib -> email -> imaplib_search_from

---
+++ std_lib/email/imaplib_search_from.py
@@ -0,0 +1,13 @@
+#! /usr/bin/env python3 
+
+import imaplib
+import imaplib_connect
+from imaplib_list_parse import parse_list_response
+
+with imaplib_connect.open_connecttion() as conn:
+    typ, mbox_data = conn.list()
+    for line in mbox_data:
+        flags, delimiter, mbox_name = parse_list_response(line)
+        conn.select(f"'{mbox_name}'", readonly=True)
+        typ, msg_ids = conn.search(None, '(FROM "D" SUBJECT "Example message 2")',)
+        print(mbox_name, typ, msg_ids)
