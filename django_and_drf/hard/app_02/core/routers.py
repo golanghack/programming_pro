f725b3ae3a9ae840966a506b90cb8786b5fe2236
golanghack <golanghack@gmail.com>
2023-06-30 14:47:12 +0300

    added new programm -> django_and_drf -> hard -> app_02 -> core -> routers -> added register

--- django_and_drf/hard/app_02/core/routers.py
+++ django_and_drf/hard/app_02/core/routers.py
@@ -1,11 +1,14 @@
 from rest_framework import routers
 from core.user.viewsets import UserViewSet
+from core.auth.viewsets.register import RegisterViewSet
 
 router = routers.SimpleRouter()
 
 # user
 router.register(r'user', UserViewSet, basename='user')
 
+# auth
+router.register(r'auth/register', RegisterViewSet, basename='auth-register')
 
 urlpatterns = [
     *router.urls,
