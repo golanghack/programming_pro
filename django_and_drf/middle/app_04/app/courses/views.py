32413c96aa81052628142e1c9d1812489218501f
golanghack <golanghack@gmail.com>
2023-06-30 10:36:20 +0300

    added new programm -> django_and_drf -> middle -> app_04 -> app -> courses -> views -> added mixins and update ManageListView

--- django_and_drf/middle/app_04/app/courses/views.py
+++ django_and_drf/middle/app_04/app/courses/views.py
@@ -1,12 +1,36 @@
 from django.views.generic.list import ListView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
 from .models import Course
 
-class ManageCourseListView(ListView):
-    """Manager for courses list""" 
-
-    model = Course
-    template_name = 'courses/manage/course/list.html'
+class OwnerMixin:
+    """Mixin class for owner course""" 
 
     def get_queryset(self):
-        qs = super().get_queryset()
+        qs = super.get_queryset()
         return qs.filter(owner=self.request.user)
+
+class OwnerEditMixin:
+    """Class for eidting course"""
+
+    def form_valid(self, form):
+        form.instance.owner = self.request.user
+        return super().form_valid(form)
+
+class OwnerCourseMixin(OwnerMixin):
+    """Class for owner course with fields""" 
+
+    model = Course
+    fields = ['subject', 'title', 'slug', 'overview']
+    success_url = reverse_lazy('manage_course_list')
+
+class OwnerCourseEditMixin(OwnerCourseMixin, OwnerEditMixin):
+    template_name = 'courses/manage/course/form.html'
+
+class ManageCourseListView(OwnerCourseMixin, ListView):
+    template_name = 'courses/manage/course/list.html'
+
+class CourseDeleteView(OwnerCourseMixin, DeleteView):
+    template_name = 'courses/manage/course/delete.html'
+
+    
