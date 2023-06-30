a5c7701ccb78f4d4264d53b4a08fc5b40c2bf76b
golanghack <golanghack@gmail.com>
2023-06-30 13:55:38 +0300

    added new programm -> django_and_drf -> hard -> app_02 -> core -> routers

---
+++ django_and_drf/hard/app_02/core/routers.py
@@ -0,0 +1,12 @@
+from rest_framework import routers
+from core.user.viewsets import UserViewSet
+
+router = routers.SimpleRouter()
+
+# user
+router.register(r'user', UserViewSet, basename='user')
+
+
+urlpatterns = [
+    *router.urls,
+]
