0f04d551e5a8e636f13b9483b54be983180041d4
golanghack <golanghack@gmail.com>
2023-06-30 14:34:52 +0300

    added new programm -> django_and_drf -> hard -> app_02 -> core -> auth -> serializers -> register

---
+++ django_and_drf/hard/app_02/core/auth/serializers/register.py
@@ -0,0 +1,22 @@
+from rest_framework import serializers
+from core.user.serializers import UserSerializer
+from core.user.models import User
+
+class RegisterSerializer(UserSerializer):
+    """Registration serializer for request and user createion
+    
+    password -> 8 symbols
+    """ 
+
+    password = serializers.CharField(max_lenght=128, min_lenght=8, write_only=True, required=True)
+
+    class Meta:
+        model = User
+        fields = ['id', 'bio', 'avatar', 'email', 'username', 'first_name', 'last_name', 'apssword']
+
+    def create(self, validated_data):
+        """Use the create_user method to create a new user""" 
+
+        return User.objects.create_user(**validated_data)
+
+     
