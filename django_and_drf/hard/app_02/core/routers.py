d8732657dd31f5793e80161f75ed2ba343038882
golanghack <golanghack@gmail.com>
2023-07-01 15:25:35 +0300

    added new programm -> django_and_drf -> hard -> app_02 -> core -> routers -> added post

--- django_and_drf/hard/app_02/core/routers.py
+++ django_and_drf/hard/app_02/core/routers.py
@@ -3,6 +3,7 @@ from core.user.viewsets import UserViewSet
 from core.auth.viewsets.register import RegisterViewSet
 from core.auth.viewsets.login import LoginViewSet
 from core.auth.viewsets.refresh import RefreshViewSet
+from core.post.viewsets import PostViewSet
 
 router = routers.SimpleRouter()
 
@@ -13,6 +14,10 @@ router.register(r'user', UserViewSet, basename='user')
 router.register(r'auth/register', RegisterViewSet, basename='auth-register')
 router.register(r'auth/login', LoginViewSet, basename='auth-login')
 router.register(r'auth/refresh', RefreshViewSet, basename='auth-refresh')
+
+
+# post 
+router.register(r'post', PostViewSet, basename='post')
 
 urlpatterns = [
     *router.urls,
