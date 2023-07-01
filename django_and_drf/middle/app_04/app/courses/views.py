c7a24f7fee2ae85be5ead169cdcc006cf7fcf31f
golanghack <golanghack@gmail.com>
2023-07-01 17:18:52 +0300

    added new programm -> django_and_drf -> middle -> app_04 -> app -> courses -> views -> CourseModuleUpdateView

--- django_and_drf/middle/app_04/app/courses/views.py
+++ django_and_drf/middle/app_04/app/courses/views.py
@@ -1,8 +1,11 @@
 from django.views.generic.list import ListView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.urls import reverse_lazy
+from django.shortcuts import redirect, get_object_or_404
+from django.views.generic.base import TemplateResponseMixin, View
 from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
-from .models import Course
+from courses.models import Course
+from courses.forms import ModuleFormSet
 
 class OwnerMixin:
     """Mixin class for owner course""" 
@@ -42,4 +45,34 @@ class CourseDeleteView(OwnerCourseMixin, DeleteView):
     template_name = 'courses/manage/course/delete.html'
     permission_required = 'courses.delete_course'
 
+# modules 
+class CourseModuleUpdateView(TemplateResponseMixin, View):
+    """Course module update view """
+    #TO-DO -> rename
 
+    template_name = 'courses/manage/module/formset.html'
+    course = None
+
+    def get_formset(self, data=None):
+        return ModuleFormSet(instance=self.course, data=data)
+    
+    def dispatch(self, request, pk):
+        self.course = get_object_or_404(Course, id=pk, owner=request.user)
+        return super().dispatch(request, pk)
+    
+    def get(self, request, *args, **kwargs):
+        formset = self.get_formset()
+        return self.render_to_response({
+            'course': self.course, 
+            'formset': formset
+        })
+    
+    def post(self, request, *args, **kwargs):
+        formset = self.get_formset(data=request.POST)
+        if formset.is_valid():
+            formset.save()
+            return redirect('manage_course_list')
+        return self.render_to_response({
+            'course': self.course,
+            'formset': formset
+        })
