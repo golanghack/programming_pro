0c14db66257b7c5399dd6dbf2bc4a5b133e8b02c
golanghack <golanghack@gmail.com>
2023-07-02 16:50:33 +0300

    added new programm -> django_and_drf > hard -> app_02 -> core -> comments -> apps -> rename

--- django_and_drf/hard/app_02/core/comments/apps.py
+++ django_and_drf/hard/app_02/core/comments/apps.py
@@ -3,4 +3,5 @@ from django.apps import AppConfig
 
 class CommentsConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
-    name = "comments"
+    name = "core.comments"
+    label = "core_comments"
