c1a8bf2a1f686aae6e89ab08b4364cce0c1f1972
golanghack <golanghack@gmail.com>
2023-07-02 17:56:12 +0300

    added new programm -> django_and_drf > middle -> app_04 -> app -> courses -> urls -> added url for list modules

--- django_and_drf/middle/app_04/app/courses/urls.py
+++ django_and_drf/middle/app_04/app/courses/urls.py
@@ -13,5 +13,6 @@ urlpatterns = [
                                                             name='module_content_update'),
     path('content/<int:id>/delete/', views.ContentDeleteView.as_view(), 
                                                             name='module_content_delete'),
-                                                             
+    path('module/<int:module_id>/', views.ModuleContentListView.as_view(), 
+                                                            name='module_content_list'), 
 ]
