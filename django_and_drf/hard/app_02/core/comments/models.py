c3e174bd1905c49260e2c04d4ebe906f1c615c7a
golanghack <golanghack@gmail.com>
2023-07-02 16:49:38 +0300

    added new programm -> django_and_drf > hard -> app_02 -> core -> comments -> models -> add model Comment and CommentManager

--- django_and_drf/hard/app_02/core/comments/models.py
+++ django_and_drf/hard/app_02/core/comments/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from core.abstract.models import AbstractModel, AbstractManager
 
-# Create your models here.
+class CommentManager(AbstractManager):
+    pass
+
+class Comment(AbstractModel):
+    """Comment model"""
+
+    post = models.ForeignKey('core_post.Post', on_delete=models.PROTECT)
+    author = models.ForeignKey('core_user.User', on_delete=models.PROTECT)
+    body = models.TextField(default='')
+    edited = models.BooleanField(default=False)
+    objects = CommentManager()
+
+    def __str__(self):
+        return self.author.name 
+
+        
