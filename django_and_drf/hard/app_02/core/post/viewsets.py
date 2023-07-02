a682075b392c747a6ccc1f6e51e39705069d9db0
golanghack <golanghack@gmail.com>
2023-07-02 16:41:54 +0300

    added new programm -> django_and_drf > hard -> app_02 -> core -> post -> viewsets -> PostViewset -> added like and remove like function

--- django_and_drf/hard/app_02/core/post/viewsets.py
+++ django_and_drf/hard/app_02/core/post/viewsets.py
@@ -1,6 +1,7 @@
 from rest_framework.permissions import IsAuthenticated, BasePermission, SAFE_METHODS
 from rest_framework.response import Response
 from rest_framework import status
+from rest_framework.decorators import action
 from core.abstract.viewsets import AbstractViewSet
 from core.post.models import Post
 from core.post.serializers import PostSerializer
@@ -25,6 +26,22 @@ class PostViewSet(AbstractViewSet):
         serializer.is_valid(raise_exception=True)
         self.perform_create(serializer)
         return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+    @action(methods=['post'], detail=True)
+    def like(self, request, *args, **kwargs):
+        post = self.get_object()
+        user = self.request.user 
+        user.like(post)
+        serializer = self.serializer_class(post)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    
+    @action(methods=['post'], detail=True)
+    def remove_like(self, request, *args, **kwargs):
+        post = self.get_object()
+        user = self.request.user
+        user.remove_like(post)
+        serializer = self.serializer_class(post)
+        return Response(serializer.data, status=status.HTTP_200_OK)
 
 class UserPermission(BasePermission):
     """User permisiion"""
