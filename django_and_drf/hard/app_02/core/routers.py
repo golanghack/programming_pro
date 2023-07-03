68005fc9d8dfa3798bd68c46e72fe5b6187de770
golanghack <golanghack@gmail.com>
2023-07-03 19:00:50 +0300

    added new programm -> django_and_drf -> hard -> app_02 -> core -> routers -> added drf_nested -> urls for post and post comment

--- django_and_drf/hard/app_02/core/routers.py
+++ django_and_drf/hard/app_02/core/routers.py
@@ -1,9 +1,10 @@
-from rest_framework import routers
+from rest_framework_nested import routers
 from core.user.viewsets import UserViewSet
 from core.auth.viewsets.register import RegisterViewSet
 from core.auth.viewsets.login import LoginViewSet
 from core.auth.viewsets.refresh import RefreshViewSet
 from core.post.viewsets import PostViewSet
+from core.comments.viewsets import CommentViewSet
 
 router = routers.SimpleRouter()
 
@@ -18,7 +19,10 @@ router.register(r'auth/refresh', RefreshViewSet, basename='auth-refresh')
 
 # post 
 router.register(r'post', PostViewSet, basename='post')
+posts_router = routers.NestedSimpleRouter(router, r'post', lookup='post')
+posts_router.register(r'comment', CommentViewSet, basename='post-comment')
 
 urlpatterns = [
     *router.urls,
+    *posts_router.urls
 ]
