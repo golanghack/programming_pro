895a3fd003f0596f150c95aa51b6ca03b5c90bab
golanghack <golanghack@gmail.com>
2023-07-04 18:23:56 +0300

    added new programm -> django_and_drf -> hard -> app_02 -> core -> comments -> serializers -> added update function in CommentSerializer

--- django_and_drf/hard/app_02/core/comments/serializers.py
+++ django_and_drf/hard/app_02/core/comments/serializers.py
@@ -24,6 +24,12 @@ class CommentSerializer(AbstractSerializer):
         if self.instance:
             return self.instance.post 
         return value
+
+    def update(self, instance, validated_data):
+        if not instance.edited:
+            validated_data['edited'] = True
+        instance = super().update(instance, validated_data)
+        return instance
     class Meta:
         model = Comment
         fields = ['id', 'post', 'author', 'body', 'edited', 'created', 'updated']
