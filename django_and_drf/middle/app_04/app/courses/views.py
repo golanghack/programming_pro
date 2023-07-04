ba8c14009f5062be08ccaea4ed7b80c914cb1d4d
golanghack <golanghack@gmail.com>
2023-07-04 21:52:09 +0300

    added new programm -> django_and_drf -> middle -> app_04 -> app -> courses -> lists -> CourseListView

--- django_and_drf/middle/app_04/app/courses/views.py
+++ django_and_drf/middle/app_04/app/courses/views.py
@@ -2,12 +2,13 @@ from braces.views import CsrfExemptMixin, JSONRequestResponseMixin
 from django.views.generic.list import ListView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.urls import reverse_lazy
+from django.db.models import Count
 from django.shortcuts import redirect, get_object_or_404
 from django.forms.models import modelform_factory
 from django.apps import apps
 from django.views.generic.base import TemplateResponseMixin, View
 from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
-from courses.models import Course, Module, Content
+from courses.models import Course, Module, Content, Subject
 from courses.forms import ModuleFormSet
 
 class OwnerMixin:
@@ -157,3 +158,19 @@ class ContentOrderView(CsrfExemptMixin, JSONRequestResponseMixin, View):
         for id, order in self.request_json.items():
             Content.objects.filter(id=id, module__course__owner=request.user).update(order=order)
         return self.render_json_response({'saved': 'OK'})
+
+
+class CourseListView(TemplateResponseMixin, View):
+    model = Course 
+    template_name = 'courses/course/list.html'
+
+    def get(self, request, subject=None):
+        subjects = Subject.objects.annotate(total_courses=Count('courses'))
+        courses = Course.objects.annotate(total_modules=Count('modules'))
+
+        if subject:
+            subject = get_object_or_404(Subject, slug=subject)
+            courses = courses.filter(subject=subject)
+        
+        return self.render_to_response({'subjects': subjects, 'subject': subject, 'courses': courses})
+        
