4e9e4546973cf6ea5c8efbebf332334b8341052f
golanghack <golanghack@gmail.com>
2023-07-05 22:40:12 +0300

    added new programm -> algo -> find_max

--- django_and_drf/middle/app_04/app/students/urls.py
+++ django_and_drf/middle/app_04/app/students/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from students import views
 
 urlpatterns = [
-    path('register/', views.StudentsRegistrationView.as_view(), name='student_registration'),
+    path('register/', views.StudentsRegistrationView.as_view(), 
+                            name='student_registration'),
 ]
