034411edef28a469afb8d203cff3eeecd0f3f5bf
golanghack <golanghack@gmail.com>
2023-07-07 16:02:06 +0300

    added new programm -> django_and_drf -> middle -> app_04 -> app -> students -> views -> StudentEnrollCourseView

--- django_and_drf/middle/app_04/app/students/views.py
+++ django_and_drf/middle/app_04/app/students/views.py
@@ -1,7 +1,10 @@
 from django.urls import reverse_lazy
+from django.views.generic.edit import FormView
 from django.views.generic.edit import CreateView
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import authenticate, login
+from django.contrib.auth.mixins import LoginRequiredMixin
+from students.forms import CourseEnrollForm
 
 class StudentsRegistrationView(CreateView):
     """Student""" 
@@ -18,4 +21,15 @@ class StudentsRegistrationView(CreateView):
         login(self.request, user)
         return result
 
-        
+
+class StudentEnrollCourseView(LoginRequiredMixin, FormView):
+    course = None
+    form_class = CourseEnrollForm
+
+    def form_valid(self, form):
+        self.course = form.cleaned_data['course']
+        self.course.students.add(self.request.user)
+        return super().form_valid(form)
+
+    def get_success_url(self):
+        return reverse_lazy('student_course_detail', args=[self.course.id])
