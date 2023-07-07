13de79fb6c0de813c9e0eb2c3af49a686649ce52
golanghack <golanghack@gmail.com>
2023-07-07 11:39:24 +0300

    added new programm -> django_and_drf -> hard -> app_02 -> app -> added corsheaders configurate in settings

--- django_and_drf/hard/app_02/app/settings.py
+++ django_and_drf/hard/app_02/app/settings.py
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
     #<--DOWNLOADED-->
     "rest_framework",
     "rest_framework_simplejwt",
+    "corsheaders",
 ]
 
 AUTH_USER_MODEL = 'core_user.User'
@@ -66,12 +67,19 @@ REST_FRAMEWORK = {
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
+    "corsheaders.middleware.CorsMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",
+    "http://127.0.0.1:3000"
+]
+
 
 ROOT_URLCONF = "app.urls"
 
