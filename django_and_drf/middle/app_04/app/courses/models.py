90f0b0147c558f8c8bf85cc8d16ab06a988e3fc4
golanghack <golanghack@gmail.com>
2023-07-10 20:29:24 +0300

    added new programm -> django_and_drf -> middle -> app_04 -> app -> courses -> models -> ItemBase -> render

--- django_and_drf/middle/app_04/app/courses/models.py
+++ django_and_drf/middle/app_04/app/courses/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.template.loader import render_to_string
 from django.contrib.auth.models import User
 from django.contrib.contenttypes.models import ContentType
 from django.contrib.contenttypes.fields import GenericForeignKey
@@ -68,6 +69,9 @@ class ItemBase(models.Model):
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
 
+    def render(self):
+        return render_to_string(f'courses/content/{self._meta.model_name}.html',
+                                {'item': self})
     class Meta:
         abstract = True
 
