f1fcb9a7c95a48cb28b46903f0502847244faf71
golanghack <golanghack@gmail.com>
2023-07-13 19:01:04 +0300

    added new programm -> django_and_drf -> middle -> app_04 -> app -> course -> api -> views -> added permission class IsAuthenticated in EnrollView

--- django_and_drf/middle/app_04/app/courses/api/views.py
+++ django_and_drf/middle/app_04/app/courses/api/views.py
@@ -2,6 +2,7 @@ from rest_framework import generics
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.authentication import BasicAuthentication
+from rest_framework.permissions import IsAuthenticated
 from django.shortcuts import get_object_or_404
 from courses.models import Subject, Course
 from courses.api.serializers import SubjectSerializer
@@ -16,7 +17,8 @@ class SubjectDetailView(generics.RetrieveAPIView):
     serializer_class = SubjectSerializer
 
 class CourseEnrollView(APIView):
-    authentication_classes = [BasicAuthentication,]
+    authentication_classes = [BasicAuthentication, ]
+    permission_classes = [IsAuthenticated, ]
     def post(self, request, pk, format=None):
         course = get_object_or_404(Course, pk=pk)
         course.students.add(request.user)
