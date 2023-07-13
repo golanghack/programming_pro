3e1b93ea6c273fda808e92b7634434adab46c1f9
golanghack <golanghack@gmail.com>
2023-07-13 18:55:37 +0300

    added new programm -> django_and_drf -> middle -> app_04 -> app -> course -> api -> urls -> added enroll course path

--- django_and_drf/middle/app_04/app/courses/api/urls.py
+++ django_and_drf/middle/app_04/app/courses/api/urls.py
@@ -4,6 +4,11 @@ from courses.api import views
 app_name = 'courses'
 
 urlpatterns = [
-    path('subjects/', views.SubjectListView.as_view(), name='subject_list'),
-    path('subjects/<pk>/', views.SubjectDetailView.as_view(), name='subject_detail'),
+    path('subjects/', views.SubjectListView.as_view(), 
+                        name='subject_list'),
+    path('subjects/<pk>/', views.SubjectDetailView.as_view(), 
+                        name='subject_detail'),
+    path('courses/<pk>/enroll/', views.CourseEnrollView.as_view(),
+                        name='course_enroll'),
+    
 ]
