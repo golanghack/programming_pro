73178ab33876d58c5904a47364b435e5f3c0be9d
golanghack <golanghack@gmail.com>
2023-07-13 19:22:05 +0300

    added new programm -> django_and_drf -> middle -> app_04 -> app -> course -> api -> viewsets -> CourseViewSet -> added enroll, permissions, authenticated

--- django_and_drf/middle/app_04/app/courses/api/viewsets.py
+++ django_and_drf/middle/app_04/app/courses/api/viewsets.py
@@ -1,7 +1,22 @@
 from rest_framework import viewsets
+from rest_framework.decorators import action
+from rest_framework.authentication import BasicAuthentication
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
 from courses.api.serializers import CourseSerializer
 from courses.models import Course
 
 class CourseViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = Course.objects.all()
     serializer_class = CourseSerializer
+
+    @action(detail=True, 
+            methods=['post'],
+            authentication_classes=[BasicAuthentication,],
+            permission_classes=[IsAuthenticated,])
+    def enroll(self, request, *args, **kwargs):
+        course = self.get_object()
+        course.students.add(request.user)
+        return Response({
+            'enrolled': True
+        })
