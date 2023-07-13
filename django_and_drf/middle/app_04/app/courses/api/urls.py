2da5f6f00d1747d4b3aad9f6d288c11d8f10b397
golanghack <golanghack@gmail.com>
2023-07-13 19:23:17 +0300

    added new programm -> django_and_drf -> middle -> app_04 -> app -> course -> api -> urls -> delete enrolled path

--- django_and_drf/middle/app_04/app/courses/api/urls.py
+++ django_and_drf/middle/app_04/app/courses/api/urls.py
@@ -13,7 +13,5 @@ urlpatterns = [
                         name='subject_list'),
     path('subjects/<pk>/', views.SubjectDetailView.as_view(), 
                         name='subject_detail'),
-    path('courses/<pk>/enroll/', views.CourseEnrollView.as_view(),
-                        name='course_enroll'),
 
 ]
