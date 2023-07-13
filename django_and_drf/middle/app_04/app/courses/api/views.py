717a577a6da8a6e72950b7efc608550a6cea43d7
golanghack <golanghack@gmail.com>
2023-07-13 19:24:01 +0300

    added new programm -> django_and_drf -> middle -> app_04 -> app -> course -> api -> views -> delete CourseEnrolView

--- django_and_drf/middle/app_04/app/courses/api/views.py
+++ django_and_drf/middle/app_04/app/courses/api/views.py
@@ -15,13 +15,3 @@ class SubjectListView(generics.ListAPIView):
 class SubjectDetailView(generics.RetrieveAPIView):
     queryset = Subject.objects.all()
     serializer_class = SubjectSerializer
-
-class CourseEnrollView(APIView):
-    authentication_classes = [BasicAuthentication, ]
-    permission_classes = [IsAuthenticated, ]
-    def post(self, request, pk, format=None):
-        course = get_object_or_404(Course, pk=pk)
-        course.students.add(request.user)
-        return Response({
-            'enrolled': True,
-        })
