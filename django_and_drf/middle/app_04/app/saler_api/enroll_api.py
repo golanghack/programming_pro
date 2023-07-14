2763bd672d13b1705be051b61fc2654868989b2b
golanghack <golanghack@gmail.com>
2023-07-14 16:14:35 +0300

    added new programm -> django_and_drf -> middle -> app_04 -> saler_api -> enroll_api -> refactoring -> deleted forloop

--- django_and_drf/middle/app_04/app/saler_api/enroll_api.py
+++ django_and_drf/middle/app_04/app/saler_api/enroll_api.py
@@ -1,9 +1,7 @@
 from requests import get
 
-base_url = 'http://127.0.0.1:8000/api/'
+req = get('http://127.0.0.1:8000/api/courses/')
 
-req = get(f'{base_url}/courses/')
 courses = req.json()
 
-available_courses = ', '.join([course['title'] for course in courses])
-print(f'All courses -> {available_courses}')
+print(courses)
