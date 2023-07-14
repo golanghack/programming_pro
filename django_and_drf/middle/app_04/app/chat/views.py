10b537040dc732e7da3f480bf980cfa8ba104963
golanghack <golanghack@gmail.com>
2023-07-14 16:34:54 +0300

    added new programm -> django_and_drf -> middle -> app_04 -> app -> chat -> views -> added course chat_room

--- django_and_drf/middle/app_04/app/chat/views.py
+++ django_and_drf/middle/app_04/app/chat/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponseForbidden
+from django.contrib.auth.decorators import login_required
 
-# Create your views here.
+@login_required
+def course_chat_room(request, course_id):
+    try:
+        # current user
+        course = request.user.courses_joined.get(id=course_id)
+    except:
+        # not students
+        return HttpResponseForbidden()
+    return render(request, 'chat/room.html', {'course': course})
