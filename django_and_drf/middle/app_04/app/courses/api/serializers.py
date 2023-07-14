eb8b34bccf49eaaaebe7fe48e9cab082c6ace5af
golanghack <golanghack@gmail.com>
2023-07-14 15:25:37 +0300

    added new programm -> django_and_drf -> middle -> app_04 -> app -> courses -> api -> serializers -> added ItemRelatedField, ContentSerializer

--- django_and_drf/middle/app_04/app/courses/api/serializers.py
+++ django_and_drf/middle/app_04/app/courses/api/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from courses.models import Subject, Course, Module
+from courses.models import Subject, Course, Module, Content
 
 class SubjectSerializer(serializers.ModelSerializer):
     class Meta:
@@ -17,3 +17,12 @@ class CourseSerializer(serializers.ModelSerializer):
         model = Course
         fields = ['id', 'subject', 'title', 'slug', 'overview', 'created', 'owner', 
                     'modules']
+
+class ItemRelatedField(serializers.RelatedField):
+    def to_representation(self, value):
+        return value.render()
+class ContentSerializer(serializers.ModelSerializer):
+    item = ItemRelatedField(read_only=True)
+    class Meta:
+        model = Content
+        fields = ['order', 'item']
