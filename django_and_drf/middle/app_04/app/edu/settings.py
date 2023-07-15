a9e58cdb5d6423f0cf7b5ebabf58996be9679c00
golanghack <golanghack@gmail.com>
2023-07-15 12:48:54 +0300

    added new programm -> django_and_drf -> middle -> app_04 -> app -> edu -> settings -> added CHANNEL_LAYERS configuration and installed cannels_redis

--- django_and_drf/middle/app_04/app/edu/settings.py
+++ django_and_drf/middle/app_04/app/edu/settings.py
@@ -105,6 +105,15 @@ DATABASES = {
 }
 
 
+CHANNEL_LAYERS = {
+    'default': {
+        'BACKEND': 'channels_redis.core.RedisChannelLayer',
+        'CONFIG': {
+            'hosts': [('127.0.0.1', 6379)],
+        },
+    },
+}
+
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
 
