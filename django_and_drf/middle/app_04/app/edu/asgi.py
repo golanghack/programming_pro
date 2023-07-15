e44bd2329b138750eabf05a0d603efe28b23c2b6
golanghack <golanghack@gmail.com>
2023-07-15 11:57:43 +0300

    added new programm -> django_and_drf -> middle -> app_04 -> app -> edu -> asgi -> added websoket

--- django_and_drf/middle/app_04/app/edu/asgi.py
+++ django_and_drf/middle/app_04/app/edu/asgi.py
@@ -10,11 +10,16 @@ https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
 import os
 
 from django.core.asgi import get_asgi_application
-from channels.routing import ProtocolTypeRouter
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.auth import AuthMiddlewareStack
+import chat.routers
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "edu.settings")
 
 django_asgi_app = get_asgi_application()
 application = ProtocolTypeRouter({
     'http': django_asgi_app,
+    'websocket': AuthMiddlewareStack(
+        URLRouter(chat.routers.websocket_urlpatterns)
+    ),
 })
