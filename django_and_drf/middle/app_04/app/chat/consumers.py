a12aa048830b5a593509cf2d87908469bd1e650d
golanghack <golanghack@gmail.com>
2023-07-15 14:11:06 +0300

    added new programm -> django_and_drf -> middle -> app_04 -> app -> chat -> templates -> chat -> consumers -> refactoring -> added async

--- django_and_drf/middle/app_04/app/chat/consumers.py
+++ django_and_drf/middle/app_04/app/chat/consumers.py
@@ -1,33 +1,32 @@
 import json
-from channels.generic.websocket import WebsocketConsumer
-from asgiref.sync import async_to_sync
+from channels.generic.websocket import AsyncWebsocketConsumer
 from django.utils import timezone
 
-class ChatConsumer(WebsocketConsumer):
-    def connect(self):
+class ChatConsumer(AsyncWebsocketConsumer):
+    async def connect(self):
         self.user = self.scope['user']
         self.id = self.scope['url_route']['kwargs']['course_id']
         self.room_group_name = f'chat_{self.id}'
         # connect to group
-        async_to_sync(self.channel_layer.group_add)(
+        await self.channel_layer.group_add(
             self.room_group_name,
             self.channel_name
         )
         # connecting
-        self.accept()
+        await self.accept()
     
-    def disconnect(self, close_code):
+    async def disconnect(self, close_code):
         # from out room 
-        async_to_sync(self.channel_layer.group_discard)(
+        await self.channel_layer.group_discard(
             self.room_group_name,
             self.channel_name
         )
 
-    def receive(self, text_data):
+    async def receive(self, text_data):
         text_data_json = json.loads(text_data)
         message = text_data_json['message']
         # send messages in group of room
-        async_to_sync(self.channel_layer.group_send)(
+        await self.channel_layer.group_send(
             self.room_group_name,
             {
                 'type': 'chat_message',
@@ -36,8 +35,6 @@ class ChatConsumer(WebsocketConsumer):
                 'datetime': timezone.now().isoformat(),
             }
         )
-        self.send(text_data=json.dumps({'message': message}))
-
-    def chat_message(self, event):
+    async def char_message(self, event):
         # send message in ws 
-        self.send(text_data=json.dumps(event))
+        await self.send(text_data=json.dumps(event))
