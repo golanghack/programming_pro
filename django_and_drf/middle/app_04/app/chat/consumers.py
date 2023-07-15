dbb131827d50d13be08031b613c9b63f6ad1ae28
golanghack <golanghack@gmail.com>
2023-07-15 12:57:31 +0300

    added new programm -> django_and_drf -> middle -> app_04 -> app -> chat -> consumers -> connect -> added layers, groups and channel names

--- django_and_drf/middle/app_04/app/chat/consumers.py
+++ django_and_drf/middle/app_04/app/chat/consumers.py
@@ -1,8 +1,17 @@
 import json
 from channels.generic.websocket import WebsocketConsumer
+from asgiref.sync import async_to_sync
 
 class ChatConsumer(WebsocketConsumer):
     def connect(self):
+        self.id = self.scope['url_route']['kwargs']['course_id']
+        self.room_group_name = f'chat_{self.id}'
+        # connect to group
+        async_to_sync(self.channel_layer.group_add)(
+            self.room_group_name,
+            self.channel_name
+        )
+        # connecting
         self.accept()
     
     def disconnect(self, close_code):
