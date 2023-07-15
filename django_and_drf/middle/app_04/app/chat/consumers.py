d8904b4038102c1404e15e0f1238e28b5b36eff0
golanghack <golanghack@gmail.com>
2023-07-15 13:02:52 +0300

    added new programm -> django_and_drf -> middle -> app_04 -> app -> chat -> consumers -> refactoring recieve function -> added functionality for messages send

--- django_and_drf/middle/app_04/app/chat/consumers.py
+++ django_and_drf/middle/app_04/app/chat/consumers.py
@@ -24,4 +24,12 @@ class ChatConsumer(WebsocketConsumer):
     def receive(self, text_data):
         text_data_json = json.loads(text_data)
         message = text_data_json['message']
+        # send messages in group of room
+        async_to_sync(self.channel_layer.group_send)(
+            self.room_group_name,
+            {
+                'type': 'chat_message',
+                'message': message,
+            }
+        )
         self.send(text_data=json.dumps({'message': message}))
