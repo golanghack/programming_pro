87f0014691fa52c37aa4d30de47b6b41cd64f6c3
golanghack <golanghack@gmail.com>
2023-07-17 19:20:25 +0300

    added new programm -> django_and_drf -> simple -> app_05 -> bboard -> models -> added verbose_name for fields, added Mta class, added verbose_name_plural, ordering

--- django_and_drf/simple/app_05/bboard/models.py
+++ django_and_drf/simple/app_05/bboard/models.py
@@ -1,7 +1,13 @@
 from django.db import models
 
 class Bb(models.Model):
-    title = models.CharField(max_length=50)
-    content = models.TextField(null=True, blank=True)
-    price = models.FloatField(null=True, blank=True)
-    published = models.DateTimeField(auto_now_add=True, db_index=True)
+    title = models.CharField(max_length=50, verbose_name='Cost')
+    content = models.TextField(null=True, blank=True, verbose_name='content')
+    price = models.FloatField(null=True, blank=True, verbose_name='price')
+    published = models.DateTimeField(auto_now_add=True, 
+                                    db_index=True, verbose_name='Published')
+
+    class Meta:
+        verbose_name_plural = 'Boards'
+        verbose_name = 'Board'
+        ordering = ['-published']
