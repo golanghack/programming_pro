7676a3dcecf4b97455b179ba74f357cf3bfba348
golanghack <golanghack@gmail.com>
2023-07-17 18:58:36 +0300

    added new programm -> django_and_drf -> simple -> app_05 -> bboard -> views -> upgrade index

--- django_and_drf/simple/app_05/bboard/views.py
+++ django_and_drf/simple/app_05/bboard/views.py
@@ -1,4 +1,8 @@
 from django.http import HttpResponse
+from bboard.models import Bb
 
 def index(request: str) -> HttpResponse:
-    return HttpResponse('Hi')
+    list_board = 'List of boards\r\n\r\n\r\n'
+    for bb in Bb.objects.order_by('-published'):
+        list_board += bb.title + '\r\n' + bb.content + '\r\n\r\n'
+    return HttpResponse(list_board, content_type='text/plain; charset=utf-8')
