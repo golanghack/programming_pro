b71f8d76ff288b301a47d651a7c8977771fc18fc
golanghack <golanghack@gmail.com>
2023-07-17 18:20:23 +0300

    added new programm -> django_and_drf -> simple -> app_05 -> bboard -> bug fixed

--- django_and_drf/simple/app_05/app/settings.py
+++ django_and_drf/simple/app_05/app/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     #<--LOCAL-->
-    "bboard.apps.bb",
+    "bboard.apps.BboardConfig",
 ]
 
 MIDDLEWARE = [
--- django_and_drf/simple/app_05/bboard/apps.py
+++ django_and_drf/simple/app_05/bboard/apps.py
@@ -3,4 +3,5 @@ from django.apps import AppConfig
 
 class BboardConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
-    name = "bb"
+    name = "bboard"
+    label = "bb"
