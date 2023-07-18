f949f841ad2c106d339a29c8135512b757710cf5
golanghack <golanghack@gmail.com>
2023-07-18 18:00:08 +0300

    added new programm -> django_an_drf -> simple -> app_05 -> bboard -> admin -> added BbAdmin class

--- django_and_drf/simple/app_05/bboard/admin.py
+++ django_and_drf/simple/app_05/bboard/admin.py
@@ -1,4 +1,9 @@
 from django.contrib import admin
 from bboard.models import Bb 
 
-admin.site.register(Bb)
+class BbAdmin(admin.ModelAdmin):
+    list_display = ('title', 'content', 'price', 'published')
+    list_display_links = ('title', 'content')
+    search_fields = ('title', 'content',)
+
+admin.site.register(Bb, BbAdmin)
