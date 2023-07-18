856cf09adc719eb5cf7e41485705715133727d4a
golanghack <golanghack@gmail.com>
2023-07-18 18:13:46 +0300

    added new programm -> django_an_drf -> simple -> app_05 -> bboard -> admin -> BBAdmin -> added field for rubric

--- django_and_drf/simple/app_05/bboard/admin.py
+++ django_and_drf/simple/app_05/bboard/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from bboard.models import Bb, Rubric
 
 class BbAdmin(admin.ModelAdmin):
-    list_display = ('title', 'content', 'price', 'published')
+    list_display = ('title', 'content', 'price', 'published', 'rubric')
     list_display_links = ('title', 'content')
     search_fields = ('title', 'content',)
 
