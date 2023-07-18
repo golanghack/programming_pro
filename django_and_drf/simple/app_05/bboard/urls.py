86b1056b41d6a6301339a5d32bc0fa137bdffef0
golanghack <golanghack@gmail.com>
2023-07-18 18:53:11 +0300

    added new programm -> django_an_drf -> simple -> app_05 -> bboard -> templates -> refactoring -> added names routrs

--- django_and_drf/simple/app_05/bboard/urls.py
+++ django_and_drf/simple/app_05/bboard/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from bboard.views import index, by_rubric
 
 urlpatterns = [
-    path('<int:rubric_id>/', by_rubric),
-    path('', index),
+    path('<int:rubric_id>/', by_rubric, name='by_rubric'),
+    path('', index, name='index'),
 ]
