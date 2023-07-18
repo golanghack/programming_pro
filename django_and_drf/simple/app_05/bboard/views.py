142fae6dced70905136348417619a1635fde94bb
golanghack <golanghack@gmail.com>
2023-07-18 18:27:46 +0300

    added new programm -> django_an_drf -> simple -> app_05 -> bboard -> views -> by_rubric

--- django_and_drf/simple/app_05/bboard/views.py
+++ django_and_drf/simple/app_05/bboard/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
-from bboard.models import Bb
+from bboard.models import Bb, Rubric
 
+
+def by_rubric(request, rubric_id):
+    bbs = Bb.objects.filter(rubric=rubric_id)
+    rubrics = Rubric.objects.all()
+    current_rubric = Rubric.objects.get(pk=rubric_id)
+    context = {'bbs': bbs, 'rubrics': rubrics, 'current_rubric': current_rubric}
+    return render(request, 'bboard/by_rubric.html', context)
 def index(request: str):
     bbs = Bb.objects.all()
     return render(request, 'bboard/base.html', {'bbs': bbs})
