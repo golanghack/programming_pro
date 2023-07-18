a849f0929de44d2652cf4bc899c4408099166a1a
golanghack <golanghack@gmail.com>
2023-07-18 18:06:24 +0300

    added new programm -> django_an_drf -> simple -> app_05 -> bboard -> models -> added Rubric model, added foreignkey for Bb and rubric

--- django_and_drf/simple/app_05/bboard/models.py
+++ django_and_drf/simple/app_05/bboard/models.py
@@ -6,8 +6,18 @@ class Bb(models.Model):
     price = models.FloatField(null=True, blank=True, verbose_name='price')
     published = models.DateTimeField(auto_now_add=True, 
                                     db_index=True, verbose_name='Published')
+    rubric = models.ForeignKey('Rubric', null=True, on_delete=models.PROTECT, 
+                                                    verbose_name='Rubric')
 
     class Meta:
         verbose_name_plural = 'Boards'
         verbose_name = 'Board'
         ordering = ['-published']
+
+class Rubric(models.Model):
+    name = models.CharField(max_length=30, db_index=True, verbose_name='Rubric')
+
+    class Meta:
+        verbose_name_plural = 'Rubrics'
+        verbose_name = 'Rubric'
+        ordering = ['name']
