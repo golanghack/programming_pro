3a4b327c6aede512865fa50bc53dd6d033d4e50c
golanghack <golanghack@gmail.com>
2023-07-18 19:01:17 +0300

    added new programm -> django_an_drf -> simple -> app_05 -> bboard -> views -> BBCreateView

--- django_and_drf/simple/app_05/bboard/views.py
+++ django_and_drf/simple/app_05/bboard/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
+from django.views.generic.edit import CreateView
 from bboard.models import Bb, Rubric
+from bboard.forms import BbForm
 
 
 def by_rubric(request, rubric_id):
@@ -15,4 +17,14 @@ def index(request: str):
     rubrics = Rubric.objects.all()
     context = {'bbs': bbs, 'rubrics': rubrics}
     return render(request, 'base.html', context)
+
+class BbCreateView(CreateView):
+    template_name = 'bboard/create.html'
+    form_class = BbForm
+    success_url = '/bboard/'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['rubrics'] = Rubric.objects.all()
+        return context
     
