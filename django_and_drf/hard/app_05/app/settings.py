b485da2247c1cb74d374e19301370a6bf9136f10
golanghack <golanghack@gmail.com>
2023-07-21 17:28:22 +0300

    added new programm -> django_an_drf -> hard -> app_05 -> upgrade settings in app

--- django_and_drf/hard/app_05/app/settings.py
+++ django_and_drf/hard/app_05/app/settings.py
@@ -9,23 +9,30 @@ https://docs.djangoproject.com/en/4.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
-
+import os 
+from dotenv import load_dotenv
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+# env
+load_dotenv()
+ENV = os.environ.get("ENV")
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-3mw_#_6-2mz1^%w78&eo&i(yfe8d7)$8f^gk51cmu8s3=9z-#$'
+SECRET_KEY = os.environ.get(
+    "SECRET_KEY", 
+    default='django-insecure-3mw_#_6-2mz1^%w78&eo&i(yfe8d7)$8f^gk51cmu8s3=9z-#$'
+)
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False if ENV == "PROD" else True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS", "*").split(".")
 
 
 # Application definition
@@ -37,7 +44,16 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # <--DOWNLOADS-->
+    "rest_framework",
+    "rest_framework_simplejwt",
+    "restframework_simplejwt.token_blacklist",
+    "corsheaders",
+    # <-LOCAL-->
 ]
+
+
+
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -75,8 +91,11 @@ WSGI_APPLICATION = 'app.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': os.getenv("DATABASE_NAME", "app_05db"),
+        "USER": os.getenv("DATABASE_USER", "app"),
+        "PASSWORD": os.getenv("DATABASE_PASSWORD", "1691"),
+        "HOST": os.getenv("DATABASE_HOST", "127.0.0.1"), 
     }
 }
 
@@ -100,12 +119,13 @@ AUTH_PASSWORD_VALIDATORS = [
 ]
 
 
+
 # Internationalization
 # https://docs.djangoproject.com/en/4.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = "ru"
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = "Europe/Moscow"
 
 USE_I18N = True
 
@@ -121,3 +141,10 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CORS_ALLOWED_ORIGINS = os.getenv("CORS_ALLOWED_ORIGINS", "").split(".")
+
+#files
+MEDIA_URL = "/media/"
+MEDIA_ROOT = BASE_DIR / "uploads"
+
