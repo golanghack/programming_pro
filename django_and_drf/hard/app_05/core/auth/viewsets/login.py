5232716d1b4611e173a163c7f79f74519de80248
golanghack <golanghack@gmail.com>
2023-07-22 16:48:48 +0300

    added new programm -> django_an_drf -> hard -> app_05 -> core -> auth -> viewsets -> logout

--- django_and_drf/hard/app_05/core/auth/viewsets/login.py
+++ django_and_drf/hard/app_05/core/auth/viewsets/login.py
@@ -3,7 +3,7 @@ from rest_framework.viewsets import ViewSet
 from rest_framework.permissions import AllowAny
 from rest_framework import status
 from rest_framework_simplejwt.exceptions import TokenError, InvalidToken
-from core.auth.serializers import LoginSerializer
+from core.auth.serializers.login import LoginSerializer
 
 class LoginViewSet(ViewSet):
     serializer_class = LoginSerializer
