8deb1c4082ace8a5ea5655377a3387ff45c27c89
golanghack <golanghack@gmail.com>
2023-07-23 17:48:47 +0300

    added new programm -> django_an_drf -> hard -> rename app_05 to app_04, create app_05, testing, renameing and more, this app for working and mistakes only

--- django_and_drf/hard/app_04/app/settings.py
+++ django_and_drf/hard/app_04/app/settings.py
@@ -38,7 +38,7 @@ ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS", "*").split(".")
 # Application definition
 
 INSTALLED_APPS = [
-    #'django.contrib.admin',
+    'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
--- django_and_drf/hard/app_04/app/urls.py
+++ django_and_drf/hard/app_04/app/urls.py
@@ -5,7 +5,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-   # path('admin/', admin.site.urls),
+    path('admin/', admin.site.urls),
     path('api/', include(('core.routers', 'core'), namespace='core-api')),
 ]
 
