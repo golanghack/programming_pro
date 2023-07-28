b26e837a0af0b70bf4e3cd1d2754c628166e9689
golanghack <golanghack@gmail.com>
2023-07-28 13:43:59 +0300

    added new program -> django_and_drf -> middle -> app_04 -> main -> added folders templates and static and installed bootstrap5

--- django_and_drf/middle/app_04/app/settings.py
+++ django_and_drf/middle/app_04/app/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     # <--LOADED-->
+    "bootstrap5",
     # <--END-->
     # <--MINE-->
     "main.apps.MainConfig",
