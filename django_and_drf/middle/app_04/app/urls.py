2be9c56b0a8558a4c26e7fd04e120488026bc99b
golanghack <golanghack@gmail.com>
2023-07-28 14:20:37 +0300

    added new program -> django_and_drf -> middle -> app_04 -> app -> urls -> added main urls and added never_cahce

--- django_and_drf/middle/app_04/app/urls.py
+++ django_and_drf/middle/app_04/app/urls.py
@@ -15,8 +15,15 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include 
+from django.conf import settings
+from django.contrib.staticfiles.views import serve
+from django.views.decorators.cache import never_cache
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path('', include('main.urls')),
 ]
+
+if settings.DEBUG:
+    urlpatterns.append(path('static/<path:path>', never_cache(serve)))
