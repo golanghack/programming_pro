1743a1cf49c80ffec7dd528d6cadee5997eecedf
golanghack <golanghack@gmail.com>
2023-07-28 13:41:33 +0300

    added new program -> django_and_drf -> middle -> app_04 -> app -> settings -> added app main

--- django_and_drf/middle/app_04/app/settings.py
+++ django_and_drf/middle/app_04/app/settings.py
@@ -42,6 +42,11 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    # <--LOADED-->
+    # <--END-->
+    # <--MINE-->
+    "main.apps.MainConfig",
+    # <--END-->
 ]
 
 MIDDLEWARE = [
