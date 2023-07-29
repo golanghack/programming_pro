b2ecb74fe5421ba8ca8855bf31199913b9e3a946
golanghack <golanghack@gmail.com>
2023-07-29 11:14:59 +0300

    added new program -> django_and_drf -> middle -> app_04 -> main -> models -> added AdvUser

--- django_and_drf/middle/app_04/main/models.py
+++ django_and_drf/middle/app_04/main/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+from django.contrib.auth.models import AbstractUser
+
+class AdvUser(AbstractUser):
+    """AdvUser 
+    is_activated -> user is activated?
+    send_messages -> user send messages?
+    """ 
+
+    is_activeted = models.BooleanField(default=True,
+                                    db_index=True, 
+                                    verbose_name='Активирован?')
+    send_messages = models.BooleanField(default=True,
+                                    verbose_name='Отправльять сообщения о комментариях?')
+    
+    class Meta(AbstractUser.Meta):
+        pass
