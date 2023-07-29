d09c5bf2be96bcd5b0857f197d95458f07e70109
golanghack <golanghack@gmail.com>
2023-07-29 10:58:23 +0300

    added new program -> django_and_drf -> middle -> app_04 -> main -> views -> other_page

--- django_and_drf/middle/app_04/main/views.py
+++ django_and_drf/middle/app_04/main/views.py
@@ -1,5 +1,16 @@
 from django.shortcuts import render
+from django.http import HttpResponse, Http404
+from django.template import TemplateDoesNotExist
+from django.template.loader import get_template
+from typing import Union
 
 
 def index(request: str) -> render:
     return render(request, 'main/index.html')
+
+def other_page(request: str, page: str) -> Union(HttpResponse, Http404):
+    try:
+        template = get_template('main/' + page + '.html')
+    except TemplateDoesNotExist:
+        raise Http404
+    return HttpResponse(template.render(request=request))
