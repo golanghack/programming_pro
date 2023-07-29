9306555ce4589ef225f627be22ee503a57caf88b
golanghack <golanghack@gmail.com>
2023-07-29 16:12:27 +0300

    added new program -> std_lib -> utils -> argparse_uses_parent

---
+++ std_lib/utils/argparse_uses_parent.py
@@ -0,0 +1,9 @@
+#! /usr/bin/env python3
+
+import argparse
+import argparse_parent_base
+
+parser = argparse.ArgumentParser(parents=[argparse_parent_base.parser],)
+parser.add_argument('--local-arg', action='store_true', default=False)
+
+print(parser.parse_args())
