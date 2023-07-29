37ba05c560207d1ee95c1837faa103a6a85e0713
golanghack <golanghack@gmail.com>
2023-07-29 12:50:33 +0300

    added new program -> django_and_drf -> middle -> app_04 -> main -> urls -> added router for password change

--- django_and_drf/middle/app_04/main/urls.py
+++ django_and_drf/middle/app_04/main/urls.py
@@ -1,11 +1,15 @@
 from django.urls import path
 from main.views import (index, other_page, AppLoginView,
-                        profile, AppLogoutView, ChangeUserInfoView)
+                        profile, AppLogoutView, ChangeUserInfoView,
+                        AppPasswordChangeView)
 
 app_name = 'main'
 urlpatterns = [
-    path('accounts/profile/change/', ChangeUserInfoView.as_view(), name='change_user_info'),
+    path('accounts/profile/change/', ChangeUserInfoView.as_view(), 
+                                            name='change_user_info'),
     path('accounts/logout/', AppLogoutView.as_view(), name='logout'),
+    path('accounts/password/change', AppPasswordChangeView.as_view(), 
+                                            name='password_change'),
     path('accounts/profile/', profile, name='profile'),
     path('accounts/login/', AppLoginView.as_view(), name='login'),
     path('<str:page>/', other_page, name='other'),
