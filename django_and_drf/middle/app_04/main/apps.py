913ff6f79d5a1a3c8cf5d081e003f930b34afce3
golanghack <golanghack@gmail.com>
2023-07-29 14:33:34 +0300

    added new program -> django_and_drf -> middle -> app_04 -> main -> apps -> added signal user_registered

--- django_and_drf/middle/app_04/main/apps.py
+++ django_and_drf/middle/app_04/main/apps.py
@@ -1,4 +1,14 @@
 from django.apps import AppConfig
+from django.dispatch import Signal
+
+from main.utils import send_activation_notification
+
+user_registered = Signal(providing_args=['instance'])
+
+def user_registered_dispatcher(sender: str, **kwargs) -> None:
+    send_activation_notification(kwargs['instance'])
+
+user_registered.connect(user_registered_dispatcher)
 
 
 class MainConfig(AppConfig):
