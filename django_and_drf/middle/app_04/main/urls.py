edbf824a98e3ce990ccb6b624249c2e67ea7e8a1
golanghack <golanghack@gmail.com>
2023-07-29 11:38:10 +0300

    added new program -> django_and_drf -> middle -> app_04 -> main -> urls -> added router for profile

--- django_and_drf/middle/app_04/main/urls.py
+++ django_and_drf/middle/app_04/main/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
-from main.views import index, other_page, AppLoginView
+from main.views import (index, 
+                        other_page, 
+                        AppLoginView,
+                        profile)
 
 app_name = 'main'
 urlpatterns = [
+    path('accounts/profile/', profile, name='profile'),
     path('accounts/login/', AppLoginView.as_view(), name='login'),
     path('<str:page>/', other_page, name='other'),
     path('', index, name='index'),
