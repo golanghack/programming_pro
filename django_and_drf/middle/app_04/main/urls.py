e4badc3a6f82783edd37573ba74f52a5730a8a42
golanghack <golanghack@gmail.com>
2023-07-29 12:38:03 +0300

    added new program -> django_and_drf -> middle -> app_04 -> main -> urls -> added router for change profile

--- django_and_drf/middle/app_04/main/urls.py
+++ django_and_drf/middle/app_04/main/urls.py
@@ -1,11 +1,10 @@
 from django.urls import path
-from main.views import (index, 
-                        other_page, 
-                        AppLoginView,
-                        profile, AppLogoutView)
+from main.views import (index, other_page, AppLoginView,
+                        profile, AppLogoutView, ChangeUserInfoView)
 
 app_name = 'main'
 urlpatterns = [
+    path('accounts/profile/change/', ChangeUserInfoView.as_view(), name='profile_change'),
     path('accounts/logout/', AppLogoutView.as_view(), name='logout'),
     path('accounts/profile/', profile, name='profile'),
     path('accounts/login/', AppLoginView.as_view(), name='login'),
