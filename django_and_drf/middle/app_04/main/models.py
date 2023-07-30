d073d27eb826fec92f72493d73a0f88854891137
golanghack <golanghack@gmail.com>
2023-07-30 17:43:16 +0300

    added new program -> django_and_drf -> middle -> app_04 -> main -> models -> added Rubric model

--- django_and_drf/middle/app_04/main/models.py
+++ django_and_drf/middle/app_04/main/models.py
@@ -16,3 +16,19 @@ class AdvUser(AbstractUser):
     
     class Meta(AbstractUser.Meta):
         pass
+
+
+class Rubric(models.Model):
+    name = models.CharField(max_length=20, 
+                            db_index=True, 
+                            unique=True,
+                            verbose_name='Название рубрики')
+    order = models.SmallIntegerField(default=0, 
+                            db_index=True, 
+                            verbose_name='Порядок рубрик')
+    super_rubric = models.ForeignKey('SuperRubric',
+                            on_delete=models.PROTECT, 
+                            null=True, 
+                            blank=True, 
+                            verbose_name='Родительская рубрика')
+    
