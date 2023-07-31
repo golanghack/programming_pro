fb5e9a875e899f5fbc63e1a0786872cf997dc57d
golanghack <golanghack@gmail.com>
2023-07-31 19:01:53 +0300

    added new program -> django_and_drf -> middle -> app_04 -> main -> urls -> added router for detail

--- django_and_drf/middle/app_04/main/urls.py
+++ django_and_drf/middle/app_04/main/urls.py
@@ -6,7 +6,7 @@ from main.views import (index, other_page, AppLoginView,
                         DeleteUserView, AppPasswordResetCompleteView,
                         AppPasswordResetConfirmView,
                         AppPasswordResetDoneView,
-                        AppPasswordResetView, by_rubric)
+                        AppPasswordResetView, by_rubric, detail)
 
 app_name = 'main'
 urlpatterns = [
@@ -32,6 +32,7 @@ urlpatterns = [
                                        name='password_reset_confirm'),
     path('accounts/profile/', profile, name='profile'),
     path('accounts/login/', AppLoginView.as_view(), name='login'),
+    path('<int:rubric_pk>/<int:pk>/', detail, name='detail'),
     path('<int:pk>/', by_rubric, name='by_rubric'),
     path('<str:page>/', other_page, name='other'),
     path('', index, name='index'),
