8d92a7fd8df49f6a33574a7a9b1e87502d5ebcdc
golanghack <golanghack@gmail.com>
2023-07-31 17:48:21 +0300

    added new program -> django_and_drf -> middle -> app_04 -> app -> settings -> added media root folders and url

--- django_and_drf/middle/app_04/app/settings.py
+++ django_and_drf/middle/app_04/app/settings.py
@@ -138,3 +138,8 @@ STATIC_URL = "static/"
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 AUTH_USER_MODEL = 'main.AdvUser'
+
+# media
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
+
