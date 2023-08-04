828a33d6745d873c8c50b186ba3e43927d1ef887
golanghack <golanghack@gmail.com>
2023-08-04 11:34:16 +0300

    added new program -> django_and_drf -> middle -> app_04 -> api -> views -> NewsDetailView

--- django_and_drf/middle/app_04/api/views.py
+++ django_and_drf/middle/app_04/api/views.py
@@ -1,8 +1,9 @@
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
+from rest_framework.generics import RetrieveAPIView
 
 from main.models import News
-from api.serializers import NewsSerializer
+from api.serializers import NewsSerializer, NewsDetailSerializer
 
 @api_view(['GET'])
 def news(request: str) -> Response:
@@ -11,4 +12,7 @@ def news(request: str) -> Response:
         news = News.objects.filter(is_active=True)[:10]
         serializer = NewsSerializer(news, many=True)
         return Response(serializer.data)
-        
+
+class NewsDetailView(RetrieveAPIView):
+    queryset = News.objects.filter(is_active=True)
+    serializer_class = NewsDetailSerializer
