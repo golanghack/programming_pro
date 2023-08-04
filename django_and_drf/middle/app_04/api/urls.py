d059f79484714f667162a4540af23b9ec0b78e68
golanghack <golanghack@gmail.com>
2023-08-04 11:35:34 +0300

    added new program -> django_and_drf -> middle -> app_04 -> api -> urls -> added News detail

--- django_and_drf/middle/app_04/api/urls.py
+++ django_and_drf/middle/app_04/api/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 
-from api.views import news
+from api.views import news, NewsDetailView
 
 urlpatterns = [
+    path('news/<int:pk>/', NewsDetailView.as_view()),
     path('news/', news),
+
 ]
