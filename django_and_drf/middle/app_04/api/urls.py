7fd7a2b8ba45bc81e844b32d4fa22bb7a7d67b2d
golanghack <golanghack@gmail.com>
2023-08-04 14:03:13 +0300

    added new program -> django_and_drf -> middle -> app_04 -> api -> urls -> added router for comments

--- django_and_drf/middle/app_04/api/urls.py
+++ django_and_drf/middle/app_04/api/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from api.views import news, NewsDetailView
+from api.views import news, NewsDetailView, comments
 
 urlpatterns = [
+    path('news/<int:pk>/comments/', comments),
     path('news/<int:pk>/', NewsDetailView.as_view()),
     path('news/', news),
 
