fa32477a05d37c82db1aa34a569e0ca1d8f16817
golanghack <golanghack@gmail.com>
2023-08-04 11:24:42 +0300

    added new program -> django_and_drf -> middle -> app_04 -> api -> views -> added news controller

--- django_and_drf/middle/app_04/api/views.py
+++ django_and_drf/middle/app_04/api/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
 
-# Create your views here.
+from main.models import News
+from api.serializers import NewsSerializer
+
+@api_view(['GET'])
+def news(request: str) -> Response:
+    if request.method == 'GET':
+        # first 10 news
+        news = News.objects.filter(is_active=True)[:10]
+        serializer = NewsSerializer(news, many=True)
+        return Response(serializer.data)
+        
