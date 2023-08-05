927c166df389baf5c13c7a26d4f134a60a90ca0c
golanghack <golanghack@gmail.com>
2023-08-05 14:49:48 +0300

    added new program -> django_nad_drf -> middle -> app_04 -> main -> sitemaps

---
+++ django_and_drf/middle/app_04/main/sitemaps.py
@@ -0,0 +1,27 @@
+from django.contrib.sitemaps import Sitemap
+from main.models import News, Rubric
+
+class NewsSitemap(Sitemap):
+    changefreq = 'weekly'
+    priority = 0.9
+
+    def items(self):
+        return News.objects.all()
+
+    def lastmod(self, obj: str):
+        return obj.updated
+
+class RubricSitemap(Sitemap):
+    changefreq = 'weekly'
+    priority = 0.9
+
+    def items(self):
+        return Rubric.objects.all()
+
+    def lastmod(self, obj: str):
+        return obj.updated
+
+
+
+
+    
