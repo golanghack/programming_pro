a99b9f493151f0e8c47b7167c74a42a2071c3e0a
golanghack <golanghack@gmail.com>
2023-08-05 17:12:50 +0300

    added new program -> django_nad_drf -> middle -> app_04 -> main -> urls -> added router for search

--- django_and_drf/middle/app_04/main/urls.py
+++ django_and_drf/middle/app_04/main/urls.py
@@ -8,12 +8,13 @@ from main.views import (index, other_page, AppLoginView,
                         AppPasswordResetDoneView,
                         AppPasswordResetView, by_rubric, detail, 
                         profile_new_detail, profile_news_add, 
-                        profile_news_change, profile_news_delete)
+                        profile_news_change, profile_news_delete, news_search)
 from main.feeds import LatestNewsFeed
 
 app_name = 'main'
 urlpatterns = [
     path('rss/', LatestNewsFeed(), name='rss'),
+    path('search/', news_search, name='search'),
     path('accounts/register/activate/<str:sign>/', user_activate, 
                                     name='register_activate'),
     path('accounts/register/done/', RegisterDoneView.as_view(),
