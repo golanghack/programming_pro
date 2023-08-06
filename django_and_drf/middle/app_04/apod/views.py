cac05320c4ac4edd6b3aa4a58f9c93a6217f4bed
golanghack <golanghack@gmail.com>
2023-08-06 14:20:15 +0300

    added new program -> django_nad_drf -> middle -> app_04 -> apod -> views -> added apod

--- django_and_drf/middle/app_04/apod/views.py
+++ django_and_drf/middle/app_04/apod/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
 
-# Create your views here.
+from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render
+from django.conf import settings
+import requests
+import json
+
+
+def home(request):
+    response = requests.get('https://api.nasa.gov/planetary/apod?api_key='+settings.NASA_API_KEY)
+    loaded_json = json.loads(response.text)
+
+    daily_image = loaded_json.get('url')
+    title = loaded_json.get('title')
+    explanation = loaded_json.get('explanation')
+    date = loaded_json.get('date')
+    owner = loaded_json.get('copyright')
+
+    context = {
+        'daily_image': daily_image,
+        'title':title,
+        'explanation':explanation,
+        'date':date,
+        'owner':owner
+    }
+
+    return render(request, "apod.html", context)
