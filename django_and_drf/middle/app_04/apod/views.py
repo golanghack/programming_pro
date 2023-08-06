6af091264d157771dc9395b3939b5e489b0e2d7d
golanghack <golanghack@gmail.com>
2023-08-06 14:20:52 +0300

    added new program -> django_nad_drf -> middle -> app_04 -> apod -> views -> refactoring

--- django_and_drf/middle/app_04/apod/views.py
+++ django_and_drf/middle/app_04/apod/views.py
@@ -1,13 +1,11 @@
 
 from django.shortcuts import render
-from django.contrib.auth.decorators import login_required
-from django.shortcuts import render
 from django.conf import settings
 import requests
 import json
 
 
-def home(request):
+def apod(request):
     response = requests.get('https://api.nasa.gov/planetary/apod?api_key='+settings.NASA_API_KEY)
     loaded_json = json.loads(response.text)
 
