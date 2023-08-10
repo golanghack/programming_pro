3a5f75eae61ac9ae14fb606bf67e457a2e67d562
golanghack <golanghack@gmail.com>
2023-08-10 17:02:55 +0300

    added new program -> django_and_drf -> simple -> app_09 -> config -> settings -> added installed apps accounts app

--- django_and_drf/simple/app_09/config/settings.py
+++ django_and_drf/simple/app_09/config/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    #<--LOCAL-->
+    "accounts.apps.AccountsConfig",
+    #<--END LOCAL-->
 ]
 
 MIDDLEWARE = [
