34db41da7fef56e0582b3d88f0d4a035137cb1c0
golanghack <golanghack@gmail.com>
2023-08-10 16:48:27 +0300

    added new program -> django_and_drf -> simple -> app_08 -> added app accounts

--- django_and_drf/simple/app_08/config/settings.py
+++ django_and_drf/simple/app_08/config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-+j!^ds@e@3n$e_k*toj+-b#k61fmrgzhkx^2t8b=yk=b*lck31
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -75,8 +75,12 @@ WSGI_APPLICATION = "config.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql_psycopg2",
+        "NAME": "postgres",
+        "USER": "postgres",
+        "PASSWORD": "postgres",
+        "HOST": "db",
+        "PORT": 5432
     }
 }
 
