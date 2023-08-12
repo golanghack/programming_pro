3f4837f49f8380dd946c35747a21c7f525ba4a2a
golanghack <golanghack@gmail.com>
2023-08-12 16:50:13 +0300

    added new program -> django_and_drf -> simple -> app_010 -> pages -> tests refactoring with DRY

--- django_and_drf/simple/app_010/pages/tests.py
+++ django_and_drf/simple/app_010/pages/tests.py
@@ -3,10 +3,19 @@ from django.urls import reverse
 
 class HomeTests(SimpleTestCase):
     
-    def test_home_status_code(self):
-        response = self.client.get('/')
-        self.assertEqual(response.status_code, 200)
+    def setUp(self):
+        url = reverse('home')
+        self.response = self.client.get(url)
 
-    def test_home_url(self):
-        response = self.client.get(reverse('home'))
-        self.assertEqual(response.status_code, 200)
+    def test_home(self):
+        self.assertEqual(self.response.status_code, 200)
+
+    def test_home_template(self):
+        self.assertTemplateUsed(self.response, 'home.html')
+
+    def test_home_contains_correct(self):
+        self.assertContains(self.response, 'Home')
+
+    def test_home_incorrect(self):
+        self.assertNotContains(self.response, 'Hi')
+        
