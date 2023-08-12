f6e54dc18b468f3776523842ef6d798a7df9cc2b
golanghack <golanghack@gmail.com>
2023-08-12 16:09:39 +0300

    added new program -> django_and_drf -> simple -> app_010 -> accounts -> admin

--- django_and_drf/simple/app_010/accounts/admin.py
+++ django_and_drf/simple/app_010/accounts/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from django.contrib.auth import get_user_model
+from django.contrib.auth.admin import UserAdmin
+from accounts.forms import MyUserChangeForm, MyUserCreationForm
 
-# Register your models here.
+MyUser = get_user_model()
+
+class MyUserAdmin(MyUser):
+    add_form = MyUserCreationForm
+    form = MyUserChangeForm
+    model = MyUser
+    list_display = ['email', 'username']
+
+admin.site.register(MyUser, MyUserAdmin)
