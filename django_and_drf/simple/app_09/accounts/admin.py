678aa53f955d1ed773ca5d7c7af8817d17b178f8
golanghack <golanghack@gmail.com>
2023-08-12 15:42:18 +0300

    added new program -> django_and_drf -> simple -> app_010 -> added requirements.txt, installed django, psycopg2

--- django_and_drf/simple/app_09/accounts/admin.py
+++ django_and_drf/simple/app_09/accounts/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
-from django.contrib.auth.mod
+from django.contrib.auth import get_user_model
+from django.contrib.auth.admin import UserAdmin
+from accounts.forms import MyUserChangeForm, MyUserCreationForm
 from accounts.models import MyUser
+
+MyUser = get_user_model()
+
+class MyUserAdmin(UserAdmin):
+    add_form = MyUserCreationForm
+    form = MyUserChangeForm
+    model = MyUser
+    list_display = ['email', 'username']
+
+admin.site.register(MyUser, MyUserAdmin)
