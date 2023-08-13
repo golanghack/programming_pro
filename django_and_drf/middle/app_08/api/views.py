2441640194809536415bfaa7714014255edf75dc
golanghack <golanghack@gmail.com>
2023-08-13 15:09:33 +0300

    added new program -> django_and_drf -> middle -> app_08 -> api -> views

--- django_and_drf/middle/app_08/api/views.py
+++ django_and_drf/middle/app_08/api/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
+from api.models import Post
+from api.serializers import PostSerializer
 
-# Create your views here.
+class PostList(ListCreateAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+
+class PostDetail(RetrieveUpdateDestroyAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
