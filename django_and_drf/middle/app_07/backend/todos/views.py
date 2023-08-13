0af651cc864f2750cb92a4032c16dd1b876a77e5
golanghack <golanghack@gmail.com>
2023-08-13 14:05:16 +0300

    added new program -> django_and_drf -> middle -> app_07 -> backend -> todos -> views -> added ListTodo, DetailTodo

--- django_and_drf/middle/app_07/backend/todos/views.py
+++ django_and_drf/middle/app_07/backend/todos/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from rest_framework.generics import ListAPIView, RetrieveAPIView
+from todos.models import Todo
+from todos.serializers import TodoSerializer
 
-# Create your views here.
+class ListTodo(ListAPIView):
+    queryset = Todo.objects.all()
+    serializer_class = TodoSerializer
+
+class DetailTodo(RetrieveAPIView):
+    queryset = Todo.objects.all()
+    serializer_class = TodoSerializer
