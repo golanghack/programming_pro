83a37b73ac12433596fbfb8bf9c6eebaa47ad5b8
golanghack <golanghack@gmail.com>
2023-08-13 13:00:52 +0300

    added new program -> django_and_drf -> middle -> app_06 -> added installed apps books, refactoring models

--- django_and_drf/middle/app_06/books/models.py
+++ django_and_drf/middle/app_06/books/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 
-class Book(models):
+class Book(models.Model):
     author = models.CharField(max_length=200)
     title = models.CharField(max_length=200)
     subtitle = models.CharField(max_length=200)
