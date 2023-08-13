dc6c4d4c71fdf2bc04f52c1e6e229d6bbfc17cc4
golanghack <golanghack@gmail.com>
2023-08-13 18:02:28 +0300

    added new program -> django_and_drf -> middle -> app_08 -> api -> views -> added UserList UserDetai;

--- django_and_drf/middle/app_08/api/views.py
+++ django_and_drf/middle/app_08/api/views.py
@@ -1,7 +1,7 @@
-
+from django.contrib.auth import get_user_model
 from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
 from api.models import Post
-from api.serializers import PostSerializer
+from api.serializers import PostSerializer, UserSerializer
 from api.permissions import IsAuthorOrReadOnly
 
 class PostList(ListCreateAPIView):
@@ -13,3 +13,10 @@ class PostDetail(RetrieveUpdateDestroyAPIView):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
 
+class UserList(ListCreateAPIView):
+    queryset = get_user_model().objects.all()
+    serializer_class = UserSerializer
+
+class UserDetail(RetrieveUpdateDestroyAPIView):
+    queryset = get_user_model().objects.all()
+    serializer_class = UserSerializer
