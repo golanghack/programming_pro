5e11379adac55a7d60c32c9c02ca4a5bb86889b1
golanghack <golanghack@gmail.com>
2023-08-13 18:04:49 +0300

    added new program -> django_and_drf -> middle -> app_08 -> api -> urls -> added routers for users

--- django_and_drf/middle/app_08/api/urls.py
+++ django_and_drf/middle/app_08/api/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
 
-from api.views import PostList, PostDetail
+from api.views import (PostList, PostDetail, 
+                        UserList, UserDetail)
 
 urlpatterns = [
+    path('users/', UserList.as_view()),
+    path('users/<int:pk>/', UserDetail.as_view()),
     path('<int:pk>/', PostDetail.as_view()),
     path('', PostList.as_view()),
 ]
