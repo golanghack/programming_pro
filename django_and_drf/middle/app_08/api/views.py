6cec1af7eeb17dbc897f65acec29deec9f242c4a
golanghack <golanghack@gmail.com>
2023-08-13 15:17:44 +0300

    added new program -> django_and_drf -> middle -> app_08 -> api -> views -> added permission_classes

--- django_and_drf/middle/app_08/api/views.py
+++ django_and_drf/middle/app_08/api/views.py
@@ -1,11 +1,14 @@
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
 from api.models import Post
 from api.serializers import PostSerializer
 
 class PostList(ListCreateAPIView):
+    permission_classes = (IsAuthenticated,)
     queryset = Post.objects.all()
     serializer_class = PostSerializer
 
 class PostDetail(RetrieveUpdateDestroyAPIView):
+    permission_classes = (IsAuthenticated,)
     queryset = Post.objects.all()
     serializer_class = PostSerializer
