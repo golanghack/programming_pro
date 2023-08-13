8d69cbd3a0c2f8ac3830d7ffb57d439dcdabf42d
golanghack <golanghack@gmail.com>
2023-08-13 13:09:56 +0300

    added new program -> django_and_drf -> middle -> app_06 -> books -> views -> Books

--- django_and_drf/middle/app_06/books/views.py
+++ django_and_drf/middle/app_06/books/views.py
@@ -1,3 +1,6 @@
-from django.shortcuts import render
+from django.views.generic import ListView
+from books.models import Book
 
-# Create your views here.
+class Books(ListView):
+    model = Book
+    template_name = 'books.html'
