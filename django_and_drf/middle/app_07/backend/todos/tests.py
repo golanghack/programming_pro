42ef185eebadcdbfb38fc02aab970696c288138d
golanghack <golanghack@gmail.com>
2023-08-13 14:20:03 +0300

    added new program -> django_and_drf -> middle -> app_07 -> backend -> todos -> tests

--- django_and_drf/middle/app_07/backend/todos/tests.py
+++ django_and_drf/middle/app_07/backend/todos/tests.py
@@ -1,3 +1,18 @@
 from django.test import TestCase
+from todos.models import Todo
 
-# Create your tests here.
+class TodoTest(TestCase):
+    
+    @classmethod
+    def setUpTestData(cls: str):
+        Todo.objects.create(title='one test', body='one test')
+
+    def test_title_correct(self):
+        todo = Todo.objects.get(id=1)
+        expected_name = f'{todo.title}'
+        self.assertEqual(expected_name, 'one test')
+
+    def test_body_correct(self):
+        todo = Todo.objects.get(id=1)
+        expected_name = f'{todo.body}'
+        self.assertEqual(expected_name, 'one test')
