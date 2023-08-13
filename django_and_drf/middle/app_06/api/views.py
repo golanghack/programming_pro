b03df7a27943832fd929122310b735726789e61a
golanghack <golanghack@gmail.com>
2023-08-13 13:25:25 +0300

    added new program -> django_and_drf -> middle -> app_06 -> api -> views -> BookApiView

--- django_and_drf/middle/app_06/api/views.py
+++ django_and_drf/middle/app_06/api/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from rest_framework import generics
+from books.models import Book
+from api.serializers import BookSerializer
 
-# Create your views here.
+class BookApiView(generics.ListAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+    
