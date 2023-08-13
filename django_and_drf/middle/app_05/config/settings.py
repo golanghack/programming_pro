9d5572db22fbf232ba00fdc399f8f4eca45e8dfb
golanghack <golanghack@gmail.com>
2023-08-13 12:06:36 +0300

    added new program -> django_and_drf -> middle -> app_05 -> installed psycopg

--- django_and_drf/middle/app_05/config/settings.py
+++ django_and_drf/middle/app_05/config/settings.py
@@ -73,10 +73,16 @@ WSGI_APPLICATION = "config.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# https://docs.djangoproject.com/en/4.2/ref/settings/#databases
+
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql_psycopg2",
+        "NAME": "postgres",
+        "USER": "postgres",
+        "PASSWORD": "postgres",
+        "HOST": "db",
+        "PORT": 5432
     }
 }
 
