e051d8e957d5377eed2f3db097073609c1b092c4
golanghack <golanghack@gmail.com>
2023-08-13 12:18:54 +0300

    added new program -> django_and_drf -> middle -> app_05 -> config -> settings -> added books in installed apps

--- django_and_drf/middle/app_05/config/settings.py
+++ django_and_drf/middle/app_05/config/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    # <--LOCAL-->
+    "books.apps.BooksConfig",
+    # <--END LOCAL-->
 ]
 
 MIDDLEWARE = [
