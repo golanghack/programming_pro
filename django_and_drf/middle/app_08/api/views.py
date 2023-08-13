e2248a28d455492071854848066fd112dbd848da
golanghack <golanghack@gmail.com>
2023-08-13 18:09:40 +0300

    added new program -> django_and_drf -> middle -> app_08 -> api -> views -> refactoting with viewsets

--- django_and_drf/middle/app_08/api/views.py
+++ django_and_drf/middle/app_08/api/views.py
@@ -1,22 +1,16 @@
 from django.contrib.auth import get_user_model
-from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
+from rest_framework import viewsets
+
 from api.models import Post
-from api.serializers import PostSerializer, UserSerializer
 from api.permissions import IsAuthorOrReadOnly
+from api.serializers import UserSerializer, PostSerializer
 
-class PostList(ListCreateAPIView):
-    queryset = Post.objects.all()
-    serializer_class = PostSerializer
-
-class PostDetail(RetrieveUpdateDestroyAPIView):
+class PostViewSet(viewsets.ModelViewSet):
     permission_classes = (IsAuthorOrReadOnly,)
     queryset = Post.objects.all()
     serializer_class = PostSerializer
 
-class UserList(ListCreateAPIView):
+class UserViewSet(viewsets.ModelViewSet):
     queryset = get_user_model().objects.all()
     serializer_class = UserSerializer
-
-class UserDetail(RetrieveUpdateDestroyAPIView):
-    queryset = get_user_model().objects.all()
-    serializer_class = UserSerializer
+    
