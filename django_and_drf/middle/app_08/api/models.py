a22fcfe8f3acca53e0eed448f189f9362e5b5c8c
golanghack <golanghack@gmail.com>
2023-08-13 14:49:17 +0300

    added new program -> django_and_drf -> middle -> app_08 -> api -> models -> added Post

--- django_and_drf/middle/app_08/api/models.py
+++ django_and_drf/middle/app_08/api/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Post(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=40)
+    body = models.TextField(default='')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
