b12edb6d8a5565e8e53556599015352a53f722a4
golanghack <golanghack@gmail.com>
2023-08-14 19:39:49 +0300

    added new program -> django_and_drf -> simple -> app_011 -> added Dockerfile, docker-compose

--- django_and_drf/simple/app_011/config/settings.py
+++ django_and_drf/simple/app_011/config/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = "django-insecure-sv$_x3gnf+u_88el@%4bx2f6f_-gd8c$2#k56&ajw%4r4zp9o9
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
