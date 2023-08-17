5db8418ae7f006e2a3a6b3480326810af394838d
golanghack <golanghack@gmail.com>
2023-08-17 18:05:15 +0300

    added new program -> django_and_drf -> simple -> app_012 -> books -> urls -> added seach router

--- django_and_drf/simple/app_012/books/urls.py
+++ django_and_drf/simple/app_012/books/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from .views import BookListView, BookDetailView
+from .views import BookListView, BookDetailView, SearchResultsView
 
 urlpatterns = [
     path("", BookListView.as_view(), name="book_list"),
-    path("<uuid:pk>", BookDetailView.as_view(), name="book_detail"),  # new
+    path("<uuid:pk>", BookDetailView.as_view(), name="book_detail"), 
+    path("search/", SearchResultsView.as_view(), name='search_results'),
 ]
