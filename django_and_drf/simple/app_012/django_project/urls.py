386f0eed217fbe5c8f9c76139de5b101b7db8625
golanghack <golanghack@gmail.com>
2023-08-18 21:17:21 +0300

    added new program -> ci_cd -> listing_01 -> search

--- django_and_drf/simple/app_012/django_project/urls.py
+++ django_and_drf/simple/app_012/django_project/urls.py
@@ -1,12 +1,17 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
-    # Django admin
     path("admin/", admin.site.urls),
-    # User management
     path("accounts/", include("allauth.urls")),
-    # Local apps
     path("", include("pages.urls")),
     path("books/", include("books.urls")),  # new
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+if settings.DEBUG:
+    import debug_toolbar
+    urlpatterns = [
+        path('__debug__/', include(debug_toolbar.urls)),
+    ] + urlpatterns
