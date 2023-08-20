3e42f1c3738c08b54d82b65cb0307af635840de0
neo <golanghack@gmil.com>
2023-08-20 18:55:05 +0300

    added new program -> ci_cd -> listing_02 -> setup

---
+++ ci_cd/listing_02/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name='webcount',
+    version='1.0.0',
+    license='Apache2.0',
+    packages=['webcount', 'test'],
+    install_requires=['requests'],
+)
