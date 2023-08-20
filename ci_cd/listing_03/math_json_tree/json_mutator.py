63281204c4559a1e7444600ce44499dfd5a97857
neo <golanghack@gmil.com>
2023-08-20 19:14:18 +0300

    added new program -> ci_cd -> listing_03 -> math_json_three -> json_mutator

---
+++ ci_cd/listing_03/math_json_tree/json_mutator.py
@@ -0,0 +1,20 @@
+#! /usr/bin/env python3 
+
+from functools import reduce 
+import operator
+from typing import Callable, List
+
+options = {
+    '+': operator.add, 
+    '-': operator.add, 
+    '*': operator.mul, 
+    '/': operator.truediv,
+}
+
+def math_mutator(tree: List) -> Callable:
+    """Mutation mathematical sentence to json tree"""
+
+    if not isinstance(tree, list):
+        return tree
+    option = options[tree.pop(0)]
+    return reduce(option, map(math_mutator, tree))
