7fd0327674fb67300d97c9a0727e3ade980b4a8a
neo <golanghack@gmil.com>
2023-08-20 18:38:08 +0300

    added new program -> ci_cd -> listing_02 -> test -> test_webcount -> added mocking

--- ci_cd/listing_02/tests/test_webcount.py
+++ ci_cd/listing_02/tests/test_webcount.py
@@ -3,11 +3,24 @@ from webcount import most_common_word_in_web_page
 
 def test_with_patch():
     mock_req = Mock()
-    mock_req.get.return_value.text = 'xx yyy zzzz'
+    mock_req.get.return_value.text = 'aa bbb c'
     with patch('webcount.webcount.requests', mock_req):
         result = most_common_word_in_web_page(
-            ['x', 'y', 'z'], 'https://google.com',
+            ['a', 'b', 'c'], 'https://python.org'
         )
-    assert result == 'z', 'function with test'
-    assert mock_req.get.call_count == 1
-    assert mock_req.get.call_args[0][0] == 'https://google.com', 'correct url'
+    assert result == 'a', 'function with test'
+
+def test_fake_client():
+    class TestResponse():
+        text = 'xx yyy, zzzz'
+
+    class TestUserAgent():
+        def get(self, url):
+            return TestResponse()
+
+    result = most_common_word_in_web_page(
+    words=['the', 'google'],
+    target_url='https://google.com',
+    user_agent=TestUserAgent()
+    )
+    assert result == 'google', 'testing with fake client'
--- ci_cd/listing_02/webcount/webcount.py
+++ ci_cd/listing_02/webcount/webcount.py
@@ -3,14 +3,18 @@
 import requests
 from typing import List, Callable
 
-def most_common_word_in_web_page(words: List[str], target_url: str) -> Callable:
+def most_common_word_in_web_page(words: List[str], 
+                                target_url: str,
+                                user_agent=requests) -> Callable:
     """ Return most common word from a list of words on url page"""
 
-    response = requests.get(target_url)
+    response = user_agent.get(target_url)
     return most_common(words, response.text)
 
 def most_common(words: List[str], text: str) -> int:
     """Return common word from a list of words in parts of text."""
 
     dict_word_frequency = {word: text.count(word) for word in words}
+    if len(words) < 1:
+        raise Exception(IndexError, 'added element in list')
     return sorted(words, key=dict_word_frequency.get)[-1]
