28bacdb078f71e46c0a9719d237150489f93f970
neo <golanghack@gmil.com>
2023-08-21 23:27:02 +0300

    added new program -> my_lang -> syntax_parse -> create::parse_express

---
+++ my_lang/syntax_parse/parse_express.py
@@ -0,0 +1,34 @@
+#! /usr/bin/env python3 
+
+"""This parser scan input and testing first char
+If first char is char or str different whitespace 
+index incrementing
+"""
+
+def parse_express(input_s: str, idx: int):
+    idx = skip_space(input_s, idx)
+    if input_s[idx] == '(':
+        # --> LIST
+        idx += 1
+        l = []
+        while True:
+            idx = skip_space(input_s, idx)
+            if idx >= len(input_s):
+                raise Exception('unbalanced!')
+            if input_s[idx] == ')':
+                idx += 1
+                break
+            idx, value = parse_express(input_s, idx)
+            l.append(value)
+        return idx, l 
+    elif input_s[idx] == ')':
+        raise Exception('bad parenthesis')
+    else:
+        # --> SYMBOL
+        start = idx 
+        while idx < len(input_s) and (
+            not input_s[idx].isspace()) and input_s[idx] not in '()':
+            idx += 1
+        if start == idx:
+            raise Exception('empty')
+        return idx, parse_symbol(input_s[start:idx])
