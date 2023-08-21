93d7687e3292d2ec1772a3caaf234cd029758b38
neo <golanghack@gmil.com>
2023-08-21 19:15:51 +0300

    added new program -> django_and_drf -> recipes -> celery -> recipe_01 -> app -> views -> CreateShipmentView -> update::added function post

--- django_and_drf/recipes/celery/recipe_01/app/views.py
+++ django_and_drf/recipes/celery/recipe_01/app/views.py
@@ -1,6 +1,11 @@
+# libs
 from rest_framework.generics import CreateAPIView
+from rest_framework.response import Response
+from rest_framework import status
+# my 
 from app.models import Order, Shipment
 from app.serializers import OrderSerializer, ShipmentSerializer
+from app.tasks import send_shipment_email
 
 class CreateOrderView(CreateAPIView):
     serializer_class = OrderSerializer
@@ -9,3 +14,15 @@ class CreateOrderView(CreateAPIView):
 class CreateShipmentView(CreateAPIView):
     serializer_class = ShipmentSerializer
     queryset = Shipment.objects.all()
+
+    def post(self, request):
+        serializer = self.serializer_class(
+            data=request.data,
+        )
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        owner_name = serializer.data['owner_name']
+        order_name = serializer.data['order']['name']
+        owner_email = serializer.data['owner_email']
+        send_shipment_email.delay(owner_name, order_name, owner_email)
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
