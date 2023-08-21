e27e9bedbc3f0f0259bdf64c042febcb18b62cc5
neo <golanghack@gmil.com>
2023-08-21 22:59:54 +0300

    added new program -> django_and_drf -> simple -> app_014 -> config -> settings -> refactoring::added environment

--- django_and_drf/simple/app_014/config/settings.py
+++ django_and_drf/simple/app_014/config/settings.py
@@ -11,7 +11,13 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+from dotenv import load_dotenv
+import os 
 
+
+# .env 
+load_dotenv()
+ENV = os.environ.get('ENV')
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,12 +26,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-0v#!x&mvaxtpl=p5c!#i)vdja@)6mnh8v78y75u(efue&3)$a='
+SECRET_KEY = os.environ.get('SECRET_KEY', 
+                default='django-insecure-0v#!x&mvaxtpl=p5c!#i)vdja@)6mnh8v78y75u(efue&3)$a=')
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False if ENV == 'PROD' else True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.environ.get('DJANGO_ALLOWED_HOSTS', '*').split('.')
 
 
 # Application definition
@@ -37,6 +45,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # <--DOWNLOADED-->
+    # <--END DOWNLOADED-->
+    # <--LOCAL-->
+    # <--END LOCAL-->
 ]
 
 MIDDLEWARE = [
