1b528a055906740e9448b850c4149a9472f510b4
neo <golanghack@gmil.com>
2023-08-21 19:10:59 +0300

    added new program -> django_and_drf -> recipes -> celery -> recipe_01 -> recipe_01 -> settings -> updated::added email conf(for console, without smtp)

--- django_and_drf/recipes/celery/recipe_01/recipe_01/settings.py
+++ django_and_drf/recipes/celery/recipe_01/recipe_01/settings.py
@@ -150,3 +150,6 @@ CELERY_RESULT_BACKEND = 'redis://localhost:6379'
 CELERY_ACCEPT_CONTENT = ['application/json']
 CELERY_TASK_SERIALIZER = 'json'
 CELERY_RESULT_SERIALIZER = 'json'
+
+# email
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
