0df976929bbe0549e566851ca831669afd6eff02
neo <golanghack@gmil.com>
2023-08-21 19:02:58 +0300

    added new program -> django_and_drf -> recipes -> celery -> recipe_01 -> settings -> updated::added celery configuration

--- django_and_drf/recipes/celery/recipe_01/recipe_01/settings.py
+++ django_and_drf/recipes/celery/recipe_01/recipe_01/settings.py
@@ -143,3 +143,10 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# celery conf
+CELERY_BROKER_URL = 'redis://localhost:6379'
+CELERY_RESULT_BACKEND = 'redis://localhost:6379'
+CELERY_ACCEPT_CONTENT = ['application/json']
+CELERY_TASK_SERIALIZER = 'json'
+CELERY_RESULT_SERIALIZER = 'json'
