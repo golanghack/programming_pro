3c2910012463ea794d8c4bbf39df8d1f6c02c0f4
neo <golanghack@gmil.com>
2023-08-21 18:09:54 +0300

    added new program -> django_and_drf -> recipes -> celery -> recipe_01 -> app -> urls -> added routers::orders, shipments

--- django_and_drf/recipes/celery/recipe_01/app/urls.py
+++ django_and_drf/recipes/celery/recipe_01/app/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
+from app.views import CreateOrderView, CreateShipmentView
 
 urlpatterns = [
-    
+    path('orders', CreateOrderView.as_view()),
+    path('shipments', CreateShipmentView.as_view()),
 ]
--- django_and_drf/recipes/celery/recipe_01/app/views.py
+++ django_and_drf/recipes/celery/recipe_01/app/views.py
@@ -2,10 +2,10 @@ from rest_framework.generics import CreateAPIView
 from app.models import Order, Shipment
 from app.serializers import OrderSerializer, ShipmentSerializer
 
-class CreateOrder(CreateAPIView):
+class CreateOrderView(CreateAPIView):
     serializer_class = OrderSerializer
     queryset = Order.objects.all()
 
-class CreateShipment(CreateAPIView):
+class CreateShipmentView(CreateAPIView):
     serializer_class = ShipmentSerializer
     queryset = Shipment.objects.all()
