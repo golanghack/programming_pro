4f7bb57d52de0cb7ab80ccc7e3c06c7419c99ecb
neo <golanghack@gmil.com>
2023-08-21 22:04:52 +0300

    added new program -> cd_ci -> listing_03 -> added tox.ini

--- django_and_drf/recipes/celery/recipe_01/recipe_01/settings.py
+++ django_and_drf/recipes/celery/recipe_01/recipe_01/settings.py
@@ -145,8 +145,8 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # celery conf
-CELERY_BROKER_URL = 'redis://localhost:6378'
-CELERY_RESULT_BACKEND = 'redis://localhost:6378'
+CELERY_BROKER_URL = 'redis://localhost:6379'
+CELERY_RESULT_BACKEND = 'redis://localhost:6379'
 CELERY_ACCEPT_CONTENT = ['application/json']
 CELERY_TASK_SERIALIZER = 'json'
 CELERY_RESULT_SERIALIZER = 'json'
