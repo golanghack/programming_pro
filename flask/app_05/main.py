8d118f944e5d3c2e6fb3b910eb884f8487e80701
neo <golanghack@gmil.com>
2023-09-26 16:53:34 +0300

    added new program -> flask -> app_03 ->5 -> added weather api scanner, and more

--- flask/app_05/main.py
+++ flask/app_05/main.py
@@ -1,45 +1,38 @@
 #! /usr/bin/env python3
 
+
 import feedparser
 from flask import Flask
 from flask import render_template
 from flask import request
+from weather import weater_function as weather 
 
 app = Flask(__name__)
+
 FEEDS = {'prolog': 'https://www.prlog.org/news/rss.xml',
             'art': 'https://rss.art19.com/apology-line',
             'freeds': 'https://feeds.simplecast.com/54nAGcIl',
             'crime': 'https://feeds.simplecast.com/qm_9xx0g',
         }
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 @app.route('/<rubric>')
 def get_news():
     field = 'entries'
-    query = request.args.get('rubric')
+    query = request.form.get('rubric')
+    weather_query = request.form.get('weather')
     if not query or query.lower() not in FEEDS:
-        rubric = 'prolog'
+        query = 'prolog'
+    if not weather_query or weather_query == '':
+        weather_query = 'Moscow'
     rubric = query.lower()
     feed = feedparser.parse(FEEDS[rubric])
+    weather_full = weather(weather_query)
+
     return render_template('home.html', 
-                            articles=feed[field])
-                            
-@app.route('/')
-@app.route('/prolog')
-def get_prolog():
-    return get_news('prolog')
-
-@app.route('/art')
-def get_art():
-    return get_news('art')
-
-@app.route('/freeds')
-def get_freeds():
-    return get_news('freeds')
-
-@app.route('/crime')
-def get_crime():
-    return get_news('crime')
+                            weather=weather_full,
+                            articles=feed[field]
+                            )
 
 
 if __name__ == '__main__':
