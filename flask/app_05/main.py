26976cc673eb15b7bbea25e2714441e757d404e7
neo <golanghack@gmil.com>
2023-09-26 14:56:55 +0300

    added new program -> flask -> app_03 ->5 -> added full project

---
+++ flask/app_05/main.py
@@ -0,0 +1,46 @@
+#! /usr/bin/env python3
+
+import feedparser
+from flask import Flask
+from flask import render_template
+from flask import request
+
+app = Flask(__name__)
+FEEDS = {'prolog': 'https://www.prlog.org/news/rss.xml',
+            'art': 'https://rss.art19.com/apology-line',
+            'freeds': 'https://feeds.simplecast.com/54nAGcIl',
+            'crime': 'https://feeds.simplecast.com/qm_9xx0g',
+        }
+
+@app.route('/')
+@app.route('/<rubric>')
+def get_news():
+    field = 'entries'
+    query = request.args.get('rubric')
+    if not query or query.lower() not in FEEDS:
+        rubric = 'prolog'
+    rubric = query.lower()
+    feed = feedparser.parse(FEEDS[rubric])
+    return render_template('home.html', 
+                            articles=feed[field])
+                            
+@app.route('/')
+@app.route('/prolog')
+def get_prolog():
+    return get_news('prolog')
+
+@app.route('/art')
+def get_art():
+    return get_news('art')
+
+@app.route('/freeds')
+def get_freeds():
+    return get_news('freeds')
+
+@app.route('/crime')
+def get_crime():
+    return get_news('crime')
+
+
+if __name__ == '__main__':
+    app.run(port=5555, debug=True)
