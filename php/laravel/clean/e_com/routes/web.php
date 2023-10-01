2c6a9f29bf65c0020dec086a46c70e9b8ace8640
neo <golanghack@gmil.com>
2023-10-01 17:10:42 +0300

    added new program -> php ->laravel -> clean -> e_com -> resourses -> routes -> added home, about

--- php/laravel/clean/e_com/routes/web.php
+++ php/laravel/clean/e_com/routes/web.php
@@ -14,5 +14,9 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+   $viewData = [];
+   $viewData['title'] = 'Главная - Онлайн магазин Зои';
+   return view('home.index')->with('viewData', $viewData);
 });
+
+Route::get('/about', 'App\Http\Controllers\HomeController@about')->name('home.about');
