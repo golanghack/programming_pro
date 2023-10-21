7924f3d9f805eb5f55ba0d23637022b1a8032dfd
neo <golanghack@gmil.com>
2023-10-21 18:56:27 +0300

    added new program -> go -> clear -> any -> fiber_api_one -> main -> added connectro

--- go/clear/any/fiber_api_one/main.go
+++ go/clear/any/fiber_api_one/main.go
@@ -2,10 +2,14 @@ package main
 
 import (
 	"github.com/gofiber/fiber/v2"
+	""github.com/golanghack/programming_pro/tree/go/go/clear/any/fiber_api_one/database""
 )
 
 func main() {
 	app := fiber.New()
+
+	// connect to DB
+	database.ConnectDB()
 
 	// endpoint 
 	app.Get("/", func(c *fiber.Ctx) error {
