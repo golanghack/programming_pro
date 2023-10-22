8fe3a41e55d56671f01565f763c0a426bde35c00
neo <golanghack@gmil.com>
2023-10-22 16:42:54 +0300

    added new program -> go -> clear -> any -> fiber -> added full app

--- go/clear/any/fiber_api_one/main.go
+++ go/clear/any/fiber_api_one/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"github.com/gofiber/fiber/v2"
 	"github.com/golanghack/programming_pro/tree/go/go/clear/any/fiber_api_one/database"
+	"github.com/golanghack/programming_pro/tree/go/go/clear/any/fiber_api_one/router"
 )
 
 func main() {
@@ -11,11 +12,7 @@ func main() {
 	// connect to DB
 	database.ConnectDB()
 
-	// endpoint 
-	app.Get("/", func(c *fiber.Ctx) error {
-		err := c.SendString("API Support")
-		return err
-	})
+	router.SetupRouters(app)
 	app.Listen(":8000")
 }
 
