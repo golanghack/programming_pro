dce4c8851094c1c99addd3bd341ab7f9118bd371
neo <golanghack@gmil.com>
2023-10-25 21:49:45 +0300

    added new program -> js_ts -> node -> cli -> added my manager

--- js_ts/node/cli/simple_file_manager/src/index.ts
+++ js_ts/node/cli/simple_file_manager/src/index.ts
@@ -1,6 +1,9 @@
+#! /usr/bin/env node 
+
+const fs = require('fs');
+const path = require('path');
 const  { Command } = require("commander");
 const figlet = require( "figlet" );
-
 const program = new Command();
 
 console.log(figlet.textSync("Simple File Manager"));
@@ -14,3 +17,45 @@ program
     .parse(process.argv);
 
 const options = program.opts();
+
+async function listDirContents(filepath: string) {
+    try {
+    const files = await fs.promises.readdir(filepath);
+    const detailFilesPromises = files.map(async (file: string) => {
+        let fileDetails = await fs.promises.lstat(path.resolve(filepath, file));
+        const { size, birthtime } = fileDetails;
+        return { filename: file, 'size(kB)': size, created_at: birthtime};
+    });
+    const detailFiles = await Promise.all(detailFilesPromises);
+    console.table(detailFiles);
+    } catch (error) {
+        console.error('Error -> ', error);
+    }
+}
+
+
+function createDir(filepath: string) {
+    if (!fs.existSync(filepath)) {
+        fs.mkdirSync(filepath);
+        console.log('Directory has been created!');
+    }
+}
+
+
+function createFile(filepath: string) {
+    fs.openSync(filepath, 'w');
+    console.log('An e,pty file created');
+}
+
+if (options.ls) {
+    const filepath = typeof options.ls === 'string' ? options.ls: __dirname;
+    listDirContents(filepath);
+}
+
+if (options.mkdir) {
+    createDir(path.resolve(__dirname, options.mkdir));
+}
+
+if (options.touch) {
+    createFile(path.resolve(__dirname, options.touch));
+}
